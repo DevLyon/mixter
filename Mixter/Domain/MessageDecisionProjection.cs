003fb9e3403e8519a4feb4201a2d2e84dae08095
fpellet <florent.pellet@gmail.com>
2015-03-25 10:52:47 +0100

    Add projection of message

---
+++ Mixter/Domain/MessageDecisionProjection.cs
@@ -0,0 +1,17 @@
+﻿namespace Mixter.Domain
+{
+    public class MessageDecisionProjection
+    {
+        public MessageDecisionProjection(MessagePublished evt)
+        {
+            Mute(evt);
+        }
+
+        public MessageId Id { get; private set; }
+
+        private void Mute(MessagePublished evt)
+        {
+            Id = evt.Id;
+        }
+    }
+}
