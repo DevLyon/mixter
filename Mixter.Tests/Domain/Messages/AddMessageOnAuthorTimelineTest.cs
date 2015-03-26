b5dbae61ce8478d808e17b07387225542466e6d5
fpellet <florent.pellet@gmail.com>
2015-03-26 21:52:30 +0100

    Test directly MessagePublished handler

--- Mixter.Tests/Domain/Messages/AddMessageOnAuthorTimelineTest.cs
+++ Mixter.Tests/Domain/Messages/AddMessageOnAuthorTimelineTest.cs
@@ -4,32 +4,21 @@ using System.Linq;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Mixter.Domain;
 using Mixter.Domain.Messages;
-using Mixter.Domain.Subscriptions;
-using Mixter.Infrastructure;
-using Mixter.Tests.Infrastructure;
 using NFluent;
 
 namespace Mixter.Tests.Domain.Messages
 {
     [TestClass]
-    public class TimelineMessageHandlerTest
+    public class AddMessageOnAuthorTimelineTest
     {
-        private static readonly UserId Followee = new UserId("followee@mixit.fr");
-
         private TimelineMessagesRepositoryFake _timelineMessagesRepositoryFake;
-        private ISubscriptionRepository _subscriptionRepository;
-        private TimelineMessageHandler _handler;
-        private EventPublisherFake _eventPublisher;
-        private EventsDatabase _database;
+        private AddMessageOnAuthorTimeline _handler;
 
         [TestInitialize]
         public void Initialize()
         {
-            _database = new EventsDatabase();
             _timelineMessagesRepositoryFake = new TimelineMessagesRepositoryFake();
-            _subscriptionRepository = new SubscriptionRepository(_database);
-            _eventPublisher = new EventPublisherFake();
-            _handler = new TimelineMessageHandler(_timelineMessagesRepositoryFake, _subscriptionRepository, _eventPublisher);
+            _handler = new AddMessageOnAuthorTimeline(_timelineMessagesRepositoryFake);
         }
 
         [TestMethod]
@@ -41,24 +30,6 @@ namespace Mixter.Tests.Domain.Messages
             _handler.Handle(new MessagePublished(messageId, author, content));
 
             Check.That(_timelineMessagesRepositoryFake.Messages.Single()).IsEqualTo(new TimelineMessage(author, author, content, messageId));
-        }
-
-        [TestMethod]
-        public void WhenMessagePublishedByFolloweeThenRaiseFollowerMessagePublished()
-        {
-            var follower = new UserId("follower@mixit.fr");
-            AddFollower(follower);
-
-            var messageId = MessageId.Generate();
-            const string content = "content";
-            _handler.Handle(new MessagePublished(messageId, Followee, content));
-
-            Check.That(_eventPublisher.Events).Contains(new FollowerMessagePublished(new SubscriptionId(follower, Followee), messageId));
-        }
-
-        private void AddFollower(UserId follower)
-        {
-            _database.Store(new UserFollowed(new SubscriptionId(follower, Followee)));
         }
 
         private class TimelineMessagesRepositoryFake : ITimelineMessagesRepository
---
+++ Mixter.Tests/Domain/Messages/NotifyFollowerOfFolloweeMessageTest.cs
@@ -0,0 +1,48 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Mixter.Domain;
+using Mixter.Domain.Messages;
+using Mixter.Domain.Subscriptions;
+using Mixter.Infrastructure;
+using Mixter.Tests.Infrastructure;
+using NFluent;
+
+namespace Mixter.Tests.Domain.Messages
+{
+    [TestClass]
+    public class NotifyFollowerOfFolloweeMessageTest
+    {
+        private static readonly UserId Followee = new UserId("followee@mixit.fr");
+
+        private ISubscriptionRepository _subscriptionRepository;
+        private NotifyFollowerOfFolloweeMessage _handler;
+        private EventPublisherFake _eventPublisher;
+        private EventsDatabase _database;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            _database = new EventsDatabase();
+            _subscriptionRepository = new SubscriptionRepository(_database);
+            _eventPublisher = new EventPublisherFake();
+            _handler = new NotifyFollowerOfFolloweeMessage(_subscriptionRepository, _eventPublisher);
+        }
+
+        [TestMethod]
+        public void WhenMessagePublishedByFolloweeThenRaiseFollowerMessagePublished()
+        {
+            var follower = new UserId("follower@mixit.fr");
+            AddFollower(follower);
+
+            var messageId = MessageId.Generate();
+            const string content = "content";
+            _handler.Handle(new MessagePublished(messageId, Followee, content));
+
+            Check.That(_eventPublisher.Events).Contains(new FollowerMessagePublished(new SubscriptionId(follower, Followee), messageId));
+        }
+
+        private void AddFollower(UserId follower)
+        {
+            _database.Store(new UserFollowed(new SubscriptionId(follower, Followee)));
+        }
+    }
+}
