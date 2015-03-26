1d4f79b0055d90490ff7708f7e8a6e4afed77c11
fpellet <florent.pellet@gmail.com>
2015-03-26 22:40:23 +0100

    Create EventPublisherWithStorage

---
+++ Mixter.Tests/Infrastructure/EventPublisherWithStorageTest.cs
@@ -0,0 +1,47 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Mixter.Domain;
+using Mixter.Infrastructure;
+using NFluent;
+
+namespace Mixter.Tests.Infrastructure
+{
+    [TestClass]
+    public class EventPublisherWithStorageTest
+    {
+        private EventsDatabase _database;
+        private EventPublisherWithStorage _publisher;
+        private EventPublisherFake _publisherBase;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            _database = new EventsDatabase();
+            _publisherBase = new EventPublisherFake();
+            _publisher = new EventPublisherWithStorage(_database, _publisherBase);
+        }
+
+        [TestMethod]
+        public void WhenPublishEventThenStoreInDatabase()
+        {
+            _publisher.Publish(new EventA());
+
+            Check.That(_database.GetEvents()).HasSize(1);
+        }
+
+        [TestMethod]
+        public void WhenPublishEventThenCallEventHandlerBase()
+        {
+            _publisher.Publish(new EventA());
+
+            Check.That(_publisherBase.Events).HasSize(1);
+        }
+
+        private struct EventA : IDomainEvent
+        {
+            public object GetAggregateId()
+            {
+                return "A";
+            }
+        }
+    }
+}
---
+++ Mixter/Infrastructure/EventPublisherWithStorage.cs
@@ -0,0 +1,22 @@
+﻿using Mixter.Domain;
+
+namespace Mixter.Infrastructure
+{
+    public class EventPublisherWithStorage : IEventPublisher
+    {
+        private readonly EventsDatabase _database;
+        private readonly IEventPublisher _publisher;
+
+        public EventPublisherWithStorage(EventsDatabase database, IEventPublisher publisher)
+        {
+            _database = database;
+            _publisher = publisher;
+        }
+
+        public void Publish<TEvent>(TEvent evt) where TEvent : IDomainEvent
+        {
+            _database.Store(evt);
+            _publisher.Publish(evt);
+        }
+    }
+}
