897eb1ea335efdf8dd30b7d6d14b6012250d5fd3
fpellet <florent.pellet@gmail.com>
2015-03-28 12:10:05 +0100

    Implement SessionsRepository

---
+++ Mixter.Tests/Infrastructure/Repositories/SessionsRepositoryTest.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Mixter.Domain.Identity;
+using Mixter.Domain.Identity.Events;
+using Mixter.Infrastructure.Repositories;
+using NFluent;
+
+namespace Mixter.Tests.Infrastructure.Repositories
+{
+    [TestClass]
+    public class SessionsRepositoryTest
+    {
+        private SessionsRepository _repository;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            _repository = new SessionsRepository();
+        }
+
+        [TestMethod]
+        public void GivenNoProjectionsWhenGetUserIdThenReturnEmpty()
+        {
+            Check.That(_repository.GetUserIdOfSession(SessionId.Generate())).IsNull();
+        }
+
+        [TestMethod]
+        public void GivenSeveralUserConnectedWhenGetUserIdOfASessionThenReturnUserIdOfThisSession()
+        {
+            var sessionId1 = SessionId.Generate();
+            var userId1 = new UserId("user1@mixit.fr");
+            _repository.Save(new SessionProjection(new UserConnected(sessionId1, userId1, DateTime.Now)));
+            _repository.Save(new SessionProjection(new UserConnected(SessionId.Generate(), new UserId("user2@mixit.fr"), DateTime.Now)));
+
+            var userIdOfSession = _repository.GetUserIdOfSession(sessionId1);
+
+            Check.That(userIdOfSession).IsEqualTo(userId1);
+        }
+
+        [TestMethod]
+        public void GivenUserDisconnectedWhenGetUserIdOfThisSessionThenReturnEmpty()
+        {
+            var sessionId1 = SessionId.Generate();
+            var userId1 = new UserId("user1@mixit.fr");
+            _repository.Save(new SessionProjection(new UserDisconnected(sessionId1, userId1)));
+
+            var userIdOfSession = _repository.GetUserIdOfSession(sessionId1);
+
+            Check.That(userIdOfSession).IsNull();
+        }
+
+        [TestMethod]
+        public void WhenReplaceProjectionThenUpdateProjection()
+        {
+            var sessionId1 = SessionId.Generate();
+            var userId1 = new UserId("user1@mixit.fr");
+            _repository.Save(new SessionProjection(new UserConnected(sessionId1, userId1, DateTime.Now)));
+            _repository.ReplaceBy(new SessionProjection(new UserDisconnected(sessionId1, userId1)));
+
+            var userIdOfSession = _repository.GetUserIdOfSession(sessionId1);
+
+            Check.That(userIdOfSession).IsNull();
+        }
+    }
+}
--- Mixter/Infrastructure/Repositories/SessionsRepository.cs
+++ Mixter/Infrastructure/Repositories/SessionsRepository.cs
@@ -1,17 +1,33 @@
-﻿using Mixter.Domain.Identity;
+﻿using System.Collections.Generic;
+using Mixter.Domain.Identity;
 
 namespace Mixter.Infrastructure.Repositories
 {
     public class SessionsRepository : ISessionsRepository
     {
+        private readonly IDictionary<SessionId, SessionProjection> _projectionsById = new Dictionary<SessionId, SessionProjection>();
+
         public void Save(SessionProjection projection)
         {
-            throw new System.NotImplementedException();
+            _projectionsById.Add(projection.SessionId, projection);
         }
 
         public void ReplaceBy(SessionProjection projection)
         {
-            throw new System.NotImplementedException();
+            _projectionsById[projection.SessionId] = projection;
+        }
+
+        public UserId? GetUserIdOfSession(SessionId sessionId)
+        {
+            if (!_projectionsById.ContainsKey(sessionId))
+            {
+                return null;
+            }
+
+            var projectionOfSession = _projectionsById[sessionId];
+            return projectionOfSession.SessionState == SessionState.Enabled
+                ? projectionOfSession.UserId 
+                : (UserId?) null;
         }
     }
 }
