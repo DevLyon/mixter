f93857448a11331a22452a5e79923bd651669c79
Cl?ment Bouillier <clement.bouillier@devcrafting.com>
2015-04-07 00:30:41 +0200

    Disconnect a disconnected user does nothing

--- app/Domain/Identity/Session.php
+++ app/Domain/Identity/Session.php
@@ -25,6 +25,10 @@ namespace App\Domain\Identity {
 
         public function logOut(IEventPublisher $eventPublisher)
         {
+            if($this->decisionProjection->isDisconnected()) {
+                return;
+            }
+
             $eventPublisher->publish(
                 new UserDisconnected(
                     $this->decisionProjection->getUserId(),
@@ -38,6 +42,7 @@ namespace App\Domain\Identity\Session {
     use App\Domain\DecisionProjectionBase;
     use App\Domain\Identity\SessionId;
     use App\Domain\Identity\UserConnected;
+    use App\Domain\Identity\UserDisconnected;
     use App\Domain\Identity\UserId;
 
     class DecisionProjection extends DecisionProjectionBase
@@ -48,21 +53,17 @@ namespace App\Domain\Identity\Session {
         /** @var SessionId */
         private $sessionId;
 
+        /** @var bool */
+        private $disconnected;
+
         /**
          * @param array $events
          */
         public function __construct($events)
         {
             $this->registerUserConnected();
+            $this->registerUserDisconnected();
             parent::__construct($events);
-        }
-
-        private function registerUserConnected()
-        {
-            $this->register('App\Domain\Identity\UserConnected', function (UserConnected $event) {
-                $this->userId = $event->getUserId();
-                $this->sessionId = $event->getSessionId();
-            });
         }
 
         /**
@@ -79,6 +80,30 @@ namespace App\Domain\Identity\Session {
         public function getSessionId()
         {
             return $this->sessionId;
+        }
+
+        /**
+         * @return boolean
+         */
+        public function isDisconnected()
+        {
+            return $this->disconnected;
+        }
+
+        private function registerUserConnected()
+        {
+            $this->register('App\Domain\Identity\UserConnected', function (UserConnected $event) {
+                $this->userId = $event->getUserId();
+                $this->sessionId = $event->getSessionId();
+                $this->disconnected = false;
+            });
+        }
+
+        private function registerUserDisconnected()
+        {
+            $this->register('App\Domain\Identity\UserDisconnected', function (UserDisconnected $event) {
+                $this->disconnected = true;
+            });
         }
     }
 }
