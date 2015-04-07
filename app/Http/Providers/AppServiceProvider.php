15b467e48c3a3fc2f7de688163d9301c25c8a44b
Cl?ment Bouillier <clement.bouillier@devcrafting.com>
2015-04-07 09:19:05 +0200

    Append logIn route through Laravel (FileEventStore is bound as EventStore)

--- app/Http/Providers/AppServiceProvider.php
+++ app/Http/Providers/AppServiceProvider.php
@@ -11,7 +11,7 @@ class AppServiceProvider extends ServiceProvider {
 	 */
 	public function boot()
 	{
-		//
+		$this->app->bind('App\Infrastructure\IEventStore', 'App\Infrastructure\FileEventStore');
 	}
 
 	/**
