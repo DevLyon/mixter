86339c82097f8953349c4f2d35d5ba665167468b
Cl?ment Bouillier <clement.bouillier@devcrafting.com>
2015-04-07 17:54:35 +0200

    Republish a published message

--- app/Domain/Messages/Message.php
+++ app/Domain/Messages/Message.php
@@ -1,11 +1,59 @@
 <?php
 
-namespace App\Domain\Messages;
+namespace App\Domain\Messages {
 
-use App\Domain\IEventPublisher;
+    use App\Domain\IEventPublisher;
+    use App\Domain\Messages\Message\DecisionProjection;
 
-class Message {
-    public static function publish(IEventPublisher $eventPublisher, $messageContent) {
-        $eventPublisher->publish(new MessagePublished(MessageId::generate(), $messageContent));
+    class Message
+    {
+        public static function publish(IEventPublisher $eventPublisher, $messageContent)
+        {
+            $eventPublisher->publish(new MessagePublished(MessageId::generate(), $messageContent));
+        }
+
+        public function __construct($events)
+        {
+            $this->decisionProjection = new DecisionProjection($events);
+        }
+
+        public function republish(IEventPublisher $eventPublisher)
+        {
+            $eventPublisher->publish(new MessageRepublished($this->decisionProjection->getMessageId()));
+        }
+    }
+}
+
+namespace App\Domain\Messages\Message {
+
+    use App\Domain\DecisionProjectionBase;
+    use App\Domain\Messages\MessageId;
+    use App\Domain\Messages\MessagePublished;
+
+    class DecisionProjection extends DecisionProjectionBase
+    {
+        /** @var MessageId */
+        private $messageId;
+
+        public function __construct($events)
+        {
+            $this->registerMessagePublished();
+            parent::__construct($events);
+        }
+
+        /**
+         * @return MessageId
+         */
+        public function getMessageId()
+        {
+            return $this->messageId;
+        }
+
+        private function registerMessagePublished()
+        {
+            $this->register('App\Domain\Messages\MessagePublished', function (MessagePublished $event) {
+                $this->messageId = $event->getMessageId();
+            });
+        }
     }
 }
