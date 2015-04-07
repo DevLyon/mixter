726052fabd3b4d8772514ad9eeffb68894ef7d8a
Cl?ment Bouillier <clement.bouillier@devcrafting.com>
2015-04-07 18:18:24 +0200

    When republisher try to republish again nothing happens

--- app/Domain/Messages/Message.php
+++ app/Domain/Messages/Message.php
@@ -20,7 +20,8 @@ namespace App\Domain\Messages {
 
         public function republish(IEventPublisher $eventPublisher, UserId $republisherId)
         {
-            if($republisherId == $this->decisionProjection->getAuthorId()) {
+            if($republisherId == $this->decisionProjection->getAuthorId()
+                || in_array($republisherId, $this->decisionProjection->getRepublishers())) {
                 return;
             }
             $eventPublisher->publish(
@@ -35,6 +36,7 @@ namespace App\Domain\Messages\Message {
     use App\Domain\Identity\UserId;
     use App\Domain\Messages\MessageId;
     use App\Domain\Messages\MessagePublished;
+    use App\Domain\Messages\MessageRepublished;
 
     class DecisionProjection extends DecisionProjectionBase
     {
@@ -44,9 +46,13 @@ namespace App\Domain\Messages\Message {
         /** @var UserId */
         private $authorId;
 
+        /** @var array */
+        private $republishers = array();
+
         public function __construct($events)
         {
             $this->registerMessagePublished();
+            $this->registerMessageRepublished();
             parent::__construct($events);
         }
 
@@ -66,11 +72,26 @@ namespace App\Domain\Messages\Message {
             return $this->authorId;
         }
 
+        /**
+         * @return array
+         */
+        public function getRepublishers()
+        {
+            return $this->republishers;
+        }
+
         private function registerMessagePublished()
         {
             $this->register('App\Domain\Messages\MessagePublished', function (MessagePublished $event) {
                 $this->messageId = $event->getMessageId();
                 $this->authorId = $event->getAuthorId();
+            });
+        }
+
+        private function registerMessageRepublished()
+        {
+            $this->register('App\Domain\Messages\MessageRepublished', function (MessageRepublished $event) {
+                $this->republishers[] = $event->getRepublisherId();
             });
         }
     }
