0639845d42471dd7a6a46ec4b0f573fdf0eaa132
Jean Helou <jean.helou@gmail.com>
2015-04-08 23:54:20 +0200

    [Subscription][Refactoring] use setup method and constants to make tests more readable

--- src/test/java/mixter/domain/subscription/SubscriptionTest.java
+++ src/test/java/mixter/domain/subscription/SubscriptionTest.java
@@ -5,42 +5,50 @@ import mixter.Event;
 import mixter.UserId;
 import mixter.domain.subscription.events.UserFollowed;
 import mixter.domain.subscription.events.UserUnfollowed;
+import org.junit.Before;
 import org.junit.Test;
-
-import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class SubscriptionTest extends AggregateTest {
+
+    private SpyEventPublisher eventPublisher;
+    public static final UserId FOLLOWER = new UserId();
+    public static final UserId FOLLOWEE = new UserId();
+
+    @Before
+    public void setUp() throws Exception {
+        eventPublisher = new SpyEventPublisher();
+    }
+
     @Test
     public void WhenAUserFollowsAnotherUserFollowedIsRaised() throws Exception {
         //Given
-        UserId follower = new UserId();
-        UserId followee = new UserId();
 
-        SpyEventPublisher eventPublisher = new SpyEventPublisher();
         //When
-        Subscription.follow(follower, followee, eventPublisher);
+        Subscription.follow(FOLLOWER, FOLLOWEE, eventPublisher);
 
         //Then
-        UserFollowed userFollowed = new UserFollowed(new SubscriptionId(follower, followee));
+        UserFollowed userFollowed = new UserFollowed(new SubscriptionId(FOLLOWER, FOLLOWEE));
         assertThat(eventPublisher.publishedEvents).containsExactly(userFollowed);
     }
 
     @Test
     public void WhenAUserUnfollowsAnotherUserUnfollowedIsRaised() throws Exception {
         //Given
-        UserId follower = new UserId();
-        UserId followee = new UserId();
-        SubscriptionId subscriptionId = new SubscriptionId(follower, followee);
-        List<Event> events = history(new UserFollowed(subscriptionId));
-        SpyEventPublisher eventPublisher = new SpyEventPublisher();
-        Subscription subscription = new Subscription(events);
+        SubscriptionId subscriptionId = new SubscriptionId(FOLLOWER, FOLLOWEE);
+        Subscription subscription = subscriptionFor(
+                new UserFollowed(subscriptionId)
+        );
         //When
         subscription.unfollow(eventPublisher);
 
         //Then
         UserUnfollowed userUnfollowed = new UserUnfollowed(subscriptionId);
         assertThat(eventPublisher.publishedEvents).containsExactly(userUnfollowed);
+    }
+
+    Subscription subscriptionFor(Event... events) {
+        return new Subscription(history(events));
     }
 }
