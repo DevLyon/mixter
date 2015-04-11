581cb0b80f73b7bcf37b1f5adb8dcc6189013f01
fpellet <florent.pellet@gmail.com>
2015-04-11 15:47:53 +0200

    [Core][UpdateTimeline][Test OK] When handle MessageQuacked Then save TimelineMessageProjection for author

--- src/domain/core/updateTimeline.js
+++ src/domain/core/updateTimeline.js
@@ -1,7 +1,18 @@
+var message = require('./message');
+var timelineMessageProjection = require('./timelineMessageProjection');
+
 var UpdateTimeline = function UpdateTimeline(timelineMessageRepository){
     var self = this;
 
+    var saveProjection = function(owner, author, content, messageId){
+        var projection = timelineMessageProjection.create(owner, author, content, messageId);
+        timelineMessageRepository.save(projection);
+    };
+
     self.register = function(eventPublisher) {
+        eventPublisher.on(message.MessageQuacked, function(event){
+            saveProjection(event.author, event.author, event.content, event.messageId);
+        });
     };
 };
 
