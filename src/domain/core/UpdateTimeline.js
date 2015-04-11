d21bf149356a3c5ceb2e1948cb711f9bf4493699
fpellet <florent.pellet@gmail.com>
2015-04-11 15:47:53 +0200

    [Core][UpdateTimeline][Test OK] When handle MessagePublished Then save TimelineMessageProjection for author

--- src/domain/core/UpdateTimeline.js
+++ src/domain/core/UpdateTimeline.js
@@ -1,7 +1,18 @@
+var Message = require('./Message');
+var TimelineMessageProjection = require('./TimelineMessageProjection');
+
 var UpdateTimeline = function UpdateTimeline(timelineMessageRepository){
     var self = this;
 
+    var saveProjection = function(owner, author, content, messageId){
+        var projection = TimelineMessageProjection.create(owner, author, content, messageId);
+        timelineMessageRepository.save(projection);
+    };
+
     self.register = function(eventPublisher) {
+        eventPublisher.on(Message.MessagePublished, function(event){
+            saveProjection(event.author, event.author, event.content, event.messageId);
+        });
     };
 };
 
