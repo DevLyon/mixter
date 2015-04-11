7ae6069bb5daa3cd9c275721e835da0a0e74d357
fpellet <florent.pellet@gmail.com>
2015-04-11 16:28:25 +0200

    [Core][Subscription] Create SubscriptionId and UserFollowed

---
+++ src/domain/core/Subscription.js
@@ -0,0 +1,20 @@
+var valueType = require('../../valueType');
+
+var SubscriptionId = exports.SubscriptionId = valueType.extends(function SubscriptionId(follower, followee){
+    this.follower = follower;
+    this.followee = followee;
+
+    Object.freeze(this);
+}, function toString() {
+    return 'Subscription:' + this.follower + ' -> ' + this.followee;
+});
+
+var UserFollowed = exports.UserFollowed = function UserFollowed(subscriptionId){
+    this.subscriptionId = subscriptionId;
+
+    Object.freeze(this);
+};
+
+UserFollowed.prototype.getAggregateId = function getAggregateId(){
+    return this.subscriptionId;
+};
---
+++ test/domain/core/Subscription.js
@@ -0,0 +1,28 @@
+var Subscription = require('../../../src/domain/core/Subscription');
+var UserId = require('../../../src/domain/UserId').UserId;
+var expect = require('chai').expect;
+
+describe('Subscription Aggregate', function() {
+    var follower = new UserId('follower@mix-it.fr');
+    var followee = new UserId('followee@mix-it.fr');
+    var subscriptionId = new Subscription.SubscriptionId(follower, followee);
+
+    var eventsRaised = [];
+    var publishEvent = function publishEvent(evt) {
+        eventsRaised.push(evt);
+    };
+
+    beforeEach(function () {
+        eventsRaised = [];
+    });
+
+    it('When create SubscriptionId Then toString return follower and followee', function() {
+        expect(subscriptionId.toString()).to.equal('Subscription:' + follower + ' -> ' + followee);
+    });
+
+    it('When create UserFollowed Then aggregateId is subscriptionId', function() {
+        var event = new Subscription.UserFollowed(subscriptionId);
+
+        expect(event.getAggregateId()).to.equal(subscriptionId);
+    });
+});
