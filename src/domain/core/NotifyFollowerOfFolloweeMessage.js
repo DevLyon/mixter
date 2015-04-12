414b58f368d638064d78bd5053d928554b6417ac
fpellet <florent.pellet@gmail.com>
2015-04-12 19:29:57 +0200

    [Core][NotifyFollowerOfFolloweeMessage][Test OK] When ReplyMessagePublished by followee Then raise FolloweeMessagePublished

--- src/domain/core/NotifyFollowerOfFolloweeMessage.js
+++ src/domain/core/NotifyFollowerOfFolloweeMessage.js
@@ -3,11 +3,17 @@ var Message = require('./Message');
 var NotifyFollowerOfFolloweeMessage = function NotifyFollowerOfFolloweeMessage(subscriptionsRepository){
     var self = this;
 
+    var notifyFollowers = function notifyFollowers(eventPublisher, author, messageId){
+        subscriptionsRepository.getSubscriptionsOfUser(author).forEach(function(subscription) {
+            subscription.notifyFollower(eventPublisher.publish, messageId);
+        });
+    };
+
     self.register = function register(eventPublisher) {
         eventPublisher.on(Message.MessagePublished, function(event){
-            subscriptionsRepository.getSubscriptionsOfUser(event.author).forEach(function(subscription) {
-                subscription.notifyFollower(eventPublisher.publish, event.messageId);
-            });
+            notifyFollowers(eventPublisher, event.author, event.messageId);
+        }).on(Message.ReplyMessagePublished, function(event){
+            notifyFollowers(eventPublisher, event.replier, event.replyId);
         });
     };
 };
