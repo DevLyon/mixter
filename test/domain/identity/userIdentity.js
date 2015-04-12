0e52ef65f9b9a6248ec0724a7b4728211bcdfd3b
fpellet <florent.pellet@gmail.com>
2015-04-12 10:47:07 +0200

    [Identity][UserIdentity] When register user Then raise userRegistered event

---
+++ test/domain/identity/userIdentity.js
@@ -0,0 +1,24 @@
+var userIdentity = require('../../../src/domain/identity/userIdentity');
+var expect = require('chai').expect;
+
+describe('User Identity Aggregate', function() {
+    var email = 'user@mix-it.fr';
+
+    var eventsRaised = [];
+    var publishEvent = function publishEvent(evt){
+        eventsRaised.push(evt);
+    };
+
+    it('When create UserIdentityId Then toString return email', function() {
+        var id = new userIdentity.UserIdentityId(email);
+
+        expect(id.toString()).to.eql(('UserIdentity:' + email));
+    });
+
+    it('When register user Then raise userRegistered event', function() {
+        userIdentity.register(publishEvent, email);
+
+        var expectedEvent = new userIdentity.UserRegistered(new userIdentity.UserIdentityId(email));
+        expect(eventsRaised).to.contains(expectedEvent);
+    });
+});
