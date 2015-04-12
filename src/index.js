4923a90f50db92eb76f0a7b860461bf682af3150
fpellet <florent.pellet@gmail.com>
2015-04-13 00:21:56 +0200

    [Api] Initialize REST api

--- src/index.js
+++ src/index.js
@@ -1,3 +1,36 @@
-exports.run = function run(){
-  return 'ok';
+var express = require('express');
+var http = require('http');
+var bodyParser = require('body-parser');
+
+var createExpressMiddleware = function(port){
+    var app = express();
+    app.set('port', port);
+
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({
+        extended: false
+    }));
+
+    return app;
+};
+
+var registerRoutes = function(app){
+    app.get('/api/ping', function(req, res){
+        res.status(200).send('pong');
+    });
+};
+
+var startServer = function(app){
+    var server = http.createServer(app);
+    server.listen(app.get('port'), function(){
+        console.log('Express server listening on port ' + app.get('port'));
+    });
+};
+
+exports.run = function run(port){
+    var app = createExpressMiddleware(port);
+
+    registerRoutes(app);
+
+    startServer(app);
 };
