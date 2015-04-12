4607246546dcb3c6f1553a0c50be71a61cbadc1a
fpellet <florent.pellet@gmail.com>
2015-04-13 00:33:58 +0200

    [Api] Add wrapper to manage errors

--- src/routes.js
+++ src/routes.js
@@ -25,6 +25,30 @@ var registerUser = function registerUser(req, res) {
     });
 };
 
+var manageError = function manageError(action){
+    return function(req, res){
+        try {
+            action(req, res);
+        } catch(e){
+            if(e.constructor) {
+                var errorName = e.constructor.name;
+
+                console.log('error: ' + errorName);
+                console.log(e);
+
+                res.status(400).send({
+                    errorName: errorName,
+                    error: e
+                });
+
+                return;
+            }
+
+            throw e;
+        }
+    };
+};
+
 exports.registerRoutes = function registerRoutes(app){
-    app.post('/api/identity/userIdentities/register', registerUser);
+    app.post('/api/identity/userIdentities/register', manageError(registerUser));
 };
