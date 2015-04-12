3e06d4e91bb54a2f5d0d42ce279e71cbfce49962
fpellet <florent.pellet@gmail.com>
2015-04-12 10:58:24 +0200

    [Identity][UserIdentity] Given UserRegistered When log in Then raise UserConnected event

--- test/domain/identity/userIdentity.js
+++ test/domain/identity/userIdentity.js
@@ -4,8 +4,9 @@ var expect = require('chai').expect;
 describe('User Identity Aggregate', function() {
     var email = 'user@mix-it.fr';
 
-    var eventsRaised = [];
+    var eventsRaised;
     var publishEvent = function publishEvent(evt){
+        eventsRaised = [];
         eventsRaised.push(evt);
     };
 
@@ -20,5 +21,19 @@ describe('User Identity Aggregate', function() {
 
         var expectedEvent = new userIdentity.UserRegistered(new userIdentity.UserIdentityId(email));
         expect(eventsRaised).to.contains(expectedEvent);
+    });
+
+    it('Given UserRegistered When log in Then raise UserConnected event', function(){
+        var id = new userIdentity.UserIdentityId(email);
+        var user = userIdentity.create([ new userIdentity.UserRegistered(id) ]);
+
+        user.logIn(publishEvent);
+
+        expect(eventsRaised).to.have.length(1);
+        var event = eventsRaised[0];
+        expect(event).to.be.an.instanceof(userIdentity.UserConnected);
+        expect(event.userIdentityId).to.equal(id);
+        expect(event.connectedAt - new Date()).to.within(-1, 1);
+        expect(event.sessionId).not.to.be.empty;
     });
 });
