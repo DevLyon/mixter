c25453f2bac75cbed65170011837c632301eb1db
fpellet <florent.pellet@gmail.com>
2015-04-12 19:16:02 +0200

    [Infra][SubscriptionsRepository] When getSubscriptionsOfUser Then return all Subscription aggregates of user

--- src/infrastructure/subscriptionsRepository.js
+++ src/infrastructure/subscriptionsRepository.js
@@ -1,11 +1,14 @@
 var subscription = require('../domain/core/subscription');
+var _ = require('lodash');
 
 var UnknownSubscription = exports.UnknownSubscription = function UnknownSubscription(subscriptionId){
     this.subscriptionId = subscriptionId;
 };
 
-var SubscriptionsRepository = function SubscriptionsRepository(eventsStore) {
-    this.getSubscription = function getSubscription(subscriptionId){
+var SubscriptionsRepository = function SubscriptionsRepository(eventsStore, followersRepository) {
+    var self = this;
+
+    self.getSubscription = function getSubscription(subscriptionId){
         var events = eventsStore.getEventsOfAggregate(subscriptionId);
         if(!events.length){
             throw new UnknownSubscription(subscriptionId);
@@ -13,8 +16,16 @@ var SubscriptionsRepository = function SubscriptionsRepository(eventsStore) {
 
         return subscription.create(events);
     };
+
+    self.getSubscriptionsOfUser = function getSubscriptionsOfUser(userId){
+        return _(followersRepository.getFollowers(userId)).map(function(follower) {
+            return new subscription.SubscriptionId(follower, userId);
+        }).map(function(subscriptionId){
+            return self.getSubscription(subscriptionId);
+        }).value();
+    };
 };
 
-exports.create = function create(eventsStore){
-    return new SubscriptionsRepository(eventsStore);
+exports.create = function create(eventsStore, followersRepository){
+    return new SubscriptionsRepository(eventsStore, followersRepository);
 };
--- test/infrastructure/subscriptionsRepository.js
+++ test/infrastructure/subscriptionsRepository.js
@@ -1,6 +1,8 @@
 var createEventsStore = require('../../src/infrastructure/eventsStore').create;
-var subscriptionsRepository = require('../../src/infrastructure/subscriptionsRepository');
+var createSubscriptionsRepository = require('../../src/infrastructure/subscriptionsRepository').create;
 var subscription = require('../../src/domain/core/subscription');
+var createFollowersRepository = require('../../src/infrastructure/followersRepository').create;
+var followerProjection = require('../../src/domain/core/followerProjection');
 var SubscriptionId = subscription.SubscriptionId;
 var UserId = require('../../src/domain/userId').UserId;
 var expect = require('chai').expect;
@@ -9,17 +11,19 @@ describe('Subscriptions Repository', function() {
     var subscriptionId = new SubscriptionId(new UserId('follower@mix-it.fr'), new UserId('followee@mix-it.fr'));
     var userFollowed = new subscription.UserFollowed(subscriptionId);
 
-    var repository;
+    var subscriptionsRepository;
+    var followersRepository;
     var eventsStore;
     beforeEach(function(){
         eventsStore = createEventsStore();
-        repository = subscriptionsRepository.create(eventsStore);
+        followersRepository = createFollowersRepository();
+        subscriptionsRepository = createSubscriptionsRepository(eventsStore, followersRepository);
     });
 
     it('Given UserFollowed When getSubscription Then return subscription aggregate', function(done) {
         eventsStore.store(userFollowed);
 
-        var subscription = repository.getSubscription(subscriptionId);
+        var subscription = subscriptionsRepository.getSubscription(subscriptionId);
 
         subscription.unfollow(function(event){
             expect(event.subscriptionId).to.eql(subscriptionId);
@@ -30,7 +34,21 @@ describe('Subscriptions Repository', function() {
 
     it('Given no events When getSubscription Then throw UnknownSubscription', function() {
         expect(function () {
-            repository.getSubscription(new SubscriptionId(new UserId('badfollower@mix-it.fr'), new UserId('badfollowee@mix-it.fr')));
+            subscriptionsRepository.getSubscription(new SubscriptionId(new UserId('badfollower@mix-it.fr'), new UserId('badfollowee@mix-it.fr')));
         }).to.throw(subscriptionsRepository.UnknownSubscription);
+    });
+
+    it('When getSubscriptionsOfUser Then return all Subscription aggregates of user', function() {
+        var followee = new UserId('followee@mix-it.fr');
+        var follower1 = new UserId('follower1@mix-it.fr');
+        var follower2 = new UserId('follower2@mix-it.fr');
+        followersRepository.save(followerProjection.create(followee, follower1));
+        followersRepository.save(followerProjection.create(followee, follower2));
+        eventsStore.store(new subscription.UserFollowed(new SubscriptionId(follower1, followee)));
+        eventsStore.store(new subscription.UserFollowed(new SubscriptionId(follower2, followee)));
+
+        var subscriptions = subscriptionsRepository.getSubscriptionsOfUser(followee);
+
+        expect(subscriptions).to.have.length(2);
     });
 });
