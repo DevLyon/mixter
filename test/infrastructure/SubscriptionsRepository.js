eb87e8cba9ab436f546fb98530968fe2b04c6ce4
fpellet <florent.pellet@gmail.com>
2015-04-12 19:16:02 +0200

    [Infra][SubscriptionsRepository] When getSubscriptionsOfUser Then return all Subscription aggregates of user

--- test/infrastructure/SubscriptionsRepository.js
+++ test/infrastructure/SubscriptionsRepository.js
@@ -1,6 +1,8 @@
 var EventsStore = require('../../src/infrastructure/EventsStore');
 var SubscriptionsRepository = require('../../src/infrastructure/SubscriptionsRepository');
 var Subscription = require('../../src/domain/core/Subscription');
+var FollowersRepository = require('../../src/infrastructure/FollowersRepository');
+var FollowerProjection = require('../../src/domain/core/FollowerProjection');
 var SubscriptionId = Subscription.SubscriptionId;
 var UserId = require('../../src/domain/UserId').UserId;
 var expect = require('chai').expect;
@@ -9,17 +11,19 @@ describe('Subscriptions Repository', function() {
     var subscriptionId = new SubscriptionId(new UserId('follower@mix-it.fr'), new UserId('followee@mix-it.fr'));
     var userFollowed = new Subscription.UserFollowed(subscriptionId);
 
-    var repository;
+    var subscriptionsRepository;
+    var followersRepository;
     var eventsStore;
     beforeEach(function(){
         eventsStore = EventsStore.create();
-        repository = SubscriptionsRepository.create(eventsStore);
+        followersRepository = FollowersRepository.create();
+        subscriptionsRepository = SubscriptionsRepository.create(eventsStore, followersRepository);
     });
 
     it('Given UserFollowed When getSubscription Then return Subscription aggregate', function(done) {
         eventsStore.store(userFollowed);
 
-        var subscription = repository.getSubscription(subscriptionId);
+        var subscription = subscriptionsRepository.getSubscription(subscriptionId);
 
         subscription.unfollow(function(event){
             expect(event.subscriptionId).to.eql(subscriptionId);
@@ -30,7 +34,21 @@ describe('Subscriptions Repository', function() {
 
     it('Given no events When getSubscription Then throw UnknownSubscription', function() {
         expect(function () {
-            repository.getSubscription(new SubscriptionId(new UserId('badfollower@mix-it.fr'), new UserId('badfollowee@mix-it.fr')));
+            subscriptionsRepository.getSubscription(new SubscriptionId(new UserId('badfollower@mix-it.fr'), new UserId('badfollowee@mix-it.fr')));
         }).to.throw(SubscriptionsRepository.UnknownSubscription);
+    });
+
+    it('When getSubscriptionsOfUser Then return all Subscription aggregates of user', function() {
+        var followee = new UserId('followee@mix-it.fr');
+        var follower1 = new UserId('follower1@mix-it.fr');
+        var follower2 = new UserId('follower2@mix-it.fr');
+        followersRepository.save(FollowerProjection.create(followee, follower1));
+        followersRepository.save(FollowerProjection.create(followee, follower2));
+        eventsStore.store(new Subscription.UserFollowed(new Subscription.SubscriptionId(follower1, followee)));
+        eventsStore.store(new Subscription.UserFollowed(new Subscription.SubscriptionId(follower2, followee)));
+
+        var subscriptions = subscriptionsRepository.getSubscriptionsOfUser(followee);
+
+        expect(subscriptions).to.have.length(2);
     });
 });
