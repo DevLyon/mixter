2ee058a9a72ccfe6995e54c6c96571aeee572be3
fpellet <florent.pellet@gmail.com>
2015-04-12 21:01:41 +0200

    [Infra][SessionsRepository] Add getSession

--- test/infrastructure/SessionsRepository.js
+++ test/infrastructure/SessionsRepository.js
@@ -1,3 +1,4 @@
+var EventsStore = require('../../src/infrastructure/EventsStore');
 var SessionsRepository = require('../../src/infrastructure/SessionsRepository');
 var SessionProjection = require('../../src/domain/Identity/SessionProjection');
 var Session = require('../../src/domain/Identity/Session');
@@ -8,9 +9,11 @@ describe('Sessions Repository', function() {
     var sessionId = new Session.SessionId('SessionA');
     var userId = new UserId('user1@mix-it.fr');
 
+    var eventsStore;
     var repository;
     beforeEach(function(){
-        repository = SessionsRepository.create();
+        eventsStore = EventsStore.create();
+        repository = SessionsRepository.create(eventsStore);
     });
 
     it('Given no projections When getUserIdOfSession Then return empty', function() {
@@ -41,5 +44,20 @@ describe('Sessions Repository', function() {
         repository.save(new SessionProjection.create(sessionId, userId, SessionProjection.SessionDisabled));
 
         expect(repository.getUserIdOfSession(sessionId)).to.be.null;
+    });
+
+    it('Given UserConnected When getSession Then return Session aggregate', function() {
+        var userConnected = new Session.UserConnected(sessionId, userId, new Date());
+        eventsStore.store(userConnected);
+
+        var session = repository.getSession(userConnected.sessionId);
+
+        expect(session).not.to.empty;
+    });
+
+    it('Given no events When getSession Then throw UnknownSession', function() {
+        expect(function () {
+            repository.getSession(sessionId);
+        }).to.throw(SessionsRepository.UnknownSession);
     });
 });
