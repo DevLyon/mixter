7126ef4d76b9368698c4038d0adbadf8e6aa0496
fpellet <florent.pellet@gmail.com>
2015-04-12 12:17:32 +0200

    [Infra][SessionsRepository] Given already projection When save same projection Then update projection

--- src/infrastructure/sessionsRepository.js
+++ src/infrastructure/sessionsRepository.js
@@ -3,18 +3,19 @@ var _ = require('lodash');
 var SessionsRepository = function SessionsRepository(){
     var self = this;
 
-    var projections = [];
+    var projections = {};
 
     self.getUserIdOfSession = function getUserIdOfSession(sessionId){
-        var projection = _.find(projections, function(projection) {
-            return projection.sessionId === sessionId && projection.isEnabled;
-        });
+        var projection = projections[sessionId];
+        if(!projection || !projection.isEnabled){
+            return null;
+        }
 
-        return projection ? projection.userId : null;
+        return projection.userId;
     };
 
     self.save = function save(projection) {
-        projections.push(projection);
+        projections[projection.sessionId] = projection;
     };
 };
 
