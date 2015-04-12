066bc35294246e11419a262154750274695b19d7
fpellet <florent.pellet@gmail.com>
2015-04-12 19:19:29 +0200

    [Core][UpdateFollowers][Test OK] When UserFollowed Then save follower

--- src/domain/core/updateFollowers.js
+++ src/domain/core/updateFollowers.js
@@ -1,7 +1,13 @@
+var subscription = require('./subscription');
+var followerProjection = require('./followerProjection');
+
 var UpdateFollowers = function UpdateFollowers(followersRepository){
     var self = this;
 
     self.register = function register(eventPublisher) {
+        eventPublisher.on(subscription.UserFollowed, function(event){
+            followersRepository.save(followerProjection.create(event.subscriptionId.followee, event.subscriptionId.follower));
+        });
     };
 };
 
