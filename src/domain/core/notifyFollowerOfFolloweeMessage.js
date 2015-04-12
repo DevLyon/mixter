52178aa2d12f16990334ba756eb7949043df6060
fpellet <florent.pellet@gmail.com>
2015-04-12 19:25:34 +0200

    [Core][NotifyFollowerOfFolloweeMessage][Test OK] Given follower When MessageQuacked by followee Then raise FolloweeMessageQuacked

--- src/domain/core/notifyFollowerOfFolloweeMessage.js
+++ src/domain/core/notifyFollowerOfFolloweeMessage.js
@@ -1,7 +1,14 @@
+var message = require('./message');
+
 var NotifyFollowerOfFolloweeMessage = function NotifyFollowerOfFolloweeMessage(subscriptionsRepository){
     var self = this;
 
     self.register = function register(eventPublisher) {
+        eventPublisher.on(message.MessageQuacked, function(event){
+            subscriptionsRepository.getSubscriptionsOfUser(event.author).forEach(function(subscription) {
+                subscription.notifyFollower(eventPublisher.publish, event.messageId);
+            });
+        });
     };
 };
 
