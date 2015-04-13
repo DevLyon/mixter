a5f0effec7cb5ec4fd57948cddad7a72cd170a43
Cl?ment Bouillier <clement.bouillier@devcrafting.com>
2015-04-13 15:03:13 +0200

    remove follower projection on UserUnfollowed

--- app/Domain/Subscriptions/UpdateFollowers.php
+++ app/Domain/Subscriptions/UpdateFollowers.php
@@ -14,9 +14,19 @@ class UpdateFollowers
         $this->followerProjectionRepository = $followerProjectionRepository;
     }
 
-    public function handle(UserFollowed $userFollowed)
+    public function handleUserFollowed(UserFollowed $userFollowed)
     {
         $this->followerProjectionRepository->save(
             new FollowerProjection($userFollowed->getSubscriptionId()->getFollowerId(), $userFollowed->getSubscriptionId()->getFolloweeId()));
+    }
+
+    public function handleUserUnfollowed(UserUnfollowed $userUnfollowed)
+    {
+        /** @var FollowerProjection $followerProjection */
+        foreach($this->followerProjectionRepository->getFollowersOf($userUnfollowed->getSubscriptionId()->getFolloweeId()) as $followerProjection){
+            if($followerProjection->getFollowerId() == $userUnfollowed->getSubscriptionId()->getFollowerId()) {
+                $this->followerProjectionRepository->remove($followerProjection);
+            }
+        }
     }
 }
