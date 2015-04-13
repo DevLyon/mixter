696ef5f09a1ec677eb0dfa6efc9d34c471677d91
Cl?ment Bouillier <clement.bouillier@devcrafting.com>
2015-04-13 12:05:24 +0200

    Get followers (FollowerProjection) from repository by followeeId returns only followers of the followee

---
+++ app/Domain/Subscriptions/IFollowerProjectionRepository.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Domain\Subscriptions;
+
+use App\Domain\Identity\UserId;
+
+interface IFollowerProjectionRepository
+{
+    /**
+     * @param UserId $followeeId
+     * @return array of FollowerProjection
+     */
+    public function getFollowersOf(UserId $followeeId);
+}
---
+++ app/Infrastructure/Subscriptions/FollowerProjectionRepository.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Infrastructure\Subscriptions;
+
+use App\Domain\Identity\UserId;
+use App\Domain\Subscriptions\FollowerProjection;
+use App\Domain\Subscriptions\IFollowerProjectionRepository;
+use App\Infrastructure\IProjectionStore;
+
+class FollowerProjectionRepository implements IFollowerProjectionRepository
+{
+    /**
+     * @var IProjectionStore
+     */
+    private $projectionStore;
+
+    public function __construct(IProjectionStore $projectionStore)
+    {
+        $this->projectionStore = $projectionStore;
+    }
+
+    public function getFollowersOf(UserId $followeeId)
+    {
+        return array_filter(
+            $this->projectionStore->getAll('App\Domain\Subscriptions\FollowerProjection'),
+            function(FollowerProjection $item) use($followeeId) {
+                return $item->getFolloweeId() == $followeeId;
+            });
+    }
+}
