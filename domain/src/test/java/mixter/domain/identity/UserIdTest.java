185a86e7f5b26dfb46dd91f9dd108422b798d44f
Jean Helou <jean.helou@gmail.com>
2015-04-14 21:25:11 +0200

    [Identity][Failing] Two UserId created from the same email should be equal

--- domain/src/test/java/mixter/domain/identity/UserIdTest.java
+++ domain/src/test/java/mixter/domain/identity/UserIdTest.java
@@ -2,6 +2,8 @@ package mixter.domain.identity;
 
 import org.junit.Test;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class UserIdTest {
 
     public static final String AN_EMAIL = "mail@mix-it.fr";
@@ -9,5 +11,12 @@ public class UserIdTest {
     @Test
     public void GivenAnEmailWhenCreatingAUserIdThenAUserIdShouldBeCreate() throws Exception {
         UserId userId1 = new UserId(AN_EMAIL);
+    }
+    @Test
+    public void Given2UserIdCreatedFromTheSameEmailWhenComparedWithEqualsThenTheyShouldBeEqual() throws Exception {
+        UserId userId1 = new UserId(AN_EMAIL);
+        UserId userId2 = new UserId(AN_EMAIL);
+
+        assertThat(userId1).isEqualTo(userId2);
     }
 }
