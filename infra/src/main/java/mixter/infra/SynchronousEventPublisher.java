7654f17910ebe3579724f203018b6d881c9a055f
Jean Helou <jean.helou@gmail.com>
2015-04-14 23:31:59 +0200

    [Infra][Passing] Given a synchronous event publisher with 2 registered handlers, when a message is published, then it calls both registered handlers

--- infra/src/main/java/mixter/infra/SynchronousEventPublisher.java
+++ infra/src/main/java/mixter/infra/SynchronousEventPublisher.java
@@ -3,17 +3,19 @@ package mixter.infra;
 import mixter.domain.Event;
 import mixter.domain.EventPublisher;
 
+import java.util.HashSet;
+import java.util.Set;
 import java.util.function.Consumer;
 
 public class SynchronousEventPublisher implements EventPublisher {
-    private Consumer handler;
+    private Set<Consumer> handlers = new HashSet<>();
 
     public <T> void register(Class<T> eventClass, Consumer<T> handler) {
-        this.handler = handler;
+        this.handlers.add(handler);
     }
 
     @SuppressWarnings("unchecked")
     public void publish(Event event) {
-        handler.accept(event);
+        handlers.forEach(handler -> handler.accept(event));
     }
 }
