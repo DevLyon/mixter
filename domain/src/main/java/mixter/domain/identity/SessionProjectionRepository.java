149c993844e1685d1ff64ae9e7ded8b124607597
Jean Helou <jean.helou@gmail.com>
2015-04-14 23:55:18 +0200

    [Infra][Passing] Given an in memory SessionProjectionRepository, when getting a session project by Id, it should return an empty option

--- domain/src/main/java/mixter/domain/identity/SessionProjectionRepository.java
+++ domain/src/main/java/mixter/domain/identity/SessionProjectionRepository.java
@@ -2,9 +2,13 @@ package mixter.domain.identity;
 
 import mixter.doc.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface SessionProjectionRepository {
     void save(SessionProjection sessionProjection);
 
     void replaceBy(SessionProjection sessionProjection);
+
+    Optional<SessionProjection> getById(SessionId sessionId);
 }
