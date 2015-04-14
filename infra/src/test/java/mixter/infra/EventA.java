7ea7c2058840906241db5375c03bc64a34cc87af
Jean Helou <jean.helou@gmail.com>
2015-04-14 23:17:02 +0200

    [Infra][Failing] Given an event store having stored events for 3 aggregates, when reading events for 1 aggregate, then it retuns events for the 1 aggregate

--- infra/src/test/java/mixter/infra/EventA.java
+++ infra/src/test/java/mixter/infra/EventA.java
@@ -4,7 +4,14 @@ import mixter.domain.AggregateId;
 import mixter.domain.Event;
 
 public class EventA implements Event {
-    public EventA(AggregateId aggregateId) {
+    private AggregateId aggregateId;
 
+    public EventA(AggregateId aggregateId) {
+        this.aggregateId = aggregateId;
+    }
+
+    @Override
+    public AggregateId getId() {
+        return aggregateId;
     }
 }
