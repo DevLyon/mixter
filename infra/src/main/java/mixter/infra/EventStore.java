5b8e3d603bf759a2bc85ee09e9a11c1136182c5e
Jean Helou <jean.helou@gmail.com>
2015-04-14 23:07:50 +0200

    [Infra][Passing] Given an empty InMemory event store, when reading events of an aggregate, an empty list is returned

---
+++ infra/src/main/java/mixter/infra/EventStore.java
@@ -0,0 +1,10 @@
+package mixter.infra;
+
+import mixter.domain.AggregateId;
+import mixter.domain.Event;
+
+import java.util.List;
+
+public interface EventStore {
+    List<Event> getEventsOfAggregate(AggregateId aggregateId);
+}
