b913fbf0e3180b3c2461e3069242c4d2af878800
Jean Helou <jean.helou@gmail.com>
2015-04-14 22:00:31 +0200

    [Identity][Passing] Given 2 SessionIds created from the same string, they should be equal

--- domain/src/main/java/mixter/domain/identity/SessionId.java
+++ domain/src/main/java/mixter/domain/identity/SessionId.java
@@ -1,7 +1,32 @@
 package mixter.domain.identity;
 
+import java.util.UUID;
+
 public class SessionId {
+    private String value;
+
+    public SessionId(String value) {
+
+        this.value = value;
+    }
+
     public static SessionId generate() {
-        return new SessionId();
+        return new SessionId(UUID.randomUUID().toString());
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        SessionId sessionId = (SessionId) o;
+
+        return !(value != null ? !value.equals(sessionId.value) : sessionId.value != null);
+
+    }
+
+    @Override
+    public int hashCode() {
+        return value != null ? value.hashCode() : 0;
     }
 }
