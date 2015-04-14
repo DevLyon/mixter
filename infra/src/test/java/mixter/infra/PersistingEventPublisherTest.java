3f86303b76d673a52520cc5e9e560ed2c30d0639
Jean Helou <jean.helou@gmail.com>
2015-04-14 23:43:23 +0200

    [Infra][Failing] Given a persisting event publisher, when publishing an event, it is stored

---
+++ infra/src/test/java/mixter/infra/PersistingEventPublisherTest.java
@@ -0,0 +1,31 @@
+package mixter.infra;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class PersistingEventPublisherTest {
+
+    private InMemoryEventStore store;
+    private SpyEventPublisher publisher;
+
+    @Before
+    public void setUp() throws Exception {
+        store = new InMemoryEventStore();
+        publisher = new SpyEventPublisher();
+    }
+
+    @Test
+    public void GivenAPersistingEventPublisherWhenPublishingAnEventThenItIsStored() throws Exception {
+        // Given
+        AnAggregateId id = new AnAggregateId();
+        PersistingEventPublisher persistedPublisher = new PersistingEventPublisher(store, publisher);
+        EventA expected = new EventA(id);
+        // When
+        persistedPublisher.publish(expected);
+
+        // Then
+        assertThat(store.getEventsOfAggregate(id)).containsExactly(expected);
+    }
+}
