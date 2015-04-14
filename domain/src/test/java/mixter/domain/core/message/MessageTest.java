6bc2e7d65dec9be5efb27780354e3dfc7c8f8b1d
Jean Helou <jean.helou@gmail.com>
2015-04-14 21:12:35 +0200

    [Message][Failing] When a message is republished then it sends a MessageRepublishedEvent

--- domain/src/test/java/mixter/domain/core/message/MessageTest.java
+++ domain/src/test/java/mixter/domain/core/message/MessageTest.java
@@ -3,14 +3,20 @@ package mixter.domain.core.message;
 import mixter.domain.SpyEventPublisher;
 import mixter.domain.UserId;
 import mixter.domain.core.message.events.MessagePublished;
+import mixter.domain.core.message.events.MessageRepublished;
 import org.junit.Before;
 import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class MessageTest {
     public String CONTENT = "content";
     public UserId AUTHOR_ID = new UserId();
+    public UserId USER_ID = new UserId();
     public SpyEventPublisher eventPublisher;
 
     @Before
@@ -29,5 +35,30 @@ public class MessageTest {
         // Then
         MessagePublished expectedEvent = new MessagePublished(messageId, CONTENT, AUTHOR_ID);
         assertThat(eventPublisher.publishedEvents).containsExactly(expectedEvent);
+    }
+
+    @Test
+    public void whenAMessageIsRepublishedThenItSendsAMessageRepublishedEvent() {
+        // Given
+        MessageId messageId = new MessageId();
+        Message message = messageFor(
+                new MessagePublished(messageId, CONTENT, AUTHOR_ID)
+        );
+
+        // When
+        message.republish(USER_ID, eventPublisher, AUTHOR_ID, CONTENT);
+
+        // Then
+        MessageRepublished expectedEvent = new MessageRepublished(messageId, USER_ID, AUTHOR_ID, CONTENT);
+        assertThat(eventPublisher.publishedEvents).containsExactly(expectedEvent);
+    }
+
+    protected Message messageFor(MessagePublished... events) {
+        return new Message(history(events));
+    }
+    protected List<MessagePublished> history(MessagePublished... events) {
+        List<MessagePublished> eventHistory = new ArrayList<>();
+        Collections.addAll(eventHistory, events);
+        return eventHistory;
     }
 }
