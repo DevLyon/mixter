60a4cbb33175854ab2d439bd5d75bfeaaa760e11
Jean Helou <jean.helou@gmail.com>
2015-04-14 23:58:16 +0200

    [Infra][Failing] Given an in memory SessionProjectionRepository with a saved session, when getting the session by Id, it should return the session

--- infra/src/test/java/mixter/infra/repositories/InMemorySessionProjectionRepositoryTest.java
+++ infra/src/test/java/mixter/infra/repositories/InMemorySessionProjectionRepositoryTest.java
@@ -1,6 +1,8 @@
 package mixter.infra.repositories;
 
 import mixter.domain.identity.SessionId;
+import mixter.domain.identity.SessionProjection;
+import mixter.domain.identity.SessionStatus;
 import mixter.domain.identity.UserId;
 import org.junit.Test;
 
@@ -16,6 +18,17 @@ public class InMemorySessionProjectionRepositoryTest {
         InMemorySessionProjectionRepository repository = new InMemorySessionProjectionRepository();
 
         assertThat(repository.getById(SessionId.generate())).isEqualTo(Optional.empty());
+    }
+
+    @Test
+    public void GivenARepositoryWithASavedSessionWhenRetrievingBySessionIdThenItShouldReturnTheSessionProjection() throws Exception {
+        // Given
+        SessionProjection sessionProjection = new SessionProjection(SessionId.generate(), USER_ID, SessionStatus.CONNECTED);
+        InMemorySessionProjectionRepository repository = new InMemorySessionProjectionRepository();
+        // When
+        repository.save(sessionProjection);
+        // Then
+        assertThat(repository.getById(sessionProjection.getSessionId()).get()).isEqualTo(sessionProjection);
     }
 
 }
