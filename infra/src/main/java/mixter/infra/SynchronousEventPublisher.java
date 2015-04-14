c1cd6a59e92e9e1f1b173cd26a64267a7a9ce7cb
Jean Helou <jean.helou@gmail.com>
2015-04-14 23:37:59 +0200

    [Infra][Passing] Given a synchronous event publisher with 2 registered handlers for different events, when an event is published, then it calls only the handlers for this event

--- infra/src/main/java/mixter/infra/SynchronousEventPublisher.java
+++ infra/src/main/java/mixter/infra/SynchronousEventPublisher.java
@@ -3,19 +3,28 @@ package mixter.infra;
 import mixter.domain.Event;
 import mixter.domain.EventPublisher;
 
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Map;
 import java.util.Set;
 import java.util.function.Consumer;
 
 public class SynchronousEventPublisher implements EventPublisher {
-    private Set<Consumer> handlers = new HashSet<>();
+    private Map<Class, Set<Consumer>> allHandlers = new HashMap<>();
 
     public <T> void register(Class<T> eventClass, Consumer<T> handler) {
-        this.handlers.add(handler);
+        Set<Consumer> handlers = allHandlers.getOrDefault(eventClass, emptySet());
+        handlers.add(handler);
+        allHandlers.put(eventClass, handlers);
     }
 
     @SuppressWarnings("unchecked")
     public void publish(Event event) {
+        Set<Consumer> handlers = allHandlers.getOrDefault(event.getClass(), emptySet());
         handlers.forEach(handler -> handler.accept(event));
+    }
+
+    private Set<Consumer> emptySet() {
+        return new HashSet<>();
     }
 }
