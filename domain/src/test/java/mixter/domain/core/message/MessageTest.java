9ba80bf4f8ee40b041876daa2aa4b972545f755b
Jean Helou <jean.helou@gmail.com>
2015-04-14 21:12:35 +0200

    [Core][Message][Failing] When a message is requacked then it sends a MessageRequackedEvent

--- domain/src/test/java/mixter/domain/core/message/MessageTest.java
+++ domain/src/test/java/mixter/domain/core/message/MessageTest.java
@@ -3,14 +3,20 @@ package mixter.domain.core.message;
 import mixter.domain.SpyEventPublisher;
 import mixter.domain.UserId;
 import mixter.domain.core.message.events.MessageQuacked;
+import mixter.domain.core.message.events.MessageReQuacked;
 import org.junit.Before;
 import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class MessageTest {
     public String CONTENT = "content";
     public UserId AUTHOR_ID = new UserId();
+    public UserId USER_ID = new UserId();
     public SpyEventPublisher eventPublisher;
 
     @Before
@@ -28,5 +34,30 @@ public class MessageTest {
         // Then
         MessageQuacked expectedEvent = new MessageQuacked(messageId, CONTENT, AUTHOR_ID);
         assertThat(eventPublisher.publishedEvents).extracting("message").containsExactly(expectedEvent.getMessage());
+    }
+
+    @Test
+    public void whenAMessageIsRequackedThenItSendsAMessageRequackedEvent() {
+        // Given
+        MessageId messageId = new MessageId();
+        Message message = messageFor(
+                new MessageQuacked(messageId, CONTENT, AUTHOR_ID)
+        );
+
+        // When
+        message.reQuack(USER_ID, eventPublisher, AUTHOR_ID, CONTENT);
+
+        // Then
+        MessageQuacked expectedEvent = new MessageQuacked(messageId, USER_ID, AUTHOR_ID, CONTENT);
+        assertThat(eventPublisher.publishedEvents).containsExactly(expectedEvent);
+    }
+
+    protected Message messageFor(MessageQuacked... events) {
+        return new Message(history(events));
+    }
+    protected List<MessageQuacked> history(MessageQuacked... events) {
+        List<MessageQuacked> eventHistory = new ArrayList<>();
+        Collections.addAll(eventHistory, events);
+        return eventHistory;
     }
 }
