bac4c44161fd926c89724433e7d2481e83baa042
Jean Helou <jean.helou@gmail.com>
2015-04-14 20:56:00 +0200

    [Core][Message] When a message is quacked then it sends a MessageQuackedEvent

---
+++ domain/src/test/java/mixter/domain/core/message/MessageTest.java
@@ -0,0 +1,32 @@
+package mixter.domain.core.message;
+
+import mixter.domain.SpyEventPublisher;
+import mixter.domain.UserId;
+import mixter.domain.core.message.events.MessagePublished;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class MessageTest {
+    public String CONTENT = "content";
+    public UserId AUTHOR_ID = new UserId();
+    public SpyEventPublisher eventPublisher;
+
+    @Before
+    public void setUp() throws Exception {
+        eventPublisher = new SpyEventPublisher();
+    }
+
+    @Test
+    public void whenAMessageIsQuackedThenAMessageQuackedEventisPublished() {
+        // Given
+
+        // When
+        MessageId messageId = Message.quack(AUTHOR_ID, CONTENT, eventPublisher);
+
+        // Then
+        MessageQuacked expectedEvent = new MessageQuacked(messageId, CONTENT, AUTHOR_ID);
+        assertThat(eventPublisher.publishedEvents).extracting("message").containsExactly(expectedEvent.getMessage());
+    }
+}
