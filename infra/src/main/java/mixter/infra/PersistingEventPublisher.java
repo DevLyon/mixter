968b737763dc13f6e7eef644863843ed9d92105d
Jean Helou <jean.helou@gmail.com>
2015-04-14 23:45:14 +0200

    [Infra][Passing] Given a persisting event publisher, when publishing an event, it is stored

---
+++ infra/src/main/java/mixter/infra/PersistingEventPublisher.java
@@ -0,0 +1,19 @@
+package mixter.infra;
+
+import mixter.domain.Event;
+import mixter.domain.EventPublisher;
+
+public class PersistingEventPublisher implements EventPublisher {
+    private final EventStore store;
+    private final EventPublisher publisher;
+
+    public PersistingEventPublisher(EventStore store, EventPublisher publisher) {
+        this.store = store;
+        this.publisher = publisher;
+    }
+
+    @Override
+    public void publish(Event event) {
+        store.store(event);
+    }
+}
