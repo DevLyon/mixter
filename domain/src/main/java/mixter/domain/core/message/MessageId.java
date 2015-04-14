59bccf5d9a1383aebe18b80dff161448c4054912
Jean Helou <jean.helou@gmail.com>
2015-04-15 00:33:47 +0200

    [Message][Failing] Given 2 message ids with the same value, when they are compared, then they should be equal

--- domain/src/main/java/mixter/domain/core/message/MessageId.java
+++ domain/src/main/java/mixter/domain/core/message/MessageId.java
@@ -3,4 +3,7 @@ package mixter.domain.core.message;
 import mixter.domain.AggregateId;
 
 public class MessageId implements AggregateId {
+    public static MessageId generate() {
+        return new MessageId();
+    }
 }
