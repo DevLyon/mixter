aa274b7f3018321b718ad4d2e97c08fec2e0197a
Jean Helou <jean.helou@gmail.com>
2015-04-14 21:15:43 +0200

    [Message][Passing] When a message is requacked by its author then it does not send a MessageRequackedEvent

--- domain/src/main/java/mixter/domain/core/message/events/MessageQuacked.java
+++ domain/src/main/java/mixter/domain/core/message/events/MessageQuacked.java
@@ -43,4 +43,8 @@ public class MessageQuacked implements Event {
     public MessageId getMessageId() {
         return messageId;
     }
+
+    public UserId getAuthorId() {
+        return authorId;
+    }
 }
