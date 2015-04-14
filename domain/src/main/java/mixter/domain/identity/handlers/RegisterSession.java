7b18ca245a3eee32b75a9fec2ec73012e6140957
Jean Helou <jean.helou@gmail.com>
2015-04-14 22:42:19 +0200

    [Identity][Passing] Given a RegisterSession handler, when it receives a UserDisconnected event, then it saves a discconnected SessionProjection

--- domain/src/main/java/mixter/domain/identity/handlers/RegisterSession.java
+++ domain/src/main/java/mixter/domain/identity/handlers/RegisterSession.java
@@ -3,6 +3,7 @@ package mixter.domain.identity.handlers;
 import mixter.domain.identity.SessionProjection;
 import mixter.domain.identity.SessionStatus;
 import mixter.domain.identity.events.UserConnected;
+import mixter.domain.identity.events.UserDisconnected;
 
 public class RegisterSession {
     private SessionProjectionRepository sessionProjectionRepository;
@@ -13,5 +14,9 @@ public class RegisterSession {
 
     public void apply(UserConnected event) {
         sessionProjectionRepository.save(new SessionProjection(event.getSessionId(), event.getUserId(), SessionStatus.CONNECTED));
+    }
+
+    public void apply(UserDisconnected event) {
+        sessionProjectionRepository.replaceBy(new SessionProjection(event.getSessionId(), event.getUserId(), SessionStatus.DISCONNECTED));
     }
 }
