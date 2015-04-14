4cb90d1fec68b787e42f5691a7b36e747b90b898
Jean Helou <jean.helou@gmail.com>
2015-04-14 21:36:52 +0200

    [Identity][Passing] Given a UserId, a UserIdentity can be registered and

---
+++ domain/src/main/java/mixter/domain/identity/events/UserRegistered.java
@@ -0,0 +1,28 @@
+package mixter.domain.identity.events;
+
+import mixter.domain.Event;
+import mixter.domain.identity.UserId;
+
+public class UserRegistered implements Event {
+    private UserId userId;
+
+    public UserRegistered(UserId userId) {
+        this.userId = userId;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        UserRegistered that = (UserRegistered) o;
+
+        return !(userId != null ? !userId.equals(that.userId) : that.userId != null);
+
+    }
+
+    @Override
+    public int hashCode() {
+        return userId != null ? userId.hashCode() : 0;
+    }
+}
