05b474ab92edae1074aed2429e1a1da2a5d384d6
Jean Helou <jean.helou@gmail.com>
2015-04-15 00:33:47 +0200

    [Message][Failing] Given 2 message ids with the same value, when they are compared, then they should be equal

--- domain/src/test/java/mixter/domain/core/message/MessageIdTest.java
+++ domain/src/test/java/mixter/domain/core/message/MessageIdTest.java
@@ -5,11 +5,20 @@ import org.junit.Test;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class MessageIdTest {
+    public static final String VALUE = "Value";
+
     @Test
     public void Given2GeneratedMessageIdsWhenComparedWithEqualThenTheyShouldBeDifferent() {
         MessageId messageId = MessageId.generate();
         MessageId otherMessageId = MessageId.generate();
         assertThat(messageId).isNotEqualTo(otherMessageId);
+    }
+
+    @Test
+    public void Given2MessageIdCreatedFromTheSameStringWhenComparedWithEqualsThenTheyShouldBeEqual() {
+        MessageId messageId = new MessageId(VALUE);
+        MessageId otherMessageId = new MessageId(VALUE);
+        assertThat(messageId).isEqualTo(otherMessageId);
     }
 
 }
