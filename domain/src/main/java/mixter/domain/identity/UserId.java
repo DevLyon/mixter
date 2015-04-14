b7194a95c0c1c3adcb45b1637e44f12d5d465e79
Jean Helou <jean.helou@gmail.com>
2015-04-14 21:25:26 +0200

    [Identity][Passing] Two UserId created from the same email should be equal

--- domain/src/main/java/mixter/domain/identity/UserId.java
+++ domain/src/main/java/mixter/domain/identity/UserId.java
@@ -1,7 +1,25 @@
 package mixter.domain.identity;
 
 public class UserId {
-    public UserId(String email) {
+    private String email;
 
+    public UserId(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        UserId userId = (UserId) o;
+
+        return !(email != null ? !email.equals(userId.email) : userId.email != null);
+
+    }
+
+    @Override
+    public int hashCode() {
+        return email != null ? email.hashCode() : 0;
     }
 }
