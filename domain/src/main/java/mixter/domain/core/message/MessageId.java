8395f809a2378e062afe2ee0f53ffec2e560e289
Jean Helou <jean.helou@gmail.com>
2015-04-15 00:39:45 +0200

    [Message][Passing] Given a message id , when it is serialized to string, then it should return its value

--- domain/src/main/java/mixter/domain/core/message/MessageId.java
+++ domain/src/main/java/mixter/domain/core/message/MessageId.java
@@ -31,4 +31,9 @@ public class MessageId implements AggregateId {
     public int hashCode() {
         return value != null ? value.hashCode() : 0;
     }
+
+    @Override
+    public String toString() {
+        return value;
+    }
 }
