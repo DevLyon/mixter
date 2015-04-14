79ecbfd90cdc43fc7132006d1c071bab8c3277ac
Jean Helou <jean.helou@gmail.com>
2015-04-14 21:44:56 +0200

    [Identity][Passing] Given a UserIdentity When logging in , Then a UserConnected event is raised

---
+++ domain/src/main/java/mixter/domain/identity/events/UserConnected.java
@@ -0,0 +1,31 @@
+package mixter.domain.identity.events;
+
+import mixter.domain.Event;
+import mixter.domain.identity.SessionId;
+import mixter.domain.identity.UserId;
+
+import java.time.Instant;
+
+public class UserConnected implements Event {
+    private final SessionId sessionId;
+    private final UserId userId;
+    private final Instant now;
+
+    public UserConnected(SessionId sessionId, UserId userId, Instant now) {
+        this.sessionId = sessionId;
+        this.userId = userId;
+        this.now = now;
+    }
+
+    public SessionId getSessionId() {
+        return sessionId;
+    }
+
+    public UserId getUserId() {
+        return userId;
+    }
+
+    public Instant getNow() {
+        return now;
+    }
+}
