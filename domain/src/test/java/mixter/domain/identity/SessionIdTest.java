e801d42106431d2c05c6ed9d02c57e97e4918863
Jean Helou <jean.helou@gmail.com>
2015-04-14 22:00:17 +0200

    [Identity][Failing] Given 2 SessionIds created from the same string, they should be equal

--- domain/src/test/java/mixter/domain/identity/SessionIdTest.java
+++ domain/src/test/java/mixter/domain/identity/SessionIdTest.java
@@ -5,10 +5,21 @@ import org.junit.Test;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class SessionIdTest {
+
+    public static final String VALUE = "Value";
+
     @Test
     public void Given2GeneratedSessionIdsWhenComparedWithEqualsThenTheyShouldNotBeEqual() throws Exception {
         SessionId sessionId = SessionId.generate();
         SessionId otherSessionId = SessionId.generate();
         assertThat(sessionId).isNotEqualTo(otherSessionId);
+    }
+
+    @Test
+    public void Given2SessionIdCreatedFromTheSameStringWhenComparedWithEqualsThenTheyShouldBeEqual() throws Exception {
+        SessionId sessionId = new SessionId(VALUE);
+        SessionId otherSessionId = new SessionId(VALUE);
+
+        assertThat(sessionId).isEqualTo(otherSessionId);
     }
 }
