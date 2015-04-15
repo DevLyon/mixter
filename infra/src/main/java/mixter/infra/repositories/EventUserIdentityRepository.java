acc885fa59373cb48acadeb45cc6aec90c692937
Jean Helou <jean.helou@gmail.com>
2015-04-15 06:53:45 +0200

    [Infra][Passing] Given a user identity repository and a UserRegisteredEvent, when retrieving the userIdentity by id, then it should find the UserIdentity

--- infra/src/main/java/mixter/infra/repositories/EventUserIdentityRepository.java
+++ infra/src/main/java/mixter/infra/repositories/EventUserIdentityRepository.java
@@ -1,17 +1,26 @@
 package mixter.infra.repositories;
 
+import mixter.domain.Event;
 import mixter.domain.identity.UserId;
 import mixter.domain.identity.UserIdentity;
 import mixter.infra.EventStore;
 
+import java.util.List;
 import java.util.NoSuchElementException;
 
 public class EventUserIdentityRepository {
-    public EventUserIdentityRepository(EventStore eventStore) {
+    private EventStore eventStore;
 
+    public EventUserIdentityRepository(EventStore eventStore) {
+        this.eventStore = eventStore;
     }
 
     public UserIdentity getById(UserId userId) {
-        throw new NoSuchElementException();
+        List<Event> history = eventStore.getEventsOfAggregate(userId);
+        if (history.isEmpty()) {
+            throw new NoSuchElementException();
+        } else {
+            return new UserIdentity(history);
+        }
     }
 }
