70bce7a11425b053be42d2c3cecf13b1876243fe
Jean Helou <jean.helou@gmail.com>
2015-04-15 19:13:47 +0200

    [Core][NotifyFollowerOfFolloweeMessage][Test OK] Given a NotifyFollowerOfFolloweeMessage handler, when it receives a MessageQuacked event, it should publish a FolloweeMessageQuacked to each folllower

--- domain/src/main/java/mixter/domain/core/subscription/handlers/NotifyFollowerOfFolloweeMessage.java
+++ domain/src/main/java/mixter/domain/core/subscription/handlers/NotifyFollowerOfFolloweeMessage.java
@@ -1,4 +1,33 @@
 package mixter.domain.core.subscription.handlers;
 
+import mixter.domain.EventPublisher;
+import mixter.domain.core.message.events.MessageQuacked;
+import mixter.domain.core.subscription.FollowerRepository;
+import mixter.domain.core.subscription.Subscription;
+import mixter.domain.core.subscription.SubscriptionId;
+import mixter.domain.core.subscription.SubscriptionRepository;
+import mixter.domain.identity.UserId;
+
+import java.util.Set;
+
 public class NotifyFollowerOfFolloweeMessage {
+
+    private final FollowerRepository followerRepository;
+    private final SubscriptionRepository subscriptionRepository;
+    private final EventPublisher eventPublisher;
+
+    public NotifyFollowerOfFolloweeMessage(FollowerRepository followerRepository, SubscriptionRepository subscriptionRepository, EventPublisher eventPublisher) {
+
+        this.followerRepository = followerRepository;
+        this.subscriptionRepository = subscriptionRepository;
+        this.eventPublisher = eventPublisher;
+    }
+
+    public void apply(MessageQuacked messagePublished) {
+        Set<UserId> followers = followerRepository.getFollowers(messagePublished.getAuthorId());
+        for (UserId follower : followers) {
+            Subscription subscription = subscriptionRepository.getById(new SubscriptionId(follower, messagePublished.getAuthorId()));
+            subscription.notifyFollower(messagePublished.getMessageId(), eventPublisher);
+        }
+    }
 }
