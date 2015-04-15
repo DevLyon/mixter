6c32c11161c9562ea45e17e2302e3e4f3965f71d
Jean Helou <jean.helou@gmail.com>
2015-04-15 17:53:38 +0200

    [Core][Subscription][Test OK] Given a follower and a followee, when the follower follows the followee, then a UserFollowed event is published

--- domain/src/main/java/mixter/domain/core/subscription/Subscription.java
+++ domain/src/main/java/mixter/domain/core/subscription/Subscription.java
@@ -1,7 +1,13 @@
 package mixter.domain.core.subscription;
 
 import mixter.doc.Aggregate;
+import mixter.domain.EventPublisher;
+import mixter.domain.core.subscription.events.UserFollowed;
+import mixter.domain.identity.UserId;
 
 @Aggregate
 public class Subscription {
+    public static void follow(UserId follower, UserId followee, EventPublisher eventPublisher) {
+        eventPublisher.publish(new UserFollowed(new SubscriptionId(follower, followee)));
+    }
 }
