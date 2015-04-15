caa19a578cbbe4f45e2c4188d78589ab6522de28
Jean Helou <jean.helou@gmail.com>
2015-04-15 19:16:25 +0200

    [Core][NotifyFollowerOfFolloweeMessage][Test KO] Given a NotifyFollowerOfFolloweeMessage handler, when it receives a MessageRequacked event, it should publish a FolloweeMessageQuacked to each folllower

--- domain/src/test/java/mixter/domain/core/subscription/handlers/NotifyFollowerOfFolloweeMessageTest.java
+++ domain/src/test/java/mixter/domain/core/subscription/handlers/NotifyFollowerOfFolloweeMessageTest.java
@@ -4,8 +4,10 @@ import mixter.domain.DomainTest;
 import mixter.domain.SpyEventPublisher;
 import mixter.domain.core.message.MessageId;
 import mixter.domain.core.message.events.MessageQuacked;
+import mixter.domain.core.message.events.MessageRequacked;
 import mixter.domain.core.subscription.FakeFollowerRepository;
 import mixter.domain.core.subscription.FakeSubscriptionRepository;
+import mixter.domain.core.subscription.Subscription;
 import mixter.domain.core.subscription.SubscriptionId;
 import mixter.domain.core.subscription.events.FolloweeMessageQuacked;
 import mixter.domain.core.subscription.events.UserFollowed;
@@ -20,6 +22,8 @@ public class NotifyFollowerOfFolloweeMessageTest extends DomainTest {
     public String CONTENT = "Content";
     public UserId AUTHOR_ID = new UserId("author@mix-it.fr");
     public UserId FOLLOWER_ID = new UserId("follower@mix-it.fr");
+    public UserId USER_ID = new UserId("someUser@mix-it.fr");
+
     public MessageId MESSAGE_ID = MessageId.generate();
 
     private SpyEventPublisher eventPublisher;
@@ -48,4 +52,24 @@ public class NotifyFollowerOfFolloweeMessageTest extends DomainTest {
         FolloweeMessageQuacked followeeMessagePublished = new FolloweeMessageQuacked(new SubscriptionId(FOLLOWER_ID, AUTHOR_ID), MESSAGE_ID);
         assertThat(eventPublisher.publishedEvents).containsExactly(followeeMessagePublished);
     }
+
+    @Test
+    public void ARepublishedMessageShouldNotifyFollowers() {
+        // Given
+        MessageRequacked messageRequacked = new MessageRequacked(MESSAGE_ID, USER_ID, AUTHOR_ID, CONTENT);
+        Subscription subscription = subscriptionFor(
+                new UserFollowed(new SubscriptionId(FOLLOWER_ID, AUTHOR_ID))
+        );
+        subscriptionRepository.add(subscription);
+        followerRepository.saveFollower(AUTHOR_ID, FOLLOWER_ID);
+
+        NotifyFollowerOfFolloweeMessage handler = new NotifyFollowerOfFolloweeMessage(followerRepository, subscriptionRepository, eventPublisher);
+
+        // When
+        handler.apply(messageRequacked);
+        // Then
+        FolloweeMessageQuacked followeeMessagePublished = new FolloweeMessageQuacked(new SubscriptionId(FOLLOWER_ID, AUTHOR_ID), MESSAGE_ID);
+        assertThat(eventPublisher.publishedEvents).containsExactly(followeeMessagePublished);
+    }
+
 }
