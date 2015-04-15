f5f56bd6a66de2603ed8863a360248bf5cdc4422
Jean Helou <jean.helou@gmail.com>
2015-04-15 18:38:17 +0200

    [Core][UpdateFollowers][Test KO] Given an UpdateFollowers handler, when it receives a UserFollowed event, it should add the follower to the followers repository

---
+++ domain/src/main/java/mixter/domain/core/subscription/handlers/UpdateFollowers.java
@@ -0,0 +1,10 @@
+package mixter.domain.core.subscription.handlers;
+
+import mixter.doc.Handler;
+import mixter.domain.core.subscription.FollowerRepository;
+import mixter.domain.core.subscription.events.UserFollowed;
+
+@Handler
+public class UpdateFollowers {
+
+}
