6b7b78f3af018108395e86201a4336590c39bb77
Jean Helou <jean.helou@gmail.com>
2015-04-15 18:38:17 +0200

    [Core][UpdateFollowers][Test KO] Given an UpdateFollowers handler, when it receives a UserFollowed event, it should add the follower to the followers repository

---
+++ domain/src/test/java/mixter/domain/core/subscription/handlers/UpdateFollowersTest.java
@@ -0,0 +1,35 @@
+package mixter.domain.core.subscription.handlers;
+
+import mixter.domain.core.subscription.FakeFollowerRepository;
+import mixter.domain.core.subscription.FollowerRepository;
+import mixter.domain.core.subscription.SubscriptionId;
+import mixter.domain.core.subscription.events.UserFollowed;
+import mixter.domain.identity.UserId;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class UpdateFollowersTest {
+
+    public static final UserId FOLLOWEE = new UserId("followee@mix-it.fr");
+    public static final UserId FOLLOWER = new UserId("follower@mix-it.fr");
+
+    private FollowerRepository repository;
+
+    @Before
+    public void setUp() throws Exception {
+        repository = new FakeFollowerRepository();
+    }
+
+    @Test
+    public void shouldAddFollowerOfFolloweeOnUserFollowed() {
+        // Given
+        UserFollowed userFollowed = new UserFollowed(new SubscriptionId(FOLLOWER, FOLLOWEE));
+        UpdateFollowers handler = new UpdateFollowers(repository);
+        // When
+        handler.apply(userFollowed);
+        // Then
+        assertThat(repository.getFollowers(FOLLOWEE)).containsExactly(FOLLOWER);
+    }
+}
