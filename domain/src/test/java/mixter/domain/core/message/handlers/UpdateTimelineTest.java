d20f19ef654813f3708b616c3c132bfad16d3ece
Jean Helou <jean.helou@gmail.com>
2015-04-15 16:10:57 +0200

    [Message][Failing] Given an UpdateTimeline handler, when it receives a ReplyMessagePublished event, then it creates a TimelineMessageProjection

--- domain/src/test/java/mixter/domain/core/message/handlers/UpdateTimelineTest.java
+++ domain/src/test/java/mixter/domain/core/message/handlers/UpdateTimelineTest.java
@@ -4,6 +4,7 @@ import mixter.domain.core.message.MessageId;
 import mixter.domain.core.message.TimelineMessageProjection;
 import mixter.domain.core.message.TimelineMessageRepository;
 import mixter.domain.core.message.events.MessagePublished;
+import mixter.domain.core.message.events.ReplyMessagePublished;
 import mixter.domain.identity.UserId;
 import org.junit.Before;
 import org.junit.Test;
@@ -17,7 +18,9 @@ public class UpdateTimelineTest {
 
     public static final UserId AUTHOR_ID = new UserId("author@mix-it.fr");
     public static final UserId USER_ID = new UserId("someUser@mix-it.fr");
+    public static final UserId REPLIER_ID = new UserId("replier@mix-it.fr");
     public static final String CONTENT = "hello world";
+    public static final String REPLY_CONTENT = "reply hello world";
     private TimelineMessageRepositoryFake timelineRepository;
 
     @Before
@@ -36,6 +39,20 @@ public class UpdateTimelineTest {
         handler.apply(messagePublished);
         // Then
         assertThat(timelineRepository.getMessages()).containsExactly(new TimelineMessageProjection(AUTHOR_ID, AUTHOR_ID, CONTENT, messageId));
+    }
+
+
+    @Test
+    public void whenUpdateTimelineAppliesAReplyMessagePublishedEventThenATimelineMessageIsCreated() {
+        // Given
+        MessageId messageId = MessageId.generate();
+        MessageId replyId = MessageId.generate();
+        ReplyMessagePublished replyMessagePublished = new ReplyMessagePublished(AUTHOR_ID, REPLIER_ID, REPLY_CONTENT, messageId, replyId);
+        UpdateTimeline handler = new UpdateTimeline(timelineRepository);
+        // When
+        handler.apply(replyMessagePublished);
+        // Then
+        assertThat(timelineRepository.getMessages()).containsExactly(new TimelineMessageProjection(REPLIER_ID, REPLIER_ID, REPLY_CONTENT, replyId));
     }
 
     class TimelineMessageRepositoryFake implements TimelineMessageRepository {
