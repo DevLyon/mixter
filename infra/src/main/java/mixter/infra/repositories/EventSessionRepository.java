a8a63962c898abfe0143fc455df69d32b8353eb7
Jean Helou <jean.helou@gmail.com>
2015-04-15 06:45:34 +0200

    [Infra][Passing] Given a session repository and a UserConnected event, when retrieving the session by id, then it should return the session

--- infra/src/main/java/mixter/infra/repositories/EventSessionRepository.java
+++ infra/src/main/java/mixter/infra/repositories/EventSessionRepository.java
@@ -1,17 +1,26 @@
 package mixter.infra.repositories;
 
+import mixter.domain.Event;
 import mixter.domain.identity.Session;
 import mixter.domain.identity.SessionId;
 import mixter.infra.EventStore;
 
+import java.util.List;
 import java.util.NoSuchElementException;
 
 public class EventSessionRepository {
-    public EventSessionRepository(EventStore eventStore) {
+    private EventStore eventStore;
 
+    public EventSessionRepository(EventStore eventStore) {
+        this.eventStore = eventStore;
     }
 
     public Session getById(SessionId sessionId) {
-        throw new NoSuchElementException();
+        List<Event> history = eventStore.getEventsOfAggregate(sessionId);
+        if (history.isEmpty()) {
+            throw new NoSuchElementException();
+        } else {
+            return new Session(history);
+        }
     }
 }
