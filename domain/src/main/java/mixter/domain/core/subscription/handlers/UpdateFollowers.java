0965642f2523c211ac4ffe72e236c9b97ceb98dc
Jean Helou <jean.helou@gmail.com>
2015-04-15 18:52:26 +0200

    [Core][UpdateFollowers][Test OK] Given an UpdateFollowers handler, when it receives a UserUnfollowed event, it should remove the follower of the followers repository

--- domain/src/main/java/mixter/domain/core/subscription/handlers/UpdateFollowers.java
+++ domain/src/main/java/mixter/domain/core/subscription/handlers/UpdateFollowers.java
@@ -3,6 +3,7 @@ package mixter.domain.core.subscription.handlers;
 import mixter.doc.Handler;
 import mixter.domain.core.subscription.FollowerRepository;
 import mixter.domain.core.subscription.events.UserFollowed;
+import mixter.domain.core.subscription.events.UserUnfollowed;
 
 @Handler
 public class UpdateFollowers {
@@ -14,5 +15,9 @@ public class UpdateFollowers {
 
     public void apply(UserFollowed event) {
         repository.saveFollower(event.getSubscriptionId().getFollowee(), event.getSubscriptionId().getFollower());
+    }
+
+    public void apply(UserUnfollowed event) {
+        repository.removeFollower(event.getSubscriptionId().getFollowee(), event.getSubscriptionId().getFollower());
     }
 }
