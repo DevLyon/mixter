1d44df9f7dffec830332c3e27dd24e8ac3c4ffa9
Jean Helou <jean.helou@gmail.com>
2015-04-15 17:55:48 +0200

    [Core][Subscription][Test KO] Given a subscription, when the follower unfollows the followee, then a UserUnfollowed event is published

--- domain/src/test/java/mixter/domain/core/subscription/SubscriptionTest.java
+++ domain/src/test/java/mixter/domain/core/subscription/SubscriptionTest.java
@@ -1,8 +1,10 @@
 package mixter.domain.core.subscription;
 
 import mixter.domain.DomainTest;
+import mixter.domain.Event;
 import mixter.domain.SpyEventPublisher;
 import mixter.domain.core.subscription.events.UserFollowed;
+import mixter.domain.core.subscription.events.UserUnfollowed;
 import mixter.domain.identity.UserId;
 import org.junit.Before;
 import org.junit.Test;
@@ -12,8 +14,9 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class SubscriptionTest extends DomainTest {
 
     private SpyEventPublisher eventPublisher;
-    public static final UserId FOLLOWER = new UserId("follower@mix-it.fr");
-    public static final UserId FOLLOWEE = new UserId("followee@mix-it.fr");
+    public UserId FOLLOWER = new UserId("follower@mix-it.fr");
+    public UserId FOLLOWEE = new UserId("followee@mix-it.fr");
+    public SubscriptionId SUBSCRIPTION_ID = new SubscriptionId(FOLLOWER, FOLLOWEE);
 
     @Before
     public void setUp() throws Exception {
@@ -32,4 +35,21 @@ public class SubscriptionTest extends DomainTest {
         assertThat(eventPublisher.publishedEvents).containsExactly(userFollowed);
     }
 
+    @Test
+    public void whenAUserUnfollowsAnotherUserUnfollowedIsRaised() throws Exception {
+        //Given
+        Subscription subscription = subscriptionFor(
+                new UserFollowed(SUBSCRIPTION_ID)
+        );
+        //When
+        subscription.unfollow(eventPublisher);
+
+        //Then
+        UserUnfollowed userUnfollowed = new UserUnfollowed(SUBSCRIPTION_ID);
+        assertThat(eventPublisher.publishedEvents).containsExactly(userUnfollowed);
+    }
+
+    protected Subscription subscriptionFor(Event... events) {
+        return new Subscription(history(events));
+    }
 }
