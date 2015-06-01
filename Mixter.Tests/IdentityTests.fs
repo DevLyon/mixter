c65f1d6ec69c8c62b2ada6def76d5c8d82923a02
Clément Bouillier <clement.bouillier@devcrafting.com>
2015-06-01 15:01:38 +0200

    Refactor to only return events (no more use EventPublisher) + add user login

--- Mixter.Tests/IdentityTests.fs
+++ Mixter.Tests/IdentityTests.fs
@@ -2,13 +2,21 @@
 
 open NUnit.Framework
 open FsUnit
+open System
 open Mixter.Domain.Identity
 
 [<TestFixture>]
 type ``User aggregate`` ()=
 
     [<Test>] 
-    member x.``When a user register, then user registered event is published`` () =
-        let assertEvents = should equal [ UserRegistered { UserId = UserId "clem@mix-it.fr" } ]
-        let assertUserRegistedPublished aggregateId events = events |> assertEvents
-        register assertUserRegistedPublished (UserId "clem@mix-it.fr" ) |> assertEvents
+    member x.``When a user register, then user registered event is returned`` () =
+        register (UserId "clem@mix-it.fr" ) 
+            |> should equal [ UserRegistered { UserId = UserId "clem@mix-it.fr" } ]
+
+    [<Test>]
+    member x.``When a user log in, then user connected event is returned`` () =
+        let sessionId = SessionId.generate
+        let getCurrentTime = new DateTime()
+        apply DecisionProjection.initial (UserRegistered { UserId = UserId "clem@mix-it.fr" })
+            |> logIn sessionId getCurrentTime
+            |> should equal [ UserConnected { SessionId = sessionId; UserId = UserId "clem@mix-it.fr"; ConnectedAt = getCurrentTime } ]
--- Mixter/Identity.fs
+++ Mixter/Identity.fs
@@ -6,13 +6,30 @@ module Identity =
     type UserId = UserId of string
 
     type SessionId = SessionId of string
+        with static member generate = SessionId (Guid.NewGuid().ToString())
 
     type UserRegisteredEvent = { UserId: UserId }
-    
+
+    type UserConnectedEvent = { SessionId: SessionId; UserId: UserId; ConnectedAt: DateTime}
+
     type Event = 
         UserRegistered of UserRegisteredEvent
+        | UserConnected of UserConnectedEvent
 
-    let register publish userId =
-        let userRegistered = UserRegistered { UserId = userId }
-        publish userId [ userRegistered ]
-        [ userRegistered ]
+    type DecisionProjection = {
+        UserId: UserId
+    }
+        with static member initial = { UserId = UserId "" }
+
+    let register userId =
+        [ UserRegistered { UserId = userId } ]
+
+    let logIn sessionId getCurrentTime decisionProjection =
+        [ UserConnected { SessionId = sessionId; UserId = decisionProjection.UserId; ConnectedAt = getCurrentTime } ]
+
+    let apply decisionProjection event =
+        match event with
+        | UserRegistered e -> { UserId = e.UserId }
+        | UserConnected _ -> decisionProjection
+
+    
