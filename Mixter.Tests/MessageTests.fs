a6a51ed83d70cdefd2c2f45a64e8091f8af168b6
Clément Bouillier <clement.bouillier@devcrafting.com>
2015-07-24 22:51:41 +0200

    Publish raises a MessagePublished event

---
+++ Mixter.Tests/MessageTests.fs
@@ -0,0 +1,15 @@
+﻿namespace Mixter.Domain.Message.Tests
+
+open NUnit.Framework
+open FsUnit
+open System
+open Mixter.Domain.Identity
+open Mixter.Domain.Message
+
+[<TestFixture>]
+type ``Given a Message`` ()=
+    [<Test>] 
+    member x.``When publish, then user published event is returned`` () =
+        let messageId = MessageId.generate
+        publish messageId (UserId "clem@mix-it.fr") "hello world" 
+            |> should equal [ MessagePublished { MessageId = messageId; UserId = UserId "clem@mix-it.fr"; Content = "hello world" } ]
---
+++ Mixter/Message.fs
@@ -0,0 +1,16 @@
+﻿namespace Mixter.Domain
+
+open Mixter.Domain.Identity
+
+module Message =
+    open System
+
+    type MessageId = MessageId of string
+        with static member generate = MessageId (Guid.NewGuid().ToString())
+
+    type Event =
+        | MessagePublished of MessagePublished
+    and MessagePublished = { MessageId: MessageId; UserId: UserId; Content: string}
+
+    let publish messageId userId content =
+        [ MessagePublished { MessageId = messageId; UserId = userId; Content = content } ]
