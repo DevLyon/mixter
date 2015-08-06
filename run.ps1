52b05b6c1d72ae6cb587f891909529190fccae52
fpellet <florent.pellet@gmail.com>
2015-08-06 18:38:07 +0200

    Refactoring windows script

--- run.ps1
+++ run.ps1
@@ -1,5 +1,4 @@
 $languages = @{ "js"="js/v2/full" }
-$branch = "js/v2/full"
 $testsNbByStep = @{ 1=4; 2=1; 3=4; 4=4; 5=2 }
 
 $testBranch = "test"
@@ -19,30 +18,34 @@ $jumpToNextStepCommandTemplate = @"
 
 git add -A 
 git commit -m ""Abort test"" 
-git checkout -b workshop-@@nexttag@@ @@nexttag@@
+git checkout -b "@ + $workshopBranch + @"-@@nexttag@@ @@nexttag@@
 git merge @@nexttag@@-test1
 git checkout --ours .
 git add .
 git commit -m "Merge with test branch"
 "@
 
+function AskParametreWithValues($name, $values){
+    do {
+        $value = Read-Host ($name + " (" + ($values -join ", ") + ")")
+    } while(-Not ($values -contains $value))
+
+    return $value
+}
+
+function askLanguage(){
+	AskParametreWithValues "Language" $languages.Keys
+}
+
 function generateTagsForStep($stepNum){
-	([array](1..$testsNbByStep.$stepNum | %{ "step" + $stepNum + "-test" + $_ })) + ("step" + $stepNum)
+	[array]$testTags = 1..$testsNbByStep.$stepNum | %{ "step" + $stepNum + "-test" + $_ }
+	[array]$stepTags = ("step" + $stepNum)
+	
+	$testTags + $stepTags
 }
 
 function generateTags(){
 	1..$testsNbByStep.Count | %{ generateTagsForStep $_ }
-}
-
-function clean(){
-	git reset --hard HEAD
-	#git clean -dfx
-	git checkout master
-	git branch -D $testBranch
-	git branch -D $solutionBranch
-	git branch -D $workshopBranch
-	1..$testsNbByStep.Count | %{ git branch -D ("workshop-step" + $_) }
-	generateTags | %{ git tag -d $_ }
 }
 
 function getCurrentTestTag(){
@@ -65,6 +68,11 @@ function getNextStepTag(){
 	"step" + ($currentStep + 1)
 }
 
+function resetTestCounter(){
+	$script:currentStep = 1
+	$script:currentTestOfStep = 1
+}
+
 function nextTest(){
 	if($testsNbByStep.$currentStep -le $currentTestOfStep){
 		$script:currentStep++
@@ -82,38 +90,51 @@ function hasNextStep(){
 	$currentStep -lt $testsNbByStep.Count
 }
 
-function addNavigationCommand(){
-	$hasNextStep = hasNextStep
-	$hasNextTestForCurrentStep = hasNextTestForCurrentStep
-	if((hasNextStep) -or (hasNextTestForCurrentStep)) {
-		$nextCommandTemplate.Replace("@@nexttag@@", (getNextTestTag)) | out-file 'next.bat' -enc ascii
-		git add next.bat > $null
+function clean(){
+	Write-Host "Clean repository..."
 
-		git commit -m "Add test navigation commands" > $null
-	}
+	git reset --hard HEAD > $null
+	git checkout master > $null
+	git branch -D $testBranch *> $null
+	git branch -D $solutionBranch *> $null
+	git branch -D $workshopBranch *> $null
+	generateTags | %{ git branch -D ($workshopBranch + "-" + $_) } *> $null
+	generateTags | %{ git tag -d $_ } *> $null
+}
+
+function extraCommitHashOfLog($line){
+	$parts = $line.split(' ')
+	$parts[0]
+}
+
+function isFailedTestCommit($line){
+	$line -like '* KO]*'
+}
+
+function addStepNavigationCommand($nextStepTag){
+	$jumpToNextStepCommandTemplate.Replace("@@nexttag@@", $nextStepTag) | out-file 'jumpToNextStep.bat' -enc ascii
+	git add jumpToNextStep.bat > $null
+
+	git commit -m "Add step navigation commands" > $null
 }
 
 function pickCommitForSolution($line){
-	$parts = $line.split(' ')
-	$hash = $parts[0]
+	$hash = extraCommitHashOfLog $line
 
-	Write-Host ("cherry-pick " + $line)
+	Write-Host -NoNewline "."
 
-	$isKoTest = $line -like '* KO]*'
+	$isKoTest = isFailedTestCommit $line
 	$isFirstTestOfStep = $currentTestOfStep -eq 1
 	if($isKoTest -and $isFirstTestOfStep -and (hasNextStep)){
-		$jumpToNextStepCommandTemplate.Replace("@@nexttag@@", (getNextStepTag)) | out-file 'jumpToNextStep.bat' -enc ascii
-		git add jumpToNextStep.bat > $null
-
-		git commit -m "Add step navigation commands" > $null
+		addStepNavigationCommand (getNextStepTag)
 	}
 
 	git cherry-pick $hash > $null
 
 	if($isKoTest -and $isFirstTestOfStep){
-		Write-Host ("add tag " + (getCurrentStepTag))
-
 		git tag (getCurrentStepTag) > $null
+
+		Write-Host ((getCurrentStepTag) + " Ok")
 	}
 
 	if($isKoTest){
@@ -121,54 +142,75 @@ function pickCommitForSolution($line){
 	}
 }
 
-function pickCommitForTest($line){
-	$parts = $line.split(' ')
-	$hash = $parts[0]
+function getCommitLog($branch){
+	git log $branch --pretty=tformat:'%h %s' --reverse -E HEAD..
+}
 
-	Write-Host ("cherry-pick " + $line)
+function initializeSolutionBranch($referenceBranch){
+	Write-Host "Initialize solution branch"
+
+	resetTestCounter
+
+	git checkout -b $solutionBranch origin/master > $null
+	getCommitLog $referenceBranch | %{ pickCommitForSolution $_ }
+
+	Write-Host "Done"
+}
+
+function addNavigationCommand($nextTestTag){
+	$nextCommandTemplate.Replace("@@nexttag@@", $nextTestTag) | out-file 'next.bat' -enc ascii
+	git add next.bat > $null
+
+	git commit -m "Add test navigation commands" > $null
+}
+
+function pickCommitForTest($line){
+	$hash = extraCommitHashOfLog $line
+	
+	Write-Host -NoNewline "."
 
 	git cherry-pick $hash > $null
 
-	if ($line -like '* KO]*') { 
-		Write-Host ("add tag " + (getCurrentTestTag))
-
-		addNavigationCommand
+	if (isFailedTestCommit $line) {
+		if((hasNextStep) -or (hasNextTestForCurrentStep)) {
+			addNavigationCommand (getNextTestTag)
+		}
 
 		git tag (getCurrentTestTag) > $null
+		Write-Host ((getCurrentTestTag) + " Ok")
+
 		nextTest
 	}
 }
 
-function AskParametreWithValues($name, $values){
-    do {
-        $value = Read-Host ($name + " (" + ($values -join ", ") + ")")
-    } while(-Not ($values -contains $value))
-
-    return $value
+function getCommitLogWithoutGreenTest($branch){
+	git log $branch --pretty=tformat:'%h %s' --grep '[^K]\][^\[]' --grep '^[^\[]' --reverse -E HEAD..
 }
 
-function askLanguage(){
-	AskParametreWithValues "Language" $languages.Keys
+function initializeTestBranch(){
+	Write-Host "Initialize test branch"
+
+	resetTestCounter
+
+	git checkout -b $testBranch origin/master > $null
+	getCommitLogWithoutGreenTest $solutionBranch | %{ pickCommitForTest $_ }
+
+	Write-Host "Done"
+}
+
+function InitializeWorkflow(){
+	Write-Host "Initialize workspace"
+	
+	resetTestCounter
+
+	git checkout -b $workshopBranch (getCurrentStepTag)
 }
 
 $selectedLanguage = askLanguage
 
-$referenceBranch = $languages.$selectedLanguage
-
 clean
+initializeSolutionBranch $languages.$selectedLanguage
+initializeTestBranch
+InitializeWorkflow
 
-sleep 5
-
-$currentStep = 1
-$currentTestOfStep = 1
-git checkout -b $solutionBranch origin/master
-git log $referenceBranch --pretty=tformat:'%h %s' --reverse -E HEAD.. | %{ pickCommitForSolution $_ }
-
-sleep 2
-
-$currentStep = 1
-$currentTestOfStep = 1
-git checkout -b $testBranch origin/master
-git log $solutionBranch --pretty=tformat:'%h %s' --grep '[^K]\][^\[]' --grep '^[^\[]' --reverse -E HEAD.. | %{ pickCommitForTest $_ }
-
-git checkout -b $workshopBranch "step1-test1"
+Write-Host "Koan OK"
