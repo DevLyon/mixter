6315e76d45b0bd9d50176062c7e50068150e911a
Cl?ment Bouillier <clement.bouillier@devcrafting.com>
2015-10-10 20:13:19 +0200

    Use "quack" instead of "publish" to avoid ambiguity between business "publish" and event publisher publish (and duck quacks, as bird tweets...)

--- tests/Infrastructure/Messages/MessageRepositoryTest.php
+++ tests/Infrastructure/Messages/MessageRepositoryTest.php
@@ -4,19 +4,19 @@ namespace Tests\Infrastructure\Messages;
 
 use App\Domain\Identity\UserId;
 use App\Domain\Messages\MessageId;
-use App\Domain\Messages\MessagePublished;
+use App\Domain\Messages\MessageQuacked;
 use App\Infrastructure\Messages\MessageRepository;
 use Tests\Infrastructure\InMemoryEventStore;
 
 class MessageRepositoryTest extends \PHPUnit_Framework_TestCase
 {
-    public function testGivenAMessageHaveBeenPublished_WhenGetByMessageId_ThenReturnsMessageAggregate()
+    public function testGivenAMessageHaveBeenQuacked_WhenGetByMessageId_ThenReturnsMessageAggregate()
     {
-        $messagePublished = new MessagePublished(MessageId::generate(), 'Hello', new UserId('clem@mix-it.fr'));
-        $eventStore = new InMemoryEventStore(array($messagePublished));
+        $messageQuacked = new MessageQuacked(MessageId::generate(), 'Hello', new UserId('clem@mix-it.fr'));
+        $eventStore = new InMemoryEventStore(array($messageQuacked));
         $messageRepository = new MessageRepository($eventStore);
 
-        $message = $messageRepository->get($messagePublished->getMessageId());
+        $message = $messageRepository->get($messageQuacked->getMessageId());
 
         \Assert\that($message)->notNull();
     }
