27392904b1270f7ca16556859ccc684bbba9c26c
fpellet <florent.pellet@gmail.com>
2015-10-14 20:03:30 +0200

    [Infra][EventPublisher] Implement EventPublisher

---
+++ Mixter.Infrastructure/EventPublisher.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Mixter.Domain;
+
+namespace Mixter.Infrastructure
+{
+    public class EventPublisher : IEventPublisher
+    {
+        private readonly IList<IEventHandler> _handlers = new List<IEventHandler>();
+
+        public void Subscribe(IEventHandler handler)
+        {
+            _handlers.Add(handler);
+        }
+
+        public void Publish<TEvent>(TEvent evt) where TEvent : IDomainEvent
+        {
+            foreach (var handler in _handlers.OfType<IEventHandler<TEvent>>())
+            {
+                handler.Handle(evt);
+            }
+        }
+    }
+}
