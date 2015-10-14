053787f00a93e03e1cdac36ae34719deb31539d5
fpellet <florent.pellet@gmail.com>
2015-10-14 19:18:47 +0200

    [Identity][Session] When user logout Then raise UserDisconnected event

---
+++ Mixter.Domain/Identity/Events/UserDisconnected.cs
@@ -0,0 +1,22 @@
+﻿namespace Mixter.Domain.Identity.Events
+{
+    [Event]
+    public struct UserDisconnected : IDomainEvent
+    {
+        public SessionId SessionId { get; private set; }
+
+        public UserId UserId { get; private set; }
+
+        public UserDisconnected(SessionId sessionId, UserId userId)
+            : this()
+        {
+            SessionId = sessionId;
+            UserId = userId;
+        }
+
+        public object GetAggregateId()
+        {
+            return SessionId;
+        }
+    }
+}
