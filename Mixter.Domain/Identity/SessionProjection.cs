ca71d0787f683d4beeb4789d4edc2773c7b906db
fpellet <florent.pellet@gmail.com>
2015-10-14 19:46:52 +0200

    [Infra][SessionsRepository] Given several user connected When getUserIdOfSession Then return userId of this session

---
+++ Mixter.Domain/Identity/SessionProjection.cs
@@ -0,0 +1,19 @@
+﻿namespace Mixter.Domain.Identity
+{
+    public struct SessionProjection
+    {
+        public SessionId SessionId { get; private set; }
+
+        public UserId UserId { get; private set; }
+
+        public SessionState SessionState { get; private set; }
+
+        public SessionProjection(SessionId sessionId, UserId userId, SessionState sessionState)
+            : this()
+        {
+            SessionId = sessionId;
+            UserId = userId;
+            SessionState = sessionState;
+        }
+    }
+}
---
+++ Mixter.Infrastructure.Tests/SessionsRepositoryTest.cs
@@ -0,0 +1,31 @@
+﻿using Mixter.Domain.Identity;
+using NFluent;
+using Xunit;
+
+namespace Mixter.Infrastructure.Tests
+{
+    public class SessionsRepositoryTest
+    {
+        private readonly SessionsRepository _repository = new SessionsRepository();
+
+        [Fact]
+        public void GivenNoProjectionsWhenGetUserIdOfSessionThenReturnEmpty()
+        {
+            var userId = _repository.GetUserIdOfSession(SessionId.Generate());
+
+            Check.That(userId.HasValue).IsFalse();
+        }
+
+        [Fact]
+        public void GivenSeveralUserConnectedWhenGetUserIdOfSessionThenReturnUserIdOfThisSession()
+        {
+            var sessionId = SessionId.Generate();
+            var userId = new UserId("user1@mix-it.fr");
+
+            _repository.Save(new SessionProjection(sessionId, userId, SessionState.Enabled));
+            _repository.Save(new SessionProjection(SessionId.Generate(), new UserId("user2@mix-it.fr"), SessionState.Enabled));
+
+            Check.That(_repository.GetUserIdOfSession(sessionId)).IsEqualTo(userId);
+        }
+    }
+}
