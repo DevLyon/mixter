e065138c0f20ec84e4e0878c5544a2e427caf689
fpellet <florent.pellet@gmail.com>
2015-10-14 19:14:11 +0200

    [Identity][UserIdentity] Given UserRegistered When log in Then raise UserConnected event

---
+++ Mixter.Domain/Identity/Events/UserConnected.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Mixter.Domain.Identity.Events
+{
+    [Event]
+    public struct UserConnected : IDomainEvent
+    {
+        public SessionId SessionId { get; private set; }
+
+        public UserId UserId { get; private set; }
+
+        public DateTime ConnectedAt { get; private set; }
+
+        public UserConnected(SessionId sessionId, UserId userId, DateTime connectedAt)
+            : this()
+        {
+            SessionId = sessionId;
+            UserId = userId;
+            ConnectedAt = connectedAt;
+        }
+
+        public object GetAggregateId()
+        {
+            return SessionId;
+        }
+    }
+}
---
+++ Mixter.Domain/Identity/Session.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Mixter.Domain.Identity.Events;
+
+namespace Mixter.Domain.Identity
+{
+    [Aggregate]
+    public class Session
+    {
+        [Command]
+        public static void LogIn(IEventPublisher eventPublisher, UserId userId)
+        {
+            var id = SessionId.Generate();
+            eventPublisher.Publish(new UserConnected(id, userId, DateTime.Now));
+        }
+    }
+}
