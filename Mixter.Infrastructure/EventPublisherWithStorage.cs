402992cc7a22417354da8e654193abdf48a22414
fpellet <florent.pellet@gmail.com>
2015-10-14 20:09:53 +0200

    [Infra][EventsStore] Create EventPublisherWithStorage

---
+++ Mixter.Infrastructure/EventPublisherWithStorage.cs
@@ -0,0 +1,22 @@
+﻿using Mixter.Domain;
+
+namespace Mixter.Infrastructure
+{
+    public class EventPublisherWithStorage : IEventPublisher
+    {
+        private readonly EventsStore _store;
+        private readonly IEventPublisher _publisher;
+
+        public EventPublisherWithStorage(EventsStore store, IEventPublisher publisher)
+        {
+            _store = store;
+            _publisher = publisher;
+        }
+
+        public void Publish<TEvent>(TEvent evt) where TEvent : IDomainEvent
+        {
+            _store.Store(evt);
+            _publisher.Publish(evt);
+        }
+    }
+}
