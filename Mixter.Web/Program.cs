7baf77dec7ad58aaa52244252674336f32e8823e
fpellet <florent.pellet@gmail.com>
2015-10-14 20:11:50 +0200

    [Api] Create project

---
+++ Mixter.Web/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Mixter.Domain;
+using Mixter.Infrastructure;
+
+var builder = WebApplication.CreateBuilder(args);
+
+var eventsStore = new EventsStore();
+
+var eventPublisher = new EventPublisher();
+var eventPublisherWithStorage = new EventPublisherWithStorage(eventsStore, eventPublisher);
+
+builder.Services.AddSingleton<IEventPublisher>(eventPublisherWithStorage);
+
+var app = builder.Build();
+
+app.MapGet("/", () => Task.FromResult("Hello World, it's Mixter on .NET Core"));
+
+app.Run();
