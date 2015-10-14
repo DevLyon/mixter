1bd1cbbe42d237757b8b188c773f58b32ae51842
fpellet <florent.pellet@gmail.com>
2015-10-14 19:58:38 +0200

    [Identity][SessionHandler] Create SessionHandler

---
+++ Mixter.Domain/Identity/ISessionsRepository.cs
@@ -0,0 +1,9 @@
+﻿namespace Mixter.Domain.Identity
+{
+    public interface ISessionsRepository
+    {
+        void Save(SessionProjection projection);
+
+        UserId? GetUserIdOfSession(SessionId sessionId);
+    }
+}
---
+++ Mixter.Domain/Identity/SessionHandler.cs
@@ -0,0 +1,26 @@
+﻿using Mixter.Domain.Identity.Events;
+
+namespace Mixter.Domain.Identity
+{
+    public class SessionHandler :
+        IEventHandler<UserConnected>,
+        IEventHandler<UserDisconnected>
+    {
+        private readonly ISessionsRepository _repository;
+
+        public SessionHandler(ISessionsRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public void Handle(UserConnected evt)
+        {
+            _repository.Save(new SessionProjection(evt));
+        }
+
+        public void Handle(UserDisconnected evt)
+        {
+            _repository.Save(new SessionProjection(evt));
+        }
+    }
+}
