786fecd342ab63da39959c4e90e52ab90552d2f8
fpellet <florent.pellet@gmail.com>
2015-10-14 21:23:04 +0200

    [Infra][UserIdentitiesRepository] Create UserIdentitiesRepository

---
+++ Mixter.Domain/Identity/UnknownUserIdentity.cs
@@ -0,0 +1,13 @@
+﻿namespace Mixter.Domain.Identity
+{
+    public class UnknownUserIdentity : DomainException
+    {
+        public UserId UserId { get; private set; }
+
+        public UnknownUserIdentity(UserId userId)
+            : base("Unknown user identity with id " + userId)
+        {
+            UserId = userId;
+        }
+    }
+}
---
+++ Mixter.Infrastructure.Tests/UserIdentitiesRepositoryTest.cs
@@ -0,0 +1,36 @@
+﻿using Mixter.Domain.Identity;
+using Mixter.Domain.Identity.Events;
+using NFluent;
+using Xunit;
+
+namespace Mixter.Infrastructure.Tests
+{
+    public class UserIdentitiesRepositoryTest
+    {
+        private readonly UserIdentitiesRepository _repository;
+        private readonly EventsStore _eventStore;
+        private static readonly UserId UserId = new UserId("user@mix-it.fr");
+
+        public UserIdentitiesRepositoryTest()
+        {
+            _eventStore = new EventsStore();
+            _repository = new UserIdentitiesRepository(_eventStore);
+        }
+
+        [Fact]
+        public void GivenUserRegisteredWhenGetUserIdentityThenReturnUserIdentityAggregate()
+        {
+            _eventStore.Store(new UserRegistered(UserId));
+
+            var user = _repository.GetUserIdentity(UserId);
+
+            Check.That(user).IsNotNull();
+        }
+
+        [Fact]
+        public void GivenNoEventsWhenGetUserIdentityThenThrowUnknownUserIdentity()
+        {
+            Check.ThatCode(() => _repository.GetUserIdentity(UserId)).Throws<UnknownUserIdentity>();
+        }
+    }
+}
