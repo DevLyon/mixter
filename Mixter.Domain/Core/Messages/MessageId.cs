800c1b3657af86c6e04db8255508550e5102776d
fpellet <florent.pellet@gmail.com>
2015-10-14 21:48:14 +0200

    [Core][Message] Create MessageId

---
+++ Mixter.Domain/Core/Messages/MessageId.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Mixter.Domain.Core.Messages
+{
+    public struct MessageId
+    {
+        private MessageId(string id)
+            : this()
+        {
+            Id = id;
+        }
+
+        public string Id { get; private set; }
+
+        public static MessageId Generate()
+        {
+            return new MessageId(Guid.NewGuid().ToString());
+        }
+
+        public override string ToString()
+        {
+            return Id;
+        }
+    }
+}
