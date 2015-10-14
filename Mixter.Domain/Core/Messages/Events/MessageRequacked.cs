41218f28e561da994495561368dabde6ebba79c2
fpellet <florent.pellet@gmail.com>
2015-10-14 22:06:53 +0200

    [Core][Message] When requack message Then raise MessageRequacked

---
+++ Mixter.Domain/Core/Messages/Events/MessageRequacked.cs
@@ -0,0 +1,24 @@
+﻿using Mixter.Domain.Identity;
+
+namespace Mixter.Domain.Core.Messages.Events
+{
+    [Event]
+    public struct MessageRequacked : IDomainEvent
+    {
+        public MessageId Id { get; private set; }
+
+        public UserId Requacker { get; private set; }
+
+        public MessageRequacked(MessageId id, UserId requacker)
+            : this()
+        {
+            Id = id;
+            Requacker = requacker;
+        }
+
+        public object GetAggregateId()
+        {
+            return Id;
+        }
+    }
+}
--- Mixter.Domain/Core/Messages/Message.cs
+++ Mixter.Domain/Core/Messages/Message.cs
@@ -1,4 +1,5 @@
-﻿using Mixter.Domain.Core.Messages.Events;
+﻿using System.Collections.Generic;
+using Mixter.Domain.Core.Messages.Events;
 using Mixter.Domain.Identity;
 
 namespace Mixter.Domain.Core.Messages
@@ -6,12 +7,45 @@ namespace Mixter.Domain.Core.Messages
     [Aggregate]
     public class Message
     {
+        private readonly DecisionProjection _projection = new DecisionProjection();
+
+        public Message(IEnumerable<IDomainEvent> events)
+        {
+            foreach (var @event in events)
+            {
+                _projection.Apply(@event);
+            }
+        }
+
         [Command]
         public static MessageId Quack(IEventPublisher eventPublisher, UserId author, string content)
         {
             var messageId = MessageId.Generate();
             eventPublisher.Publish(new MessageQuacked(messageId, author, content));
             return messageId;
+        }
+
+        [Command]
+        public void Requack(IEventPublisher eventPublisher, UserId requacker)
+        {
+            var evt = new MessageRequacked(_projection.Id, requacker);
+            eventPublisher.Publish(evt);
+        }
+
+        [Projection]
+        private class DecisionProjection : DecisionProjectionBase
+        {
+            public MessageId Id { get; private set; }
+
+            public DecisionProjection()
+            {
+                AddHandler<MessageQuacked>(When);
+            }
+            
+            private void When(MessageQuacked evt)
+            {
+                Id = evt.Id;
+            }
         }
     }
 }
