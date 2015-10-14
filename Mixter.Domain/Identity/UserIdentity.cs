2e963d5861443b90887d0b0a73debf6acb777d89
fpellet <florent.pellet@gmail.com>
2015-10-14 19:14:11 +0200

    [Identity][UserIdentity] Given UserRegistered When log in Then raise UserConnected event

--- Mixter.Domain/Identity/UserIdentity.cs
+++ Mixter.Domain/Identity/UserIdentity.cs
@@ -5,10 +5,42 @@ namespace Mixter.Domain.Identity
     [Aggregate]
     public class UserIdentity
     {
+        private readonly DecisionProjection _projection = new DecisionProjection();
+
+        public UserIdentity(params IDomainEvent[] events)
+        {
+            foreach (var @event in events)
+            {
+                _projection.Apply(@event);
+            }
+        }
+
         [Command]
         public static void Register(IEventPublisher eventPublisher, UserId userId)
         {
             eventPublisher.Publish(new UserRegistered(userId));
+        }
+
+        [Command]
+        public void LogIn(IEventPublisher eventPublisher)
+        {
+            Session.LogIn(eventPublisher, _projection.Id);
+        }
+
+        [Projection]
+        private class DecisionProjection
+        {
+            public UserId Id { get; private set; }
+
+            public void Apply(IDomainEvent @event)
+            {
+                When((dynamic)@event);
+            }
+
+            private void When(UserRegistered evt)
+            {
+                Id = evt.UserId;
+            }
         }
     }
 }
