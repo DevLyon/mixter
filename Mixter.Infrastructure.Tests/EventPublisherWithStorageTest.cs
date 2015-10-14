86270ff3d3a2f90f5eb759860eb46f0ca704128a
fpellet <florent.pellet@gmail.com>
2015-10-14 20:09:53 +0200

    [Infra][EventsStore] Create EventPublisherWithStorage

---
+++ Mixter.Infrastructure.Tests/EventPublisherWithStorageTest.cs
@@ -0,0 +1,45 @@
+﻿using Mixter.Domain;
+using Mixter.Domain.Tests;
+using NFluent;
+using Xunit;
+
+namespace Mixter.Infrastructure.Tests
+{
+    public class EventPublisherWithStorageTest
+    {
+        private readonly EventsStore _store;
+        private readonly EventPublisherWithStorage _publisher;
+        private readonly EventPublisherFake _publisherBase;
+
+        public EventPublisherWithStorageTest()
+        {
+            _store = new EventsStore();
+            _publisherBase = new EventPublisherFake();
+            _publisher = new EventPublisherWithStorage(_store, _publisherBase);
+        }
+
+        [Fact]
+        public void WhenPublishEventThenStoreInDatabase()
+        {
+            _publisher.Publish(new EventA());
+
+            Check.That(_store.GetEventsOfAggregate("A")).HasSize(1);
+        }
+
+        [Fact]
+        public void WhenPublishEventThenCallEventHandlerBase()
+        {
+            _publisher.Publish(new EventA());
+
+            Check.That(_publisherBase.Events).HasSize(1);
+        }
+
+        private struct EventA : IDomainEvent
+        {
+            public object GetAggregateId()
+            {
+                return "A";
+            }
+        }
+    }
+}
