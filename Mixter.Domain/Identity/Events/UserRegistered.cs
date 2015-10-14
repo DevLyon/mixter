badfc6735bc1f764f9c22fc056c030782f236e17
fpellet <florent.pellet@gmail.com>
2015-10-14 19:06:29 +0200

    [Identity][UserIdentity] When register user Then raise UserRegistered event

---
+++ Mixter.Domain/Identity/Events/UserRegistered.cs
@@ -0,0 +1,19 @@
+﻿namespace Mixter.Domain.Identity.Events
+{
+    [Event]
+    public struct UserRegistered : IDomainEvent
+    {
+        public UserId UserId { get; private set; }
+
+        public UserRegistered(UserId userId)
+            : this()
+        {
+            UserId = userId;
+        }
+
+        public object GetAggregateId()
+        {
+            return UserId;
+        }
+    }
+}
