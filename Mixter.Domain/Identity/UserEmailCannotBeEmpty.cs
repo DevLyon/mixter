5511444a02d0e05178f21d52b6429c5068c0d059
fpellet <florent.pellet@gmail.com>
2015-10-14 20:58:25 +0200

    [Api] Manage errors

--- Mixter.Domain/Identity/UserEmailCannotBeEmpty.cs
+++ Mixter.Domain/Identity/UserEmailCannotBeEmpty.cs
@@ -1,8 +1,6 @@
-using System;
-
 namespace Mixter.Domain.Identity
 {
-    public class UserEmailCannotBeEmpty : Exception
+    public class UserEmailCannotBeEmpty : DomainException
     {
         public UserEmailCannotBeEmpty()
             : base("Email cannot be empty")
