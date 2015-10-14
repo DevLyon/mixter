b73c8e814d4e009ce756c0c916f68c029f8b10e8
fpellet <florent.pellet@gmail.com>
2015-10-14 21:23:04 +0200

    [Infra][UserIdentitiesRepository] Create UserIdentitiesRepository

---
+++ Mixter.Infrastructure/UserIdentitiesRepository.cs
@@ -0,0 +1,26 @@
+﻿using System.Linq;
+using Mixter.Domain.Identity;
+
+namespace Mixter.Infrastructure
+{
+    public class UserIdentitiesRepository
+    {
+        private readonly EventsStore _eventStore;
+
+        public UserIdentitiesRepository(EventsStore eventStore)
+        {
+            _eventStore = eventStore;
+        }
+
+        public UserIdentity GetUserIdentity(UserId userId)
+        {
+            var events = _eventStore.GetEventsOfAggregate(userId).ToArray();
+            if (!events.Any())
+            {
+                throw new UnknownUserIdentity(userId);
+            }
+
+            return new UserIdentity(events);
+        }
+    }
+}
