e0995696b5ff6cd0ab70a08f5e35577f1aaa1a60
fpellet <florent.pellet@gmail.com>
2015-10-14 19:18:47 +0200

    [Identity][Session] When user logout Then raise UserDisconnected event

--- Mixter.Domain/Identity/Session.cs
+++ Mixter.Domain/Identity/Session.cs
@@ -6,11 +6,46 @@ namespace Mixter.Domain.Identity
     [Aggregate]
     public class Session
     {
+        private readonly DecisionProjection _projection = new DecisionProjection();
+
+        public Session(params IDomainEvent[] events)
+        {
+            foreach (var @event in events)
+            {
+                _projection.Apply(@event);
+            }
+        }
+
         [Command]
         public static void LogIn(IEventPublisher eventPublisher, UserId userId)
         {
             var id = SessionId.Generate();
             eventPublisher.Publish(new UserConnected(id, userId, DateTime.Now));
+        }
+
+        [Command]
+        public void Logout(IEventPublisher eventPublisher)
+        {
+            eventPublisher.Publish(new UserDisconnected(_projection.Id, _projection.UserId));
+        }
+
+        [Projection]
+        private class DecisionProjection
+        {
+            public SessionId Id { get; private set; }
+
+            public UserId UserId { get; private set; }
+
+            public void Apply(IDomainEvent @event)
+            {
+                When((dynamic)@event);
+            }
+
+            private void When(UserConnected evt)
+            {
+                Id = evt.SessionId;
+                UserId = evt.UserId;
+            }
         }
     }
 }
