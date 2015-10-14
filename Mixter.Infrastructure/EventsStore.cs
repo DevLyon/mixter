b6deb6edf82df423942a0b22fe5533f0afaf0b9e
fpellet <florent.pellet@gmail.com>
2015-10-14 20:08:04 +0200

    [Infra][EventsStore] Create EventsStore

---
+++ Mixter.Infrastructure/EventsStore.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Mixter.Domain;
+
+namespace Mixter.Infrastructure
+{
+    public class EventsStore
+    {
+        private readonly IList<IDomainEvent> _events = new List<IDomainEvent>();
+
+        public void Store(IDomainEvent evt)
+        {
+            _events.Add(evt);
+        }
+
+        public IEnumerable<IDomainEvent> GetEventsOfAggregate<TAggregateId>(TAggregateId id)
+        {
+            return _events.Where(o => o.GetAggregateId().Equals(id));
+        }
+    }
+}
