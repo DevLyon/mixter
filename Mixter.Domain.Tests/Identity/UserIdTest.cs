688d21dc2bc7e032a9aa87f0e5fa646db1f80b44
fpellet <florent.pellet@gmail.com>
2015-10-14 19:04:32 +0200

    [Identity][UserIdentity] Create UserId

---
+++ Mixter.Domain.Tests/Identity/UserIdTest.cs
@@ -0,0 +1,34 @@
+﻿using Mixter.Domain.Identity;
+using NFluent;
+using Xunit;
+
+namespace Mixter.Domain.Tests.Identity
+{
+    public class UserIdTest
+    {
+        private const string UserEmail = "test@mixit.fr";
+
+        [Fact]
+        public void WhenCreate2IdWithSameEmailsThenAreEquals()
+        {
+            var userId1 = new UserId(UserEmail);
+            var userId2 = new UserId(UserEmail);
+
+            Check.That(userId1).IsEqualTo(userId2);
+        }
+
+        [Fact]
+        public void WhenToStringIdThenEmail()
+        {
+            var userId1 = new UserId(UserEmail);
+
+            Check.That(userId1.ToString()).IsEqualTo(UserEmail);
+        }
+
+        [Fact]
+        public void WhenCreateUserIdWithEmptyEmailThenThrowUserEmailCannotBeEmpty()
+        {
+            Check.ThatCode(() => new UserId(string.Empty)).Throws<UserEmailCannotBeEmpty>();
+        }
+    }
+}
