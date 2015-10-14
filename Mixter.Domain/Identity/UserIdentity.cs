ed2d8283c49570a1f9ac358abc416908df46af29
fpellet <florent.pellet@gmail.com>
2015-10-14 19:06:29 +0200

    [Identity][UserIdentity] When register user Then raise UserRegistered event

---
+++ Mixter.Domain/Identity/UserIdentity.cs
@@ -0,0 +1,14 @@
+﻿using Mixter.Domain.Identity.Events;
+
+namespace Mixter.Domain.Identity
+{
+    [Aggregate]
+    public class UserIdentity
+    {
+        [Command]
+        public static void Register(IEventPublisher eventPublisher, UserId userId)
+        {
+            eventPublisher.Publish(new UserRegistered(userId));
+        }
+    }
+}
