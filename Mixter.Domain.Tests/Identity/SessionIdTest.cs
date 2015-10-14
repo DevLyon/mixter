dbdc018a49a17b8322c1a67a588bed15b70ebe63
fpellet <florent.pellet@gmail.com>
2015-10-14 19:14:11 +0200

    [Identity][UserIdentity] Given UserRegistered When log in Then raise UserConnected event

---
+++ Mixter.Domain.Tests/Identity/SessionIdTest.cs
@@ -0,0 +1,18 @@
+﻿using Mixter.Domain.Identity;
+using NFluent;
+using Xunit;
+
+namespace Mixter.Domain.Tests.Identity
+{
+    public class SessionIdTest
+    {
+        [Fact]
+        public void WhenGenerate2SessionIdThenIsNotEquals()
+        {
+            var sessionId1 = SessionId.Generate();
+            var sessionId2 = SessionId.Generate();
+
+            Check.That(sessionId1).IsNotEqualTo(sessionId2);
+        }
+    }
+}
