d77495eb0338b5ee2b50edb5a9e697554b5c4132
fpellet <florent.pellet@gmail.com>
2015-10-14 21:58:53 +0200

    [Core][Message] When quack message Then raise UserMessageQuacked

---
+++ Mixter.Domain/Core/Messages/Message.cs
@@ -0,0 +1,17 @@
+﻿using Mixter.Domain.Core.Messages.Events;
+using Mixter.Domain.Identity;
+
+namespace Mixter.Domain.Core.Messages
+{
+    [Aggregate]
+    public class Message
+    {
+        [Command]
+        public static MessageId Quack(IEventPublisher eventPublisher, UserId author, string content)
+        {
+            var messageId = MessageId.Generate();
+            eventPublisher.Publish(new MessageQuacked(messageId, author, content));
+            return messageId;
+        }
+    }
+}
