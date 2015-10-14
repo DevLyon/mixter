f5e22013dcf85838e9823c948072720790c2707a
fpellet <florent.pellet@gmail.com>
2015-10-14 21:48:14 +0200

    [Core][Message] Create MessageId

---
+++ Mixter.Domain.Tests/Core/Messages/MessageIdTest.cs
@@ -0,0 +1,26 @@
+﻿using Mixter.Domain.Core.Messages;
+using NFluent;
+using Xunit;
+
+namespace Mixter.Domain.Tests.Core.Messages
+{
+    public class MessageIdTest
+    {
+        [Fact]
+        public void WhenGenerate2IdThenIsNotEquals()
+        {
+            var id1 = MessageId.Generate();
+            var id2 = MessageId.Generate();
+
+            Check.That(id1).IsNotEqualTo(id2);
+        }
+
+        [Fact]
+        public void WhenToStringIdThenId()
+        {
+            var id = MessageId.Generate();
+
+            Check.That(id.ToString()).IsEqualTo(id.Id);
+        }
+    }
+}
