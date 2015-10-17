7ee8d09ab0b8a6ec866cba90b7612a67aeab4acd
fpellet <florent.pellet@gmail.com>
2015-10-17 20:32:02 +0200

    [Core][Subscription][Test KO] When Follow Then UserFollowed is raised

--- Mixter.Domain.Tests/Core/Subscriptions/SubscriptionTest.cs
+++ Mixter.Domain.Tests/Core/Subscriptions/SubscriptionTest.cs
@@ -1,6 +1,30 @@
-﻿namespace Mixter.Domain.Tests.Core.Subscriptions
+﻿using Mixter.Domain.Core.Subscriptions;
+using Mixter.Domain.Core.Subscriptions.Events;
+using Mixter.Domain.Identity;
+using NFluent;
+using Xunit;
+
+namespace Mixter.Domain.Tests.Core.Subscriptions
 {
     public class SubscriptionTest
     {
+        private static readonly UserId Follower = new UserId("emilien@mixit.fr");
+        private static readonly UserId Followee = new UserId("florent@mixit.fr");
+        private static readonly SubscriptionId SubscriptionId = new SubscriptionId(Follower, Followee);
+
+        private readonly EventPublisherFake _eventPublisher;
+
+        public SubscriptionTest()
+        {
+            _eventPublisher = new EventPublisherFake();
+        }
+
+        [Fact]
+        public void WhenFollowThenUserFollowedIsRaised()
+        {
+            Subscription.FollowUser(_eventPublisher, Follower, Followee);
+
+            Check.That(_eventPublisher.Events).Contains(new UserFollowed(SubscriptionId));
+        }
     }
 }
