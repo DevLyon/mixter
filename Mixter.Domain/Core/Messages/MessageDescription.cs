22438294ab3b5ba2f0d31ed2a4ce51827128e6cb
fpellet <florent.pellet@gmail.com>
2015-10-17 20:52:40 +0200

    [Infra][MessagesRepository] Given MessageQuacked Then getDescription Then return MessageDescription

---
+++ Mixter.Domain/Core/Messages/MessageDescription.cs
@@ -0,0 +1,25 @@
+﻿using Mixter.Domain.Core.Messages.Events;
+using Mixter.Domain.Identity;
+
+namespace Mixter.Domain.Core.Messages
+{
+    [Projection]
+    public struct MessageDescription
+    {
+        public UserId Author { get; private set; }
+
+        public string Content { get; private set; }
+
+        public MessageDescription(MessageQuacked evt)
+            : this(evt.Author, evt.Content)
+        {
+        }
+
+        public MessageDescription(UserId author, string content)
+            : this()
+        {
+            Author = author;
+            Content = content;
+        }
+    }
+}
