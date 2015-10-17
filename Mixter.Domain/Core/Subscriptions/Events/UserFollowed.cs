f99f67596f343c35a2104015f774cde76478abd4
fpellet <florent.pellet@gmail.com>
2015-10-17 20:29:25 +0200

    [Core][Subscription] Initialize Subscription

---
+++ Mixter.Domain/Core/Subscriptions/Events/UserFollowed.cs
@@ -0,0 +1,19 @@
+﻿namespace Mixter.Domain.Core.Subscriptions.Events
+{
+    [Event]
+    public struct UserFollowed : IDomainEvent
+    {
+        public SubscriptionId SubscriptionId { get; private set; }
+
+        public UserFollowed(SubscriptionId subscriptionId)
+            : this()
+        {
+            SubscriptionId = subscriptionId;
+        }
+
+        public object GetAggregateId()
+        {
+            return SubscriptionId;
+        }
+    }
+}
