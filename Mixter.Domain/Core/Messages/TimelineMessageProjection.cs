5ad1c0815d682a567d4c0720f47aec08b5b29861
fpellet <florent.pellet@gmail.com>
2015-10-17 19:58:10 +0200

    [Infra][TimelineMessageRepository] Given a message saved When getMessageOfUser Then message is returned

---
+++ Mixter.Domain/Core/Messages/TimelineMessageProjection.cs
@@ -0,0 +1,25 @@
+﻿using Mixter.Domain.Identity;
+
+namespace Mixter.Domain.Core.Messages
+{
+    [Projection]
+    public struct TimelineMessageProjection
+    {
+        public UserId OwnerId { get; private set; }
+
+        public UserId AuthorId { get; private set; }
+
+        public string Content { get; private set; }
+
+        public MessageId MessageId { get; private set; }
+
+        public TimelineMessageProjection(UserId ownerId, UserId authorId, string content, MessageId messageId)
+            : this()
+        {
+            OwnerId = ownerId;
+            AuthorId = authorId;
+            Content = content;
+            MessageId = messageId;
+        }
+    }
+}
