b2249169b0c5b912d923fd607ab9207eddeb0439
fpellet <florent.pellet@gmail.com>
2015-10-17 19:58:10 +0200

    [Infra][TimelineMessageRepository] Given a message saved When getMessageOfUser Then message is returned

---
+++ Mixter.Domain/Core/Messages/ITimelineMessageRepository.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using Mixter.Domain.Identity;
+
+namespace Mixter.Domain.Core.Messages
+{
+    public interface ITimelineMessageRepository
+    {
+        void Save(TimelineMessageProjection messageProjection);
+        IEnumerable<TimelineMessageProjection> GetMessagesOfUser(UserId userId);
+    }
+}
