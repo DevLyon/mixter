07330481bf1d1f39d6764611a22a07a8bf91c04b
fpellet <florent.pellet@gmail.com>
2015-10-17 20:29:25 +0200

    [Core][Subscription] Initialize Subscription

---
+++ Mixter.Domain/Core/Subscriptions/Events/FolloweeMessageQuacked.cs
@@ -0,0 +1,24 @@
+﻿using Mixter.Domain.Core.Messages;
+
+namespace Mixter.Domain.Core.Subscriptions.Events
+{
+    [Event]
+    public struct FolloweeMessageQuacked : IDomainEvent
+    {
+        public SubscriptionId SubscriptionId { get; private set; }
+
+        public MessageId MessageId { get; private set; }
+
+        public FolloweeMessageQuacked(SubscriptionId subscriptionId, MessageId messageId)
+            : this()
+        {
+            SubscriptionId = subscriptionId;
+            MessageId = messageId;
+        }
+
+        public object GetAggregateId()
+        {
+            return SubscriptionId;
+        }
+    }
+}
