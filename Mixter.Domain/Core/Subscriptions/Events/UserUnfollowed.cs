9046ee392b9056ef88a186ec2bd6d7d7de75e80e
fpellet <florent.pellet@gmail.com>
2015-10-17 20:29:25 +0200

    [Core][Subscription] Initialize Subscription

---
+++ Mixter.Domain/Core/Subscriptions/Events/UserUnfollowed.cs
@@ -0,0 +1,18 @@
+﻿namespace Mixter.Domain.Core.Subscriptions.Events
+{
+    [Event]
+    public struct UserUnfollowed : IDomainEvent
+    {
+        public SubscriptionId SubscriptionId { get; private set; }
+
+        public UserUnfollowed(SubscriptionId subscriptionId) : this()
+        {
+            SubscriptionId = subscriptionId;
+        }
+
+        public object GetAggregateId()
+        {
+            return SubscriptionId;
+        }
+    }
+}
---
+++ Mixter.Domain/Core/Subscriptions/SubscriptionId.cs
@@ -0,0 +1,18 @@
+﻿using Mixter.Domain.Identity;
+
+namespace Mixter.Domain.Core.Subscriptions
+{
+    public struct SubscriptionId
+    {
+        public UserId Follower { get; private set; }
+
+        public UserId Followee { get; private set; }
+
+        public SubscriptionId(UserId follower, UserId followee)
+            : this()
+        {
+            Follower = follower;
+            Followee = followee;
+        }
+    }
+}
