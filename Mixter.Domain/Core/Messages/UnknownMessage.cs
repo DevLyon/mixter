f077a5399596e195981241886c18f2c3d07fa564
fpellet <florent.pellet@gmail.com>
2015-10-17 21:03:09 +0200

    [Infra][MessagesRepository] Given no events When GetMessage Then throw UnknownMessage

---
+++ Mixter.Domain/Core/Messages/UnknownMessage.cs
@@ -0,0 +1,13 @@
+﻿namespace Mixter.Domain.Core.Messages
+{
+    public class UnknownMessage : DomainException
+    {
+        public MessageId MessageId { get; private set; }
+
+        public UnknownMessage(MessageId messageId)
+            : base("Unknown message with id " + messageId)
+        {
+            MessageId = messageId;
+        }
+    }
+}
