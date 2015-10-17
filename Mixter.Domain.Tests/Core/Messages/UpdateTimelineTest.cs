8996630e44fa3bebe6cc8def88dc309ed05e437c
fpellet <florent.pellet@gmail.com>
2015-10-17 20:07:36 +0200

    [Core][UpdateTimeline] Initialize UpdateTimeline handler

---
+++ Mixter.Domain.Tests/Core/Messages/UpdateTimelineTest.cs
@@ -0,0 +1,14 @@
+﻿using Mixter.Domain.Core.Messages.Handlers;
+
+namespace Mixter.Domain.Tests.Core.Messages
+{
+    public class UpdateTimelineTest
+    {
+        private readonly UpdateTimeline _handler;
+
+        public UpdateTimelineTest()
+        {
+            _handler = new UpdateTimeline();
+        }
+    }
+}
