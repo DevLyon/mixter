6551a2d18a2225ea95fb39a0917381204c0943aa
fpellet <florent.pellet@gmail.com>
2015-10-17 20:04:27 +0200

    [Infra][TimelineMessageRepository] When save two messages of different owners Then GetMessagesOfUser return only messages of user

--- Mixter.Infrastructure.Tests/TimelineMessagesRepositoryTest.cs
+++ Mixter.Infrastructure.Tests/TimelineMessagesRepositoryTest.cs
@@ -1,4 +1,5 @@
-﻿using Mixter.Domain.Core.Messages;
+﻿using System.Linq;
+using Mixter.Domain.Core.Messages;
 using Mixter.Domain.Identity;
 using NFluent;
 using Xunit;
@@ -22,6 +23,20 @@ namespace Mixter.Infrastructure.Tests
             _repository.Save(new TimelineMessageProjection(_ownerId, _authorId, "MessageA", MessageId.Generate()));
 
             Check.That(_repository.GetMessagesOfUser(_ownerId)).HasSize(1);
+        }
+
+        [Fact]
+        public void WhenSaveTwoMessagesOfDifferentOwnersThenGetMessagesOfUserThisMessage()
+        {
+            const string messageA = "MessageA";
+            const string messageB = "MessageB";
+
+            _repository.Save(new TimelineMessageProjection(_ownerId, _authorId, messageA, MessageId.Generate()));
+            _repository.Save(new TimelineMessageProjection(new UserId("florent@mixit.fr"), _authorId, messageB, MessageId.Generate()));
+
+            var messagesOfUser = _repository.GetMessagesOfUser(_ownerId).ToList();
+            Check.That(messagesOfUser).HasSize(1);
+            Check.That(messagesOfUser.First().Content).IsEqualTo(messageA);
         }
     }
 }
