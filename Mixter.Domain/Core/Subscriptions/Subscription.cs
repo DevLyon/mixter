95be338861d13ea4e14375283f4941904a7b8057
fpellet <florent.pellet@gmail.com>
2015-10-17 20:36:01 +0200

    [Core][Subscription][Test OK] When unfollow Then UserUnfollowed is raised

--- Mixter.Domain/Core/Subscriptions/Subscription.cs
+++ Mixter.Domain/Core/Subscriptions/Subscription.cs
@@ -1,14 +1,47 @@
-﻿using Mixter.Domain.Core.Subscriptions.Events;
+﻿using System.Collections.Generic;
+using Mixter.Domain.Core.Subscriptions.Events;
 using Mixter.Domain.Identity;
 
 namespace Mixter.Domain.Core.Subscriptions
 {
     public class Subscription
     {
+        private readonly DecisionProjection _projection;
+
+        public Subscription(IEnumerable<IDomainEvent> events)
+        {
+            _projection = new DecisionProjection();
+
+            foreach (var evt in events)
+            {
+                _projection.Apply(evt);
+            }
+        }
+
         public static void FollowUser(IEventPublisher eventPublisher, UserId follower, UserId followee)
         {
             var userFollowed = new UserFollowed(new SubscriptionId(follower, followee));
             eventPublisher.Publish(userFollowed);
+        }
+
+        public void Unfollow(IEventPublisher eventPublisher)
+        {
+            eventPublisher.Publish(new UserUnfollowed(_projection.Id));
+        }
+
+        private class DecisionProjection : DecisionProjectionBase
+        {
+            public DecisionProjection()
+            {
+                AddHandler<UserFollowed>(When);
+            }
+
+            public SubscriptionId Id { get; private set; }
+
+            private void When(UserFollowed evt)
+            {
+                Id = evt.SubscriptionId;
+            }
         }
     }
 }
