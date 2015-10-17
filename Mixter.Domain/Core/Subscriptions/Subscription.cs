60534695e1f4db8f4327299a43738b77fded5e09
fpellet <florent.pellet@gmail.com>
2015-10-17 20:32:53 +0200

    [Core][Subscription][Test OK] When Follow Then UserFollowed is raised

--- Mixter.Domain/Core/Subscriptions/Subscription.cs
+++ Mixter.Domain/Core/Subscriptions/Subscription.cs
@@ -1,7 +1,16 @@
-﻿namespace Mixter.Domain.Core.Subscriptions
+﻿using Mixter.Domain.Core.Subscriptions.Events;
+using Mixter.Domain.Identity;
+
+namespace Mixter.Domain.Core.Subscriptions
 {
     [Aggregate]
     public class Subscription
     {
+        [Command]
+        public static void FollowUser(IEventPublisher eventPublisher, UserId follower, UserId followee)
+        {
+            var userFollowed = new UserFollowed(new SubscriptionId(follower, followee));
+            eventPublisher.Publish(userFollowed);
+        }
     }
 }
