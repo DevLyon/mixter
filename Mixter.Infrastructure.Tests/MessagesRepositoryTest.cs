14ea241c3fced6c5461052cd32f3ada8e6654b1e
fpellet <florent.pellet@gmail.com>
2015-10-17 20:48:26 +0200

    [Infra][MessagesRepository] Given MessageQuacked When GetMessage Then return Message aggregate

---
+++ Mixter.Infrastructure.Tests/MessagesRepositoryTest.cs
@@ -0,0 +1,35 @@
+﻿using System.Linq;
+using Mixter.Domain.Core.Messages;
+using Mixter.Domain.Core.Messages.Events;
+using Mixter.Domain.Identity;
+using Mixter.Domain.Tests;
+using NFluent;
+using Xunit;
+
+namespace Mixter.Infrastructure.Tests
+{
+    public class MessagesRepositoryTest
+    {
+        private readonly EventsStore _eventsStore;
+        private readonly MessagesRepository _repository;
+
+        public MessagesRepositoryTest()
+        {
+            _eventsStore = new EventsStore();
+            _repository = new MessagesRepository(_eventsStore);
+        }
+
+        [Fact]
+        public void GivenMessageQuackedThenGetMessageThenReturnTheMessage()
+        {
+            var messageQuacked = new MessageQuacked(MessageId.Generate(), new UserId("bob@mixit.fr"), "Hello");
+            _eventsStore.Store(messageQuacked);
+
+            var message = _repository.Get(messageQuacked.Id);
+
+            var eventsPublisher = new EventPublisherFake();
+            message.Requack(eventsPublisher, new UserId("joe@mitit.fr"));
+            Check.That(eventsPublisher.Events.Cast<MessageRequacked>().Single().Id).IsEqualTo(messageQuacked.Id);
+        }
+    }
+}
---
+++ Mixter.Infrastructure/MessagesRepository.cs
@@ -0,0 +1,19 @@
+﻿using Mixter.Domain.Core.Messages;
+
+namespace Mixter.Infrastructure
+{
+    public class MessagesRepository
+    {
+        private readonly EventsStore _eventsStore;
+
+        public MessagesRepository(EventsStore eventsStore)
+        {
+            _eventsStore = eventsStore;
+        }
+
+        public Message Get(MessageId id)
+        {
+            return new Message(_eventsStore.GetEventsOfAggregate(id));
+        }
+    }
+}
