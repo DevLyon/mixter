d0889c717858667d585cccbc1f7c4a2b9bde13cf
fpellet <florent.pellet@gmail.com>
2015-10-17 19:42:35 +0200

    [Core][Message][Test KO] When delete Then raise MessageDeleted

---
+++ Mixter.Domain/Core/Messages/Events/MessageDeleted.cs
@@ -0,0 +1,24 @@
+﻿using Mixter.Domain.Identity;
+
+namespace Mixter.Domain.Core.Messages.Events
+{
+    [Event]
+    public struct MessageDeleted : IDomainEvent
+    {
+        public MessageId MessageId { get; private set; }
+
+        public UserId Deleter { get; private set; }
+
+        public MessageDeleted(MessageId messageId, UserId deleter) 
+            : this()
+        {
+            MessageId = messageId;
+            Deleter = deleter;
+        }
+
+        public object GetAggregateId()
+        {
+            return MessageId;
+        }
+    }
+}
