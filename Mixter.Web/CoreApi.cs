73016d108920b61c7273fa734fbb1965a7a819b4
fpellet <florent.pellet@gmail.com>
2015-10-18 13:13:25 +0200

    [Api][Core] Exposes delete message

--- Mixter.Web/CoreApi.cs
+++ Mixter.Web/CoreApi.cs
@@ -9,11 +9,12 @@ namespace Mixter.Web
 {
     public class CoreApi : NancyModule
     {
-        public CoreApi(IEventPublisher eventPublisher, ITimelineMessageRepository timelineMessageRepository)
+        public CoreApi(IEventPublisher eventPublisher, ITimelineMessageRepository timelineMessageRepository, ISessionsRepository sessionsRepository, IMessagesRepository messagesRepository)
             : base("/api/core")
         {
             Post("/messages/quack", _ => Execute(eventPublisher, this.Bind<QuackMessage>()));
-            Get("/timelineMessages/{author}", _ => Execute(eventPublisher, timelineMessageRepository, (string)_.author));
+            Delete("/messages/{id}", _ => Execute(eventPublisher, sessionsRepository, messagesRepository, this.Bind<DeleteMessage>().WithMessageId((string)_.id)));
+            Get("/timelineMessages/{author}", _ => Execute(timelineMessageRepository, (string)_.author));
         }
 
         private dynamic Execute(IEventPublisher eventPublisher, QuackMessage command)
@@ -27,7 +28,25 @@ namespace Mixter.Web
             });
         }
 
-        private dynamic Execute(IEventPublisher eventPublisher, ITimelineMessageRepository timelineMessageRepository, string author)
+        private dynamic Execute(IEventPublisher eventPublisher, ISessionsRepository sessionsRepository, IMessagesRepository messagesRepository, DeleteMessage command)
+        {
+            var sessionId = new SessionId(command.SessionId);
+
+            var deleter = sessionsRepository.GetUserIdOfSession(sessionId);
+            if (!deleter.HasValue)
+            {
+                return Negotiate.WithStatusCode(HttpStatusCode.Forbidden).WithModel("Invalid session");
+            }
+            
+            var messageId = new MessageId(command.MessageId);
+            var messageToDeleted = messagesRepository.Get(messageId);
+
+            messageToDeleted.Delete(eventPublisher, deleter.Value);
+
+            return Negotiate.WithStatusCode(HttpStatusCode.OK).WithModel("Message deleted");
+        }
+
+        private dynamic Execute(ITimelineMessageRepository timelineMessageRepository, string author)
         {
             var messages = timelineMessageRepository.GetMessagesOfUser(new UserId(author));
 
@@ -39,6 +58,20 @@ namespace Mixter.Web
             public string Author { get; set; }
 
             public string Content { get; set; }
+        }
+
+        private class DeleteMessage
+        {
+            public string SessionId { get; set; }
+
+            public string MessageId { get; set; }
+
+            public DeleteMessage WithMessageId(string id)
+            {
+                MessageId = id;
+
+                return this;
+            }
         }
     }
 }
