3efd533188f12db2ef19f8121e54bbb1f7a66731
fpellet <florent.pellet@gmail.com>
2015-10-18 12:13:50 +0200

    [Infra][SubscriptionsRepository] Given Not Event When GetSubscription Then Throw UnknownSubscription

---
+++ Mixter.Domain/Core/Subscriptions/UnknownSubscription.cs
@@ -0,0 +1,13 @@
+﻿namespace Mixter.Domain.Core.Subscriptions
+{
+    public class UnknownSubscription : DomainException
+    {
+        public SubscriptionId SubscriptionId { get; private set; }
+
+        public UnknownSubscription(SubscriptionId subscriptionId)
+            : base("Unknown subscription with id " + subscriptionId)
+        {
+            SubscriptionId = subscriptionId;
+        }
+    }
+}
