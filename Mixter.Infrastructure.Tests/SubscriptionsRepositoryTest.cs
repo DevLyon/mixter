00addd4848ada1e249aadf063fb9270a845c4d89
fpellet <florent.pellet@gmail.com>
2015-10-18 12:10:32 +0200

    [Infra][SubscriptionsRepository] Given UserFollowed When getSubscription Then return Subscription aggregate

---
+++ Mixter.Infrastructure.Tests/SubscriptionsRepositoryTest.cs
@@ -0,0 +1,35 @@
+﻿using Mixter.Domain.Core.Subscriptions;
+using Mixter.Domain.Core.Subscriptions.Events;
+using Mixter.Domain.Identity;
+using Mixter.Domain.Tests;
+using NFluent;
+using Xunit;
+
+namespace Mixter.Infrastructure.Tests
+{
+    public class SubscriptionsRepositoryTest
+    {
+        private static readonly SubscriptionId SubscriptionId = new SubscriptionId(new UserId("follower@mix-it.fr"), new UserId("followee@mix-it.fr"));
+
+        private readonly EventsStore _eventsStore;
+        private readonly SubscriptionsRepository _repository;
+
+        public SubscriptionsRepositoryTest()
+        {
+            _eventsStore = new EventsStore();
+            _repository = new SubscriptionsRepository(_eventsStore);
+        }
+
+        [Fact]
+        public void GivenUserFollowedWhenGetSubscriptionThenReturnSubscriptionAggregate()
+        {
+            _eventsStore.Store(new UserFollowed(SubscriptionId));
+
+            var subscription = _repository.GetSubscription(SubscriptionId);
+
+            var eventPublisher = new EventPublisherFake();
+            subscription.Unfollow(eventPublisher);
+            Check.That(eventPublisher.Events).ContainsExactly(new UserUnfollowed(SubscriptionId));
+        }
+    }
+}
