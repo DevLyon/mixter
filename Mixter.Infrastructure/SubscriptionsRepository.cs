8b726a500baef8fabaa5d9f75e089d8a16b3256a
fpellet <florent.pellet@gmail.com>
2015-10-18 12:10:32 +0200

    [Infra][SubscriptionsRepository] Given UserFollowed When getSubscription Then return Subscription aggregate

---
+++ Mixter.Infrastructure/SubscriptionsRepository.cs
@@ -0,0 +1,19 @@
+﻿using Mixter.Domain.Core.Subscriptions;
+
+namespace Mixter.Infrastructure
+{
+    public class SubscriptionsRepository
+    {
+        private readonly EventsStore _eventsStore;
+
+        public SubscriptionsRepository(EventsStore eventsStore)
+        {
+            _eventsStore = eventsStore;
+        }
+
+        public Subscription GetSubscription(SubscriptionId id)
+        {
+            return new Subscription(_eventsStore.GetEventsOfAggregate(id));
+        }
+    }
+}
