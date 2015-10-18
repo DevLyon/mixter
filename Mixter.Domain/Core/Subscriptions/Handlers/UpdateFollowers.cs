d2cdf124ec04966c4c365986c86ac79384994475
fpellet <florent.pellet@gmail.com>
2015-10-18 12:35:18 +0200

    [Core][UpdateFollowers][Test OK] When UserFollowed Then save follower

--- Mixter.Domain/Core/Subscriptions/Handlers/UpdateFollowers.cs
+++ Mixter.Domain/Core/Subscriptions/Handlers/UpdateFollowers.cs
@@ -1,6 +1,20 @@
-﻿namespace Mixter.Domain.Core.Subscriptions.Handlers
+﻿using Mixter.Domain.Core.Subscriptions.Events;
+
+namespace Mixter.Domain.Core.Subscriptions.Handlers
 {
     public class UpdateFollowers
     {
+        private readonly IFollowersRepository _repository;
+
+        public UpdateFollowers(IFollowersRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public void Handle(UserFollowed evt)
+        {
+            var subscriptionId = evt.SubscriptionId;
+            _repository.Save(new FollowerProjection(subscriptionId.Followee, subscriptionId.Follower));
+        }
     }
 }
