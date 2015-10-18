82eac8b1df328440e12ca22a956901f28d378ca2
fpellet <florent.pellet@gmail.com>
2015-10-18 11:56:27 +0200

    [Infra][FollowersRepository] When save Then getFollowers Return followerId

---
+++ Mixter.Infrastructure.Tests/FollowersRepositoryTest.cs
@@ -0,0 +1,29 @@
+﻿using Mixter.Domain.Core.Subscriptions;
+using Mixter.Domain.Identity;
+using NFluent;
+using Xunit;
+
+namespace Mixter.Infrastructure.Tests
+{
+    public class FollowersRepositoryTest
+    {
+        private FollowersRepository _repository;
+        
+        public FollowersRepositoryTest()
+        {
+            _repository = new FollowersRepository();
+        }
+
+        [Fact]
+        public void WhenSaveThenGetFollowersReturnFollowerId()
+        {
+            var followee = new UserId("followee@mixit.fr");
+            var follower = new UserId("follower@mixit.fr");
+            _repository.Save(new FollowerProjection(followee, follower));
+
+            var followers = _repository.GetFollowers(followee);
+
+            Check.That(followers).ContainsExactly(follower);
+        }
+    }
+}
