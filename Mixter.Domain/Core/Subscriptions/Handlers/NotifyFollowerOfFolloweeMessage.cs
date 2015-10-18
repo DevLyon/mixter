bf70c41e641e20572c8dedf6c3b8f342cba597cc
fpellet <florent.pellet@gmail.com>
2015-10-18 12:54:09 +0200

    [Core][NotifyFollowerOfFolloweeMessage][Test KO] Given follower When MessageQuacked by followee Then raise FolloweeMessageQuacked

---
+++ Mixter.Domain/Core/Subscriptions/Handlers/NotifyFollowerOfFolloweeMessage.cs
@@ -0,0 +1,23 @@
+﻿using Mixter.Domain.Core.Messages.Events;
+
+namespace Mixter.Domain.Core.Subscriptions.Handlers
+{
+    public class NotifyFollowerOfFolloweeMessage
+    {
+        private IFollowersRepository _followersRepository;
+        private ISubscriptionsRepository _subscriptionsRepository;
+        private IEventPublisher _eventPublisher;
+
+        public NotifyFollowerOfFolloweeMessage(IFollowersRepository followersRepository, ISubscriptionsRepository subscriptionsRepository, IEventPublisher eventPublisher)
+        {
+            _followersRepository = followersRepository;
+            _subscriptionsRepository = subscriptionsRepository;
+            _eventPublisher = eventPublisher;
+        }
+
+        public void Handle(MessageQuacked evt)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
