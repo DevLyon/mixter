1f1dcb346c9f3b6bc18695ae3e9452eadbada372
fpellet <florent.pellet@gmail.com>
2015-10-18 11:56:27 +0200

    [Infra][FollowersRepository] When save Then getFollowers Return followerId

---
+++ Mixter.Infrastructure/FollowersRepository.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Mixter.Domain.Core.Subscriptions;
+using Mixter.Domain.Identity;
+
+namespace Mixter.Infrastructure
+{
+    public class FollowersRepository
+    {
+        private readonly IList<FollowerProjection> _projections = new List<FollowerProjection>();
+
+        public void Save(FollowerProjection projection)
+        {
+            _projections.Add(projection);
+        }
+
+        public IEnumerable<UserId> GetFollowers(UserId followee)
+        {
+            return _projections.Select(o => o.Follower);
+        }
+    }
+}
