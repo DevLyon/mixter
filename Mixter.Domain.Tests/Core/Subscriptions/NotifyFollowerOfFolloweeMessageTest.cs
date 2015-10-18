643f6dc3070143e535426deff9cf4f1eac34d648
fpellet <florent.pellet@gmail.com>
2015-10-18 12:54:09 +0200

    [Core][NotifyFollowerOfFolloweeMessage][Test KO] Given follower When MessageQuacked by followee Then raise FolloweeMessageQuacked

---
+++ Mixter.Domain.Tests/Core/Subscriptions/NotifyFollowerOfFolloweeMessageTest.cs
@@ -0,0 +1,48 @@
+﻿using Mixter.Domain.Core.Messages;
+using Mixter.Domain.Core.Messages.Events;
+using Mixter.Domain.Core.Subscriptions;
+using Mixter.Domain.Core.Subscriptions.Events;
+using Mixter.Domain.Core.Subscriptions.Handlers;
+using Mixter.Domain.Identity;
+using Mixter.Infrastructure;
+using NFluent;
+using Xunit;
+
+namespace Mixter.Domain.Tests.Core.Subscriptions
+{
+    public class NotifyFollowerOfFolloweeMessageTest
+    {
+        private static readonly SubscriptionId SubscriptionId = new SubscriptionId(new UserId("follower@mix-it.fr"), new UserId("followee@mix-it.fr"));
+
+        private readonly EventsStore _eventsStore;
+        private readonly NotifyFollowerOfFolloweeMessage _handler;
+        private readonly FollowersRepository _followersRepository;
+        private readonly EventPublisherFake _eventPublisher;
+
+        public NotifyFollowerOfFolloweeMessageTest()
+        {
+            _eventsStore = new EventsStore();
+            _followersRepository = new FollowersRepository();
+            var subscriptionsRepository = new SubscriptionsRepository(_eventsStore, _followersRepository);
+            _eventPublisher = new EventPublisherFake();
+            _handler = new NotifyFollowerOfFolloweeMessage(_followersRepository, subscriptionsRepository, _eventPublisher);
+        }
+
+        [Fact]
+        public void GivenFollowerWhenMessageQuackedByFolloweeThenRaiseFolloweeMessageQuacked()
+        {
+            AddFollower(SubscriptionId);
+            var messageQuacked = new MessageQuacked(MessageId.Generate(), SubscriptionId.Followee, "hello");
+
+            _handler.Handle(messageQuacked);
+
+            Check.That(_eventPublisher.Events).Contains(new FolloweeMessageQuacked(SubscriptionId, messageQuacked.Id));
+        }
+
+        private void AddFollower(SubscriptionId subscriptionId)
+        {
+            _followersRepository.Save(new FollowerProjection(SubscriptionId.Followee, subscriptionId.Follower));
+            _eventsStore.Store(new UserFollowed(subscriptionId));
+        }
+    }
+}
---
+++ Mixter.Domain/Core/Subscriptions/Handlers/NotifyFollowerOfFolloweeMessage.cs
@@ -0,0 +1,24 @@
+﻿using Mixter.Domain.Core.Messages.Events;
+
+namespace Mixter.Domain.Core.Subscriptions.Handlers
+{
+    [Handler]
+    public class NotifyFollowerOfFolloweeMessage
+    {
+        private IFollowersRepository _followersRepository;
+        private ISubscriptionsRepository _subscriptionsRepository;
+        private IEventPublisher _eventPublisher;
+
+        public NotifyFollowerOfFolloweeMessage(IFollowersRepository followersRepository, ISubscriptionsRepository subscriptionsRepository, IEventPublisher eventPublisher)
+        {
+            _followersRepository = followersRepository;
+            _subscriptionsRepository = subscriptionsRepository;
+            _eventPublisher = eventPublisher;
+        }
+
+        public void Handle(MessageQuacked evt)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
