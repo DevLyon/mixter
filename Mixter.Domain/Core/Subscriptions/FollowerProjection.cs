7dc66582c413e698ec5d4c478db3bf06b81e3aba
fpellet <florent.pellet@gmail.com>
2015-10-18 11:56:27 +0200

    [Infra][FollowersRepository] When save Then getFollowers Return followerId

---
+++ Mixter.Domain/Core/Subscriptions/FollowerProjection.cs
@@ -0,0 +1,19 @@
+﻿using Mixter.Domain.Identity;
+
+namespace Mixter.Domain.Core.Subscriptions
+{
+    [Projection]
+    public struct FollowerProjection
+    {
+        public UserId Followee { get; private set; }
+
+        public UserId Follower { get; private set; }
+
+        public FollowerProjection(UserId followee, UserId follower)
+            : this()
+        {
+            Followee = followee;
+            Follower = follower;
+        }
+    }
+}
