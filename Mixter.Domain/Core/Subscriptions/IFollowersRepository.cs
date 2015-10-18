558ffb49c9396d011c136c6c5765ab24a07416cc
fpellet <florent.pellet@gmail.com>
2015-10-18 12:03:33 +0200

    [Infra][FollowersRepository] Create interface

---
+++ Mixter.Domain/Core/Subscriptions/IFollowersRepository.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using Mixter.Domain.Identity;
+
+namespace Mixter.Domain.Core.Subscriptions
+{
+    public interface IFollowersRepository
+    {
+        void Save(FollowerProjection projection);
+        void Remove(FollowerProjection projection);
+        IEnumerable<UserId> GetFollowers(UserId followee);
+    }
+}
