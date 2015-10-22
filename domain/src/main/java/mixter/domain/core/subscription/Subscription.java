749af911b56b7cdbd9b077e5b68e6a5ca3b6e577
Jean Helou <jean.helou@gmail.com>
2015-10-22 16:19:06 +0200

    [Core][Subscription][Test OK] Given a subscription, when the follower unfollows the followee, then a UserUnfollowed event is published

--- domain/src/main/java/mixter/domain/core/subscription/Subscription.java
+++ domain/src/main/java/mixter/domain/core/subscription/Subscription.java
@@ -1,13 +1,42 @@
 package mixter.domain.core.subscription;
 
 import mixter.doc.Aggregate;
+import mixter.domain.DecisionProjectionBase;
+import mixter.domain.Event;
 import mixter.domain.EventPublisher;
 import mixter.domain.core.subscription.events.UserFollowed;
+import mixter.domain.core.subscription.events.UserUnfollowed;
 import mixter.domain.identity.UserId;
+
+import java.util.List;
 
 @Aggregate
 public class Subscription {
+    private DecisionProjection projection;
+
+    public Subscription(List<Event> history) {
+        projection = new DecisionProjection(history);
+    }
+
     public static void follow(UserId follower, UserId followee, EventPublisher eventPublisher) {
         eventPublisher.publish(new UserFollowed(new SubscriptionId(follower, followee)));
+    }
+
+    public void unfollow(EventPublisher eventPublisher) {
+        eventPublisher.publish(new UserUnfollowed(projection.id));
+    }
+
+    private class DecisionProjection extends DecisionProjectionBase {
+        public SubscriptionId id;
+
+        public DecisionProjection(List<Event> history) {
+            super();
+            super.register(UserFollowed.class, this::apply);
+            history.forEach(this::apply);
+        }
+
+        private void apply(UserFollowed event) {
+            id = event.getSubscriptionId();
+        }
     }
 }
