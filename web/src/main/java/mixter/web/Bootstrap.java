3aca3dfd2ea58a883c3dedec98dded4db84ddeaa
Jean Helou <jean.helou@gmail.com>
2015-10-23 22:39:04 +0200

    [WEB] wire timeline and handlers

--- web/src/main/java/mixter/web/Bootstrap.java
+++ web/src/main/java/mixter/web/Bootstrap.java
@@ -3,6 +3,16 @@ package mixter.web;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.datatype.jsr310.JSR310Module;
+import mixter.domain.core.message.TimelineMessageRepository;
+import mixter.domain.core.message.events.MessageQuacked;
+import mixter.domain.core.message.events.MessageRequacked;
+import mixter.domain.core.message.handlers.UpdateTimeline;
+import mixter.domain.core.subscription.FollowerRepository;
+import mixter.domain.core.subscription.SubscriptionRepository;
+import mixter.domain.core.subscription.events.UserFollowed;
+import mixter.domain.core.subscription.events.UserUnfollowed;
+import mixter.domain.core.subscription.handlers.NotifyFollowerOfFolloweeMessage;
+import mixter.domain.core.subscription.handlers.UpdateFollowers;
 import mixter.domain.identity.SessionProjectionRepository;
 import mixter.domain.identity.events.UserConnected;
 import mixter.domain.identity.events.UserDisconnected;
@@ -11,9 +21,7 @@ import mixter.infra.EventStore;
 import mixter.infra.InMemoryEventStore;
 import mixter.infra.PersistingEventPublisher;
 import mixter.infra.SynchronousEventPublisher;
-import mixter.infra.repositories.EventSessionRepository;
-import mixter.infra.repositories.EventUserIdentityRepository;
-import mixter.infra.repositories.InMemorySessionProjectionRepository;
+import mixter.infra.repositories.*;
 import net.codestory.http.WebServer;
 import net.codestory.http.extensions.Extensions;
 import net.codestory.http.misc.Env;
@@ -37,7 +45,25 @@ public class Bootstrap {
         SessionResource sessionResource = new SessionResource(userIdentityRepository, sessionRepository, eventPublisher);
 
         //Message
-        MessageResource messageResource = new MessageResource(sessionProjectionRepository, eventPublisher);
+        TimelineMessageRepository timelineMessageRepository = new InMemoryTimelineMessageRepository();
+        MessageResource messageResource = new MessageResource(sessionProjectionRepository, timelineMessageRepository, eventPublisher);
+        UpdateTimeline updateTimeline = new UpdateTimeline(timelineMessageRepository);
+
+        dispatcher.register(MessageQuacked.class, updateTimeline::apply);
+
+        //Subscription
+        FollowerRepository followerRepository= new InMemoryFollowerRepository();
+        SubscriptionRepository subscriptionRepository = new InMemorySubscriptionRepository(eventStore,followerRepository);
+
+
+        UpdateFollowers updateFollowers = new UpdateFollowers(followerRepository);
+        NotifyFollowerOfFolloweeMessage notifyFollowerOfFolloweeMessageHandler = new NotifyFollowerOfFolloweeMessage(followerRepository, subscriptionRepository,eventPublisher);
+
+        dispatcher.register(UserFollowed.class, updateFollowers::apply);
+        dispatcher.register(UserUnfollowed.class, updateFollowers::apply);
+        dispatcher.register(MessageQuacked.class,notifyFollowerOfFolloweeMessageHandler::apply);
+        dispatcher.register(MessageRequacked.class,notifyFollowerOfFolloweeMessageHandler::apply);
+
 
         new WebServer().configure(routes -> routes
                         .setExtensions(new JsonExtension())
