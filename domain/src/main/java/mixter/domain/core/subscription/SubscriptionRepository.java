cf428f807fc40557d86a9c2a31116dd4a3e25737
Jean Helou <jean.helou@gmail.com>
2015-10-23 22:07:46 +0200

    [Infra][Subscription] Init InMemorySubscriptionRepository

--- domain/src/main/java/mixter/domain/core/subscription/SubscriptionRepository.java
+++ domain/src/main/java/mixter/domain/core/subscription/SubscriptionRepository.java
@@ -1,9 +1,14 @@
 package mixter.domain.core.subscription;
 
 import mixter.doc.Repository;
+import mixter.domain.identity.UserId;
+
+import java.util.Iterator;
 
 @Repository
 public interface SubscriptionRepository {
     Subscription getById(SubscriptionId subscriptionId);
+
+    Iterator<Subscription> getSubscriptionsOfUser(UserId userId);
 }
 
