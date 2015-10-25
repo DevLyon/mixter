4c9346bf217671fa0f5be5a0859df9ee46cfd5b0
Jean Helou <jean.helou@gmail.com>
2015-10-25 09:52:41 +0100

    [Core][UpdateTimeline][Test OK] When handle MessageDeleted Then remove this message in TimelineMessageProjections

--- domain/src/main/java/mixter/domain/core/message/handlers/UpdateTimeline.java
+++ domain/src/main/java/mixter/domain/core/message/handlers/UpdateTimeline.java
@@ -3,6 +3,7 @@ package mixter.domain.core.message.handlers;
 import mixter.doc.Handler;
 import mixter.domain.core.message.TimelineMessageProjection;
 import mixter.domain.core.message.TimelineMessageRepository;
+import mixter.domain.core.message.events.MessageDeleted;
 import mixter.domain.core.message.events.MessageQuacked;
 
 @Handler
@@ -16,5 +17,9 @@ public class UpdateTimeline {
 
     public void apply(MessageQuacked event) {
         timelineRepository.save(new TimelineMessageProjection(event.getAuthorId(), event.getAuthorId(), event.getMessage(), event.getMessageId()));
+    }
+
+    public void apply(MessageDeleted messageDeleted) {
+        timelineRepository.delete(messageDeleted.getId());
     }
 }
