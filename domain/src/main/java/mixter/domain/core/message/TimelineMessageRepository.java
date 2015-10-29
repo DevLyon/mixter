76d045a334b45107a5f1c05f9e8bac2577fd0dfb
fpellet <florent.pellet@gmail.com>
2015-10-29 16:54:49 +0100

    [Infra][TimelineMessageRepository][Test OK] Given an message saved for several users, when it is deleted, it should be removed for all the users

--- domain/src/main/java/mixter/domain/core/message/TimelineMessageRepository.java
+++ domain/src/main/java/mixter/domain/core/message/TimelineMessageRepository.java
@@ -2,6 +2,8 @@ package mixter.domain.core.message;
 
 import mixter.doc.Repository;
 import mixter.domain.identity.UserId;
+import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+
 import java.util.Iterator;
 
 @Repository
@@ -9,4 +11,10 @@ public interface TimelineMessageRepository {
     TimelineMessageProjection save(TimelineMessageProjection message);
 
     Iterator<TimelineMessageProjection> getMessageOfUser(UserId ownerId);
+
+    //this is done only for the exercise to avoid git conflitcts when
+    // moving from step to step, this should not be done in real code
+    default void delete(MessageId messageId){
+        throw new NotImplementedException();
+    }
 }
