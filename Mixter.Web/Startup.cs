a0a0b0254a00d0ba20d55b8e59de250d07d44064
fpellet <florent.pellet@gmail.com>
2015-10-29 16:23:29 +0100

    [Api] Initialize server

---
+++ Mixter.Web/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Owin;
+
+namespace Mixter.Web
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.UseNancy(options => options.Bootstrapper = new Bootstrapper());
+        }
+    }
+}
