575dbe184c8aa356aa8f377d08412f3767251267
fpellet <florent.pellet@gmail.com>
2015-10-29 16:26:49 +0100

    [Api][Identity] Expose register user

---
+++ Mixter.Web/Bootstrapper.cs
@@ -0,0 +1,40 @@
+using Mixter.Domain;
+using Mixter.Domain.Identity;
+using Mixter.Infrastructure;
+using Nancy;
+using Nancy.Bootstrapper;
+using Nancy.Responses.Negotiation;
+using Nancy.TinyIoc;
+using System;
+
+namespace Mixter.Web
+{
+    public class Bootstrapper : DefaultNancyBootstrapper
+    {
+        protected override Func<ITypeCatalog, NancyInternalConfiguration> InternalConfiguration
+        {
+            get
+            {
+                return typeCatalog => NancyInternalConfiguration.WithOverrides(config => {
+                        config.StatusCodeHandlers = new[] { typeof(NotFoundStatusCodeHandler), typeof(ExceptionStatusCodeHandler) };
+                        config.ResponseProcessors = new[] { typeof(JsonProcessor) };
+                    })(typeCatalog);
+            }
+        }
+
+        protected override void ConfigureApplicationContainer(TinyIoCContainer container)
+        {
+            base.ConfigureApplicationContainer(container);
+
+            var eventsStore = new EventsStore();
+
+            var sessionsRepository = new SessionsRepository();
+
+            var eventPublisher = new EventPublisher();
+            var eventPublisherWithStorage = new EventPublisherWithStorage(eventsStore, eventPublisher);
+            eventPublisher.Subscribe(new SessionHandler(sessionsRepository));
+
+            container.Register<IEventPublisher>(eventPublisherWithStorage);
+        }
+    }
+}
