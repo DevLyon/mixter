0ecaddd41afad62ca205a3cb748120f4ee80efd2
Jean Helou <jean.helou@gmail.com>
2016-05-20 19:08:38 +0300

    [Identity][Passing] A UserId can not be created from an empty email string

--- src/main/scala/domain/identity/UserId.scala
+++ src/main/scala/domain/identity/UserId.scala
@@ -3,5 +3,7 @@ package domain.identity
 case class UserId(email: String)
 
 object UserId {
-  def of(email: String): Option[UserId] = Some(UserId(email))
+  def of(email: String): Option[UserId] =
+    if(email.isEmpty) None
+    else Some(UserId(email))
 }
