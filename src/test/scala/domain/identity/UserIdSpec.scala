16b4a53d59ed559724dbb372ebe8edec8f967e62
Jean Helou <jean.helou@gmail.com>
2016-05-20 18:55:10 +0300

    [Identity][Failing] A userId should be created from an email string

---
+++ src/test/scala/domain/identity/UserIdSpec.scala
@@ -0,0 +1,14 @@
+package domain.identity
+
+import org.scalatest.{Matchers, WordSpec}
+
+class UserIdSpec extends WordSpec with Matchers {
+  val AN_EMAIL = "mail@mix-it.fr"
+  "a new UserId" should {
+    "be created with an email" in {
+      val expectedUserId = UserId(AN_EMAIL)
+      val actualUserId = UserId.of(AN_EMAIL)
+      actualUserId shouldBe Some(expectedUserId)
+    }
+  }
+}
