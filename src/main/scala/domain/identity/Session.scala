9d1efed6fc31309fa7ac0af7f3c6bebc06414f78
Jean Helou <jean.helou@gmail.com>
2017-02-15 22:36:41 +0100

    [Identity][Failing] Given a session, when logging out, then a UserDisconnected event is raised

---
+++ src/main/scala/domain/identity/Session.scala
@@ -0,0 +1,5 @@
+package domain.identity
+
+class Session {
+
+}
--- src/test/scala/domain/identity/SessionSpec.scala
+++ src/test/scala/domain/identity/SessionSpec.scala
@@ -3,11 +3,12 @@ package domain.identity
 import java.time.LocalDateTime
 
 import domain.SpyEventPublisher
-import domain.identity.event.{UserConnected, UserRegistered}
+import domain.identity.event.{UserConnected, UserDisconnected, UserRegistered}
 import org.scalatest.{Matchers, WordSpec}
 
 class SessionSpec extends WordSpec with Matchers {
   val USER_ID = UserId("john@example.com")
+  val SESSION_ID = SessionId()
 
   "A session" should {
     "be connected when a UserIdentity logs in" in {
@@ -18,6 +19,19 @@ class SessionSpec extends WordSpec with Matchers {
 
       eventPublisher.publishedEvents should matchPattern {
         case Seq(UserConnected(SessionId(_), USER_ID, _:LocalDateTime)) =>
+      }
+    }
+  }
+  "A session" should{
+    "raise UserDisconnected when logging out" in {
+      // Given
+      val session: Session= Session(UserConnected(SESSION_ID, USER_ID, LocalDateTime.now()))
+      implicit val eventPublisher=new SpyEventPublisher()
+      // When
+      session.logout()
+      // Then
+      eventPublisher.publishedEvents should matchPattern {
+        case Seq(UserDisconnected(SESSION_ID, USER_ID)) =>
       }
     }
   }
