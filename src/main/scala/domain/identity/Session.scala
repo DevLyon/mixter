edaa110d0446a07eb834c514e2d98871c7edc216
Jean Helou <jean.helou@gmail.com>
2017-02-15 22:47:40 +0100

    [Identity][Passing] Given a session, when logging out, then a UserDisconnected event is raised

--- src/main/scala/domain/identity/Session.scala
+++ src/main/scala/domain/identity/Session.scala
@@ -1,5 +1,18 @@
 package domain.identity
 
-class Session {
+import domain.EventPublisher
+import domain.identity.event.{UserConnected, UserDisconnected}
 
+object Session {
+  private case class DecisionProjection(userConnected: UserConnected){
+    val userId: UserId = userConnected.userId
+    val sessionId: SessionId = userConnected.sessionId
+  }
+}
+
+case class Session(userConnected:UserConnected){
+  private val projection=Session.DecisionProjection(userConnected)
+
+  def logout()(implicit ep:EventPublisher):Unit =
+    ep.publish(UserDisconnected(projection.sessionId, projection.userId))
 }
