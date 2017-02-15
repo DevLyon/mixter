e081a838bf5b429b06aad465b138a6a0cb920a63
Jean Helou <jean.helou@gmail.com>
2017-02-15 22:27:58 +0100

    [Identity][Passing] Given a session Id, when serializing to string, then it is serialized as its value

--- src/main/scala/domain/identity/SessionId.scala
+++ src/main/scala/domain/identity/SessionId.scala
@@ -5,4 +5,6 @@ import java.util.UUID
 case class SessionId(value:String=UUID.randomUUID().toString){
   require(value != null, "A session id cannot be null")
   require(value.nonEmpty, "A session id cannot be empty")
+
+  override val toString: String = value
 }
