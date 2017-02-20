d9ac98741bd68863451c530f8f07fee8c2ef7c6d
Jean Helou <jean.helou@gmail.com>
2017-02-20 20:58:32 +0100

    [Identity][Passing] Given a disconnected session, when logging out, then no event is raised

--- src/main/scala/domain/identity/Session.scala
+++ src/main/scala/domain/identity/Session.scala
@@ -1,18 +1,29 @@
 package domain.identity
 
 import domain.EventPublisher
-import domain.identity.event.{UserConnected, UserDisconnected}
+import domain.identity.event._
 
 object Session {
-  private case class DecisionProjection(userConnected: UserConnected){
-    val userId: UserId = userConnected.userId
-    val sessionId: SessionId = userConnected.sessionId
+  private case class DecisionProjection(userId:UserId, sessionId: SessionId, active:Boolean){
+    def apply(event:UserSessionEvent):DecisionProjection= event match{
+      case UserDisconnected(_,_) => copy(active=false)
+      case UserConnected(_,_,_) => this
+    }
+  }
+  private object DecisionProjection{
+    def of(userConnected: UserConnected)=DecisionProjection(userConnected.userId, userConnected.sessionId, active = true)
   }
 }
 
-case class Session(userConnected:UserConnected){
-  private val projection=Session.DecisionProjection(userConnected)
+case class Session(userConnected:UserConnected, history: Seq[UserSessionEvent]=Seq.empty){
+  private val projection={
+    val seed = Session.DecisionProjection.of(userConnected)
+    history.foldLeft(seed)((acc, ev)=>acc(ev))
+  }
 
   def logout()(implicit ep:EventPublisher):Unit =
-    ep.publish(UserDisconnected(projection.sessionId, projection.userId))
+    if(projection.active){
+      ep.publish(UserDisconnected(projection.sessionId, projection.userId))
+    }
+
 }
