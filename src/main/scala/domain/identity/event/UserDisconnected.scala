51c7d373ff9bffb04ab136bef9eea54b3bce31b6
Jean Helou <jean.helou@gmail.com>
2017-02-20 20:58:32 +0100

    [Identity][Passing] Given a disconnected session, when logging out, then no event is raised

--- src/main/scala/domain/identity/event/UserDisconnected.scala
+++
@@ -1,6 +0,0 @@
-package domain.identity.event
-
-import domain.Event
-import domain.identity.{SessionId, UserId}
-
-case class UserDisconnected(sessionId:SessionId, userId:UserId) extends Event
--- src/main/scala/domain/identity/event/UserRegistered.scala
+++
@@ -1,6 +0,0 @@
-package domain.identity.event
-
-import domain.Event
-import domain.identity.UserId
-
-case class UserRegistered(userId: UserId) extends Event
