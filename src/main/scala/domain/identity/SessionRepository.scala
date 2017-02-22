2ce2233d96008f82fcefcd19b54e7d6d877546e7
Jean Helou <jean.helou@gmail.com>
2017-02-22 21:13:04 +0100

    [Identity][Passing] Given a RegisterSession handler, when it receives a UserDisconnected event, then it saves a disconnected SessionProjection

--- src/main/scala/domain/identity/SessionRepository.scala
+++ src/main/scala/domain/identity/SessionRepository.scala
@@ -2,4 +2,5 @@ package domain.identity
 
 trait SessionRepository {
   def save(sessionProjection: SessionProjection):Unit
+  def replaceBy(sessionProjection: SessionProjection):Unit
 }
--- src/main/scala/domain/identity/handlers/RegisterSession.scala
+++ src/main/scala/domain/identity/handlers/RegisterSession.scala
@@ -1,11 +1,15 @@
 package domain.identity.handlers
 
-import domain.identity.event.UserConnected
+import domain.identity.event.{UserConnected, UserDisconnected}
 import domain.identity.{SessionProjection, SessionRepository, SessionStatus}
 
 class RegisterSession(sessionRepository:SessionRepository){
   def apply(userConnected:UserConnected):Unit = {
     val projection = SessionProjection(userConnected.sessionId,  userConnected.userId, SessionStatus.CONNECTED)
     sessionRepository.save(projection)
+  }
+  def apply(userDisconnected: UserDisconnected):Unit = {
+    val projection = SessionProjection(userDisconnected.sessionId,  userDisconnected.userId, SessionStatus.DISCONNECTED)
+    sessionRepository.replaceBy(projection)
   }
 }
