cc2f71786aacf214ee88a736a706a2d21aebb5be
Jean Helou <jean.helou@gmail.com>
2017-02-22 21:15:05 +0100

    [Refactoring] Avoid repeating type name in variable name

--- src/main/scala/domain/identity/handlers/RegisterSession.scala
+++ src/main/scala/domain/identity/handlers/RegisterSession.scala
@@ -4,12 +4,12 @@ import domain.identity.event.{UserConnected, UserDisconnected}
 import domain.identity.{SessionProjection, SessionRepository, SessionStatus}
 
 class RegisterSession(sessionRepository:SessionRepository){
-  def apply(userConnected:UserConnected):Unit = {
-    val projection = SessionProjection(userConnected.sessionId,  userConnected.userId, SessionStatus.CONNECTED)
+  def apply(event: UserConnected):Unit = {
+    val projection = SessionProjection(event.sessionId,  event.userId, SessionStatus.CONNECTED)
     sessionRepository.save(projection)
   }
-  def apply(userDisconnected: UserDisconnected):Unit = {
-    val projection = SessionProjection(userDisconnected.sessionId,  userDisconnected.userId, SessionStatus.DISCONNECTED)
+  def apply(event: UserDisconnected):Unit = {
+    val projection = SessionProjection(event.sessionId,  event.userId, SessionStatus.DISCONNECTED)
     sessionRepository.replaceBy(projection)
   }
 }
