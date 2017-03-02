b72e359ea0b632af50ae3337a3cc1e2385717ba7
Jean Helou <jean.helou@gmail.com>
2017-03-02 21:39:58 +0100

    [Infra][Passing] Given an empty InMemory event store, when storing and reading events of a given aggregate, the stored event is returned

--- src/main/scala/infra/EventStore.scala
+++ src/main/scala/infra/EventStore.scala
@@ -3,5 +3,6 @@ package infra
 import domain.{AggregateId, Event}
 
 trait EventStore {
+  def store(event:Event):Unit
   def eventsOfAggregate(aggregateId: AggregateId):Seq[Event]
 }
--- src/main/scala/infra/InMemoryEventStore.scala
+++ src/main/scala/infra/InMemoryEventStore.scala
@@ -3,6 +3,11 @@ import domain.{AggregateId, Event}
 
 private[infra] class InMemoryEventStore extends EventStore {
   var events = Seq.empty[Event]
+
+  override def store(event: Event): Unit =
+    events = events :+ event
+
   override def eventsOfAggregate(aggregateId: AggregateId): Seq[Event] =
     events
+
 }
