598ae9a17f31e7d9223fdedb2f8f93105c5ece6e
Jean Helou <jean.helou@gmail.com>
2017-03-02 21:45:06 +0100

    [Infra][Failing] Given an event store having stored events for 3 aggregates, when reading events for 1 aggregate, then it retuns events for the 1 aggregate

--- src/test/scala/infra/InMemoryEventStoreSpec.scala
+++ src/test/scala/infra/InMemoryEventStoreSpec.scala
@@ -5,7 +5,10 @@ import org.scalatest.{Matchers, WordSpec}
 
 class InMemoryEventStoreSpec extends WordSpec with Matchers {
 
-  val AGGREGATE_ID1 = AnAggregateId()
+  val AGGREGATE_ID1 = AnAggregateId("1")
+  val AGGREGATE_ID2 = AnAggregateId("2")
+  val AGGREGATE_ID3 = AnAggregateId("3")
+
   "An empty event store" should {
     "return an empty list when getting events of an aggregate" in {
       // Given
@@ -30,6 +33,22 @@ class InMemoryEventStoreSpec extends WordSpec with Matchers {
 
       // Then
       eventsOfAggregate should contain theSameElementsAs Seq(expected)
+    }
+  }
+  "A store containing events of 3 aggregates" should {
+    "should return only the events of one aggregate when getting events for this aggregate" in {
+      //Given
+      val eventStore = new InMemoryEventStore()
+      val eventA = EventA(AGGREGATE_ID1)
+      eventStore.store(eventA)
+      eventStore.store(EventA(AGGREGATE_ID2))
+      eventStore.store(EventA(AGGREGATE_ID3))
+
+      //When
+      val eventsOfAggregate = eventStore.eventsOfAggregate(AGGREGATE_ID1)
+
+      //Then
+      eventsOfAggregate should contain theSameElementsAs Seq(eventA)
     }
   }
 }
