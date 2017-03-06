aca116b1f49668c333d14707ae0997cb7f7706ed
Jean Helou <jean.helou@gmail.com>
2017-03-06 18:26:43 +0100

    [Refactoring] Extract common elements of MessageSpec to constants or fixtures

--- domain/src/test/scala/mixter/domain/SpyEventPublisher.scala
+++ domain/src/test/scala/mixter/domain/SpyEventPublisher.scala
@@ -1,8 +1,16 @@
 package mixter.domain
 
+import org.scalatest.{Suite, SuiteMixin}
+
 class SpyEventPublisher extends EventPublisher {
   var publishedEvents:Seq[Event]=Seq.empty
   override def publish[T<:Event](event:T): Unit = {
     publishedEvents = publishedEvents :+ event
+  }
+}
+trait SpyEventPublisherFixture extends SuiteMixin { this: Suite =>
+  def withSpyEventPublisher(test: SpyEventPublisher=>Any): Any = {
+    val spyEventPublisher= new SpyEventPublisher()
+    test(spyEventPublisher)
   }
 }
