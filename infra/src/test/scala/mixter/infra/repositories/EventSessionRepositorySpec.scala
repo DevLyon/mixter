8a2a6142753f40ce4679aa829c88b1a25b52b0b3
Jean Helou <jean.helou@gmail.com>
2017-03-06 17:02:07 +0100

    [Infra][Failing] Given a session repository and a UserConnected event, when retrieving the session by id, then it should return the session

--- infra/src/test/scala/mixter/infra/repositories/EventSessionRepositorySpec.scala
+++ infra/src/test/scala/mixter/infra/repositories/EventSessionRepositorySpec.scala
@@ -1,11 +1,15 @@
 package mixter.infra.repositories
 
-import mixter.domain.identity.{SessionId, UserId}
+import java.time.LocalDateTime
+
+import mixter.domain.identity.event.UserConnected
+import mixter.domain.identity.{Session, SessionId, UserId}
 import mixter.infra.InMemoryEventStore
+import org.scalatest.OptionValues._
 import org.scalatest.{Matchers, WordSpec}
 
 class EventSessionRepositorySpec extends WordSpec with Matchers {
-  "A EventSessionRepository backed by an empty event store" should {
+  "An EventSessionRepository backed by an empty event store" should {
     "not find a Session aggregate by its Id" in {
       // Given
       val store = new InMemoryEventStore()
@@ -14,6 +18,20 @@ class EventSessionRepositorySpec extends WordSpec with Matchers {
       val maybeSession = repository.getById(SESSION_ID)
       //Then
       maybeSession  should be(None)
+    }
+  }
+
+  "An EventSessionRepository backed by an event store with a UserConnected event" should {
+    "find a Session aggregate by its id" in {
+      // Given
+      val userConnected = UserConnected(SESSION_ID, USER_ID,LocalDateTime.now())
+      val store = new InMemoryEventStore()
+      store.store(userConnected)
+      val repository = new EventSessionRepository(store)
+
+      val maybeSession = repository.getById(SESSION_ID)
+      //Then
+      maybeSession.value should ===(Session(userConnected, Seq.empty))
     }
   }
   val SESSION_ID = SessionId()
