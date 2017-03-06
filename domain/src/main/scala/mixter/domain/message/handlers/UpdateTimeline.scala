da67dd0870c6467e841184d47cdce40b0206dfcc
Jean Helou <jean.helou@gmail.com>
2017-03-06 22:48:30 +0100

    [Core][Timeline][Test OK] Given an UpdateTimeline handler, when it receives a MessageQuacked event, then it creates a TimelineMessageProjection

--- domain/src/main/scala/mixter/domain/message/handlers/UpdateTimeline.scala
+++ domain/src/main/scala/mixter/domain/message/handlers/UpdateTimeline.scala
@@ -1,4 +1,11 @@
 package mixter.domain.message.handlers
 
-class UpdateTimeline
+import mixter.domain.message.event.MessageQuacked
+import mixter.domain.message.{TimelineMessageProjection, TimelineMessageRepository}
+
+class UpdateTimeline(repository: TimelineMessageRepository) {
+  def apply(event: MessageQuacked): Unit = {
+    repository.save(TimelineMessageProjection(event.author, event.author, event.message, event.id))
+  }
+}
 
