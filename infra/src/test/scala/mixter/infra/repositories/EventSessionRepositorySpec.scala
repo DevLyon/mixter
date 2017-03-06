cd90cfb954d9d3fc73a04278eb77ebb17fcd297a
Jean Helou <jean.helou@gmail.com>
2017-03-06 16:57:49 +0100

    [Infra][Failing] Given a session repository backed by an empty event store, when retrieving a session by id, then it should return an empty option

---
+++ infra/src/test/scala/mixter/infra/repositories/EventSessionRepositorySpec.scala
@@ -0,0 +1,22 @@
+package mixter.infra.repositories
+
+import mixter.domain.identity.{SessionId, UserId}
+import mixter.infra.InMemoryEventStore
+import org.scalatest.matchers.should.Matchers
+import org.scalatest.wordspec.AnyWordSpec
+
+class EventSessionRepositorySpec extends AnyWordSpec with Matchers {
+  "A EventSessionRepository backed by an empty event store" should {
+    "not find a Session aggregate by its Id" in {
+      // Given
+      val store = new InMemoryEventStore()
+      val repository = new EventSessionRepository(store)
+
+      val maybeSession = repository.getById(SESSION_ID)
+      //Then
+      maybeSession  should be(None)
+    }
+  }
+  val SESSION_ID = SessionId()
+  val USER_ID = UserId("user@example.localhost")
+}
