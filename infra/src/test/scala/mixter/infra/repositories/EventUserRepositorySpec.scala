a22dbd9ed3a585bebbd3638b38ac374c710aea0e
Jean Helou <jean.helou@gmail.com>
2017-03-06 17:25:05 +0100

    [Infra][Failing] Given a user identity repository and a UserRegisteredEvent, when retrieving the userIdentity by id, then it should find the UserIdentity

--- infra/src/test/scala/mixter/infra/repositories/EventUserRepositorySpec.scala
+++ infra/src/test/scala/mixter/infra/repositories/EventUserRepositorySpec.scala
@@ -1,8 +1,10 @@
 package mixter.infra.repositories
 
-import mixter.domain.identity.UserId
+import mixter.domain.identity.event.UserRegistered
+import mixter.domain.identity.{UserId, UserIdentity}
 import mixter.infra.InMemoryEventStore
 import org.scalatest.{Matchers, WordSpec}
+import org.scalatest.OptionValues._
 
 class EventUserRepositorySpec extends WordSpec with Matchers {
   "An EventUserRepository backed by an empty event store" should {
@@ -14,6 +16,20 @@ class EventUserRepositorySpec extends WordSpec with Matchers {
       val maybeUser = repository.getById(USER_ID)
       //Then
       maybeUser should be(None)
+    }
+  }
+
+  "An EventUserRepository backed by an event store with a UserRegistered event" should {
+    "find a User aggregate by its id" in {
+      // Given
+      val userRegistered = UserRegistered(USER_ID)
+      val store = new InMemoryEventStore()
+      store.store(userRegistered)
+      val repository = new EventUserRepository(store)
+
+      val maybeUser = repository.getById(USER_ID)
+      //Then
+      maybeUser.value should ===(UserIdentity(userRegistered))
     }
   }
   val USER_ID = UserId("user@example.localhost")
