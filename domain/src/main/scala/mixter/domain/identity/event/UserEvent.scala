628fa717dc5a8cb489073965f7b1abf9f9b9e079
Jean Helou <jean.helou@gmail.com>
2017-03-06 17:27:11 +0100

    [Refactoring] Remove erroneous inheritance between UserEvent and UserSessionEvent

--- domain/src/main/scala/mixter/domain/identity/event/UserEvent.scala
+++ domain/src/main/scala/mixter/domain/identity/event/UserEvent.scala
@@ -7,7 +7,8 @@ import mixter.domain.{AggregateId, Event}
 
 sealed trait UserEvent extends Event
 case class UserRegistered(id: UserId) extends UserEvent
-sealed trait UserSessionEvent extends UserEvent
+
+sealed trait UserSessionEvent extends Event
 case class UserConnected(sessionId: SessionId, userId: UserId, since: LocalDateTime) extends UserSessionEvent{
   override def id: AggregateId = sessionId
 }
