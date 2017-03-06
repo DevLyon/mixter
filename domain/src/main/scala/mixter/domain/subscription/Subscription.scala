fc02bad8005eb1236d1668a76073dfc936ff9b22
Jean Helou <jean.helou@gmail.com>
2017-03-06 22:58:38 +0100

    [Core][Subscription][Test OK] Given a follower and a followee, when the follower follows the followee, then a UserFollowed event is published

--- domain/src/main/scala/mixter/domain/subscription/Subscription.scala
+++ domain/src/main/scala/mixter/domain/subscription/Subscription.scala
@@ -1,6 +1,13 @@
 package mixter.domain.subscription
 
+import mixter.domain.EventPublisher
 import mixter.domain.identity.UserId
 import mixter.domain.subscription.event.UserFollowed
 
 class Subscription
+
+object Subscription {
+  def follow(follower: UserId, followee: UserId)(implicit ep:EventPublisher): Unit = {
+    ep.publish(UserFollowed(SubscriptionId(follower, followee)))
+  }
+}
