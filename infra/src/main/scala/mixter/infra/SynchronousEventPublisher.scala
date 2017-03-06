5e105d97e69f6e9758e0837dd0afad04061d6391
Jean Helou <jean.helou@gmail.com>
2017-03-06 15:07:57 +0100

    [Infra][Passing] Given a synchronous event publisher with 2 registered handlers, when a message is published, then it calls both registered handlers

--- infra/src/main/scala/mixter/infra/SynchronousEventPublisher.scala
+++ infra/src/main/scala/mixter/infra/SynchronousEventPublisher.scala
@@ -5,13 +5,13 @@ import scala.reflect.ClassTag
 import mixter.domain.{Event, EventPublisher}
 
 class SynchronousEventPublisher extends EventPublisher {
-  private var handler: Event => Unit = _
+  private var handlers: Seq[Event => Unit]= Seq.empty
 
   def register[T <: Event](handler: T => Unit)(implicit ct: ClassTag[T]) {
-    this.handler = handler.asInstanceOf[Event=>Unit]
+    this.handlers = handlers :+ handler.asInstanceOf[Event=>Unit]
   }
 
   override def publish[T<:Event](event: T): Unit = {
-    handler.asInstanceOf[T=>Unit](event)
+    handlers.foreach(_.asInstanceOf[T=>Unit](event))
   }
 }
