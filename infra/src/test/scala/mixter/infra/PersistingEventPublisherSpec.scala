6b636c875b47bcb196108391845603c30f6ff17d
Jean Helou <jean.helou@gmail.com>
2017-03-06 15:37:28 +0100

    [Infra][Failing] Given a persisting event publisher, when publishing an event, it is forwarded

--- infra/src/test/scala/mixter/infra/PersistingEventPublisherSpec.scala
+++ infra/src/test/scala/mixter/infra/PersistingEventPublisherSpec.scala
@@ -17,5 +17,18 @@ class PersistingEventPublisherSpec extends WordSpec with Matchers {
       // Then
       store.eventsOfAggregate(event.id) should contain theSameElementsAs Seq(event)
     }
+    "forward the event when it is published " in {
+      //Given
+      val store = new InMemoryEventStore()
+      val publisher = new SpyEventPublisher()
+      val event = EventA(AnAggregateId("id"))
+      val persistedPublisher = new PersistingEventPublisher(store, publisher)
+
+      // When
+      persistedPublisher.publish(event)
+
+      // Then
+      publisher.publishedEvents should contain theSameElementsAs Seq(event)
+    }
   }
 }
