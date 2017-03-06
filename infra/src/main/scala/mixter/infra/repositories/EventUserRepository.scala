0419e486156c4dac0b0d0c6821e17a23cc5bff4d
Jean Helou <jean.helou@gmail.com>
2017-03-06 17:25:22 +0100

    [Infra][Passing] Given a user identity repository and a UserRegisteredEvent, when retrieving the userIdentity by id, then it should find the UserIdentity

--- infra/src/main/scala/mixter/infra/repositories/EventUserRepository.scala
+++ infra/src/main/scala/mixter/infra/repositories/EventUserRepository.scala
@@ -1,10 +1,14 @@
 package mixter.infra.repositories
 
+import mixter.domain.identity.event.UserRegistered
 import mixter.domain.identity.{UserId, UserIdentity}
 import mixter.infra.EventStore
 
 class EventUserRepository(store: EventStore) {
   def getById(id: UserId):Option[UserIdentity] ={
-    None
+    val history = store.eventsOfAggregate(id)
+    history.headOption.map( userConnected=>
+      UserIdentity(userConnected.asInstanceOf[UserRegistered])
+    )
   }
 }
