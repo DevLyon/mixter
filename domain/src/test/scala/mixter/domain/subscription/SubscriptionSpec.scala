c8599996a53215974f5aa137cc2e8aca0c3e77ec
Jean Helou <jean.helou@gmail.com>
2017-03-06 23:34:53 +0100

    [Core][Subscription][Test KO] Given a subscription, when the follower unfollows the followee, then a UserUnfollowed event is published

--- domain/src/test/scala/mixter/domain/subscription/SubscriptionSpec.scala
+++ domain/src/test/scala/mixter/domain/subscription/SubscriptionSpec.scala
@@ -2,7 +2,7 @@ package mixter.domain.subscription
 
 import mixter.domain.SpyEventPublisherFixture
 import mixter.domain.identity.UserId
-import mixter.domain.subscription.event.UserFollowed
+import mixter.domain.subscription.event.{UserFollowed, UserUnfollowed}
 import org.scalatest.{Matchers, WordSpec}
 
 class SubscriptionSpec extends WordSpec with Matchers with SpyEventPublisherFixture {
@@ -13,10 +13,20 @@ class SubscriptionSpec extends WordSpec with Matchers with SpyEventPublisherFixt
       //When
       Subscription.follow(FOLLOWER, FOLLOWEE)
       //Then
-      val expected = UserFollowed(SubscriptionId(FOLLOWER, FOLLOWEE))
+      val expected = UserFollowed(SUBSCRIPTION_ID)
+      eventPublisher.publishedEvents should contain only expected
+    }
+    "raise UserUnfollowed when a follower unfollows a followee" in withSpyEventPublisher { implicit eventPublisher =>
+      //Given
+      val subscription = Subscription(UserFollowed(SUBSCRIPTION_ID))
+      //When
+      subscription.unfollow()
+      //Then
+      val expected = UserUnfollowed(SUBSCRIPTION_ID)
       eventPublisher.publishedEvents should contain only expected
     }
   }
   private val FOLLOWER = UserId("follower@example.localhost")
   private val FOLLOWEE = UserId("followee@example.localhost")
+  private val SUBSCRIPTION_ID = SubscriptionId(FOLLOWER, FOLLOWEE)
 }
