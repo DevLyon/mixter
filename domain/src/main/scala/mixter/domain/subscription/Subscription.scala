66f9674e47121e271a22cf2c1e02b8a39419ef49
Jean Helou <jean.helou@gmail.com>
2017-03-06 23:37:08 +0100

    [Core][Subscription][Test OK] Given a subscription, when the follower unfollows the followee, then a UserUnfollowed event is published

--- domain/src/main/scala/mixter/domain/subscription/Subscription.scala
+++ domain/src/main/scala/mixter/domain/subscription/Subscription.scala
@@ -2,12 +2,26 @@ package mixter.domain.subscription
 
 import mixter.domain.EventPublisher
 import mixter.domain.identity.UserId
-import mixter.domain.subscription.event.UserFollowed
+import mixter.domain.subscription.event.{UserFollowed, UserUnfollowed}
 
-class Subscription
+
+case class Subscription(initialEvent:UserFollowed) {
+  import Subscription._
+  private val projection = DecisionProjection.of(initialEvent)
+
+  def unfollow()(implicit ep:EventPublisher):Unit = {
+    ep.publish(UserUnfollowed(projection.subscriptionId))
+  }
+}
 
 object Subscription {
   def follow(follower: UserId, followee: UserId)(implicit ep:EventPublisher): Unit = {
     ep.publish(UserFollowed(SubscriptionId(follower, followee)))
   }
+
+  case class DecisionProjection(subscriptionId: SubscriptionId)
+  object DecisionProjection{
+    def of(userFollowed: UserFollowed)= DecisionProjection(userFollowed.subscriptionId)
+  }
 }
+
