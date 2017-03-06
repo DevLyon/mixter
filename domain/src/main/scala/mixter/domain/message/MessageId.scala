5aae804b0f717ffe37aa7b14805a8868b3cb37c9
Jean Helou <jean.helou@gmail.com>
2017-03-06 16:16:25 +0100

    [Message][Passing] Given a message id , when it is serialized to string, then it should return its value

--- domain/src/main/scala/mixter/domain/message/MessageId.scala
+++ domain/src/main/scala/mixter/domain/message/MessageId.scala
@@ -4,7 +4,9 @@ import java.util.UUID
 
 import mixter.domain.AggregateId
 
-case class MessageId(id:String) extends AggregateId
+case class MessageId(id:String) extends AggregateId {
+  override val toString: String = id
+}
 
 object MessageId{
   def generate():MessageId = MessageId(UUID.randomUUID.toString)
