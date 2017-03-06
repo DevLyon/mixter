01e7fa9e869a6f4f3ac053a1ee94d13bbf99ea09
Jean Helou <jean.helou@gmail.com>
2017-03-06 16:04:55 +0100

    [Infra][Passing] Given an in memory SessionProjectionRepository with a saved session, when replacing the session, it should return the  updated session

--- infra/src/main/scala/mixter/infra/InMemorySessionProjectionRepository.scala
+++ infra/src/main/scala/mixter/infra/InMemorySessionProjectionRepository.scala
@@ -8,7 +8,8 @@ class InMemorySessionProjectionRepository extends SessionRepository{
   override def save(sessionProjection: SessionProjection): Unit =
     sessions+=sessionProjection.sessionId -> sessionProjection
 
-  override def replaceBy(sessionProjection: SessionProjection): Unit = Unit
+  override def replaceBy(sessionProjection: SessionProjection): Unit =
+    save(sessionProjection)
 
   override def getById(id: SessionId): Option[SessionProjection] =
     sessions.get(id)
