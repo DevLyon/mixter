3c92e8e796bea0a0e6941af69530d74667bcbc57
Jean Helou <jean.helou@gmail.com>
2017-03-07 22:37:25 +0100

    [Infra][TimelineMessageRepository] Init InitMemoryTimelineRepository

--- domain/src/main/scala/mixter/domain/message/TimelineMessageRepository.scala
+++ domain/src/main/scala/mixter/domain/message/TimelineMessageRepository.scala
@@ -1,5 +1,8 @@
 package mixter.domain.message
 
+import mixter.domain.identity.UserId
+
 trait TimelineMessageRepository {
   def save(message:TimelineMessageProjection):TimelineMessageProjection
+  def getMessageOfUser(ownerId:UserId):Iterable[TimelineMessageProjection]
 }
