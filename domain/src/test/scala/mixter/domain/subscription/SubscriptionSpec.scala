d16b6baf1807b8190fff7085fee67c5880d178c5
Jean Helou <jean.helou@gmail.com>
2017-03-07 17:31:36 +0100

    [Core][Subscription][Test KO] Given a subscription, when notifying the follower, then a FolloweeMessageQuacked event is published

--- domain/src/test/scala/mixter/domain/subscription/SubscriptionSpec.scala
+++ domain/src/test/scala/mixter/domain/subscription/SubscriptionSpec.scala
@@ -2,7 +2,8 @@ package mixter.domain.subscription
 
 import mixter.domain.SpyEventPublisherFixture
 import mixter.domain.identity.UserId
-import mixter.domain.subscription.event.{UserFollowed, UserUnfollowed}
+import mixter.domain.message.MessageId
+import mixter.domain.subscription.event.{FolloweeMessageQuacked, UserFollowed, UserUnfollowed}
 import org.scalatest.{Matchers, WordSpec}
 
 class SubscriptionSpec extends WordSpec with Matchers with SpyEventPublisherFixture {
@@ -16,6 +17,7 @@ class SubscriptionSpec extends WordSpec with Matchers with SpyEventPublisherFixt
       val expected = UserFollowed(SUBSCRIPTION_ID)
       eventPublisher.publishedEvents should contain only expected
     }
+
     "raise UserUnfollowed when a follower unfollows a followee" in withSpyEventPublisher { implicit eventPublisher =>
       //Given
       val subscription = Subscription(UserFollowed(SUBSCRIPTION_ID))
@@ -23,6 +25,17 @@ class SubscriptionSpec extends WordSpec with Matchers with SpyEventPublisherFixt
       subscription.unfollow()
       //Then
       val expected = UserUnfollowed(SUBSCRIPTION_ID)
+      eventPublisher.publishedEvents should contain only expected
+    }
+
+    "raise FolloweeMessageQuacked when notifying a follower" in withSpyEventPublisher { implicit eventPublisher =>
+      //Given
+      val subscription = Subscription(UserFollowed(SUBSCRIPTION_ID))
+      val messageId = MessageId.generate()
+      //When
+      subscription.notifyFollower(messageId)
+      //Then
+      val expected = FolloweeMessageQuacked(SUBSCRIPTION_ID, messageId)
       eventPublisher.publishedEvents should contain only expected
     }
   }
