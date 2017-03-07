2cfba8a26cea104579add535cd7066ace700268d
Jean Helou <jean.helou@gmail.com>
2017-03-07 22:17:40 +0100

    [Core][NotifyFollowerOfFolloweeMessage][Test OK] Given a NotifyFollowerOfFolloweeMessage handler, when it receives a MessageRequacked event, it should publish a FolloweeMessageQuacked to each folllower

--- domain/src/main/scala/mixter/domain/subscription/handlers/NotifyFollowerOfFolloweeMessage.scala
+++ domain/src/main/scala/mixter/domain/subscription/handlers/NotifyFollowerOfFolloweeMessage.scala
@@ -1,7 +1,7 @@
 package mixter.domain.subscription.handlers
 
 import mixter.domain.EventPublisher
-import mixter.domain.message.event.MessageQuacked
+import mixter.domain.message.event.{MessageQuacked, MessageRequacked}
 import mixter.domain.subscription.{FollowerRepository, SubscriptionId, SubscriptionRepository}
 
 class NotifyFollowerOfFolloweeMessage(followerRepository: FollowerRepository,
@@ -13,6 +13,12 @@ class NotifyFollowerOfFolloweeMessage(followerRepository: FollowerRepository,
       follower <- followerRepository.getFollowers(messageQuacked.author)
       subscription <- subscriptionRepository.getById(SubscriptionId(follower, messageQuacked.author))
     } subscription.notifyFollower(messageQuacked.id)
+  }
+  def apply(messageRequacked: MessageRequacked): Unit = {
+    for {
+      follower <- followerRepository.getFollowers(messageRequacked.requacker)
+      subscription <- subscriptionRepository.getById(SubscriptionId(follower, messageRequacked.requacker))
+    } subscription.notifyFollower(messageRequacked.id)
   }
 
 }
