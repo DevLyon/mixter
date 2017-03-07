f4ac0a47a253ca4644b012d21b32b64dde85062c
Jean Helou <jean.helou@gmail.com>
2017-03-07 20:47:32 +0100

    [Core][UpdateFollowers][Test OK] Given an UpdateFollowers handler, when it receives a UserUnfollowed event, it should remove the follower of the followers repository

--- domain/src/main/scala/mixter/domain/subscription/handlers/UpdateFollowers.scala
+++ domain/src/main/scala/mixter/domain/subscription/handlers/UpdateFollowers.scala
@@ -1,10 +1,14 @@
 package mixter.domain.subscription.handlers
 
 import mixter.domain.subscription.FollowerRepository
-import mixter.domain.subscription.event.UserFollowed
+import mixter.domain.subscription.event.{UserFollowed, UserUnfollowed}
 
 class UpdateFollowers(repository: FollowerRepository) {
   def apply(event:UserFollowed):Unit ={
     repository.saveFollower(event.subscriptionId.followee, event.subscriptionId.follower)
+  }
+
+  def apply(event:UserUnfollowed):Unit ={
+    repository.removeFollower(event.subscriptionId.followee, event.subscriptionId.follower)
   }
 }
