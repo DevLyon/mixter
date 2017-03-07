c7a4278bd4598578014a1a02c51d76b1b339925c
Jean Helou <jean.helou@gmail.com>
2017-03-07 22:54:44 +0100

    [Core][UpdateTimeline][Test OK] When handle MessageDeleted Then remove this message in TimelineMessageProjections

--- domain/src/main/scala/mixter/domain/message/handlers/UpdateTimeline.scala
+++ domain/src/main/scala/mixter/domain/message/handlers/UpdateTimeline.scala
@@ -1,11 +1,14 @@
 package mixter.domain.message.handlers
 
-import mixter.domain.message.event.MessageQuacked
+import mixter.domain.message.event.{MessageDeleted, MessageQuacked}
 import mixter.domain.message.{TimelineMessageProjection, TimelineMessageRepository}
 
 class UpdateTimeline(repository: TimelineMessageRepository) {
   def apply(event: MessageQuacked): Unit = {
     repository.save(TimelineMessageProjection(event.author, event.author, event.message, event.id))
+  }
+  def apply(event: MessageDeleted): Unit = {
+    repository.delete(event.id)
   }
 }
 
