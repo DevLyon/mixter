e123cb64a944e9b90d5e58ffec740b775e3d01cd
Jean Helou <jean.helou@gmail.com>
2017-03-07 17:37:33 +0100

    [Core][Subscription][Test OK] Given an unfollowed subscription, when notifying the follower, then no event should be published

--- domain/src/main/scala/mixter/domain/subscription/Subscription.scala
+++ domain/src/main/scala/mixter/domain/subscription/Subscription.scala
@@ -3,31 +3,41 @@ package mixter.domain.subscription
 import mixter.domain.EventPublisher
 import mixter.domain.identity.UserId
 import mixter.domain.message.MessageId
-import mixter.domain.subscription.event.{FolloweeMessageQuacked, UserFollowed, UserUnfollowed}
+import mixter.domain.subscription.event.{FolloweeMessageQuacked, SubscriptionEvent, UserFollowed, UserUnfollowed}
 
-
-case class Subscription(initialEvent:UserFollowed) {
-
+case class Subscription(initialEvent: UserFollowed, history: Seq[SubscriptionEvent]=Seq.empty) {
   import Subscription._
-  private val projection = DecisionProjection.of(initialEvent)
 
-  def unfollow()(implicit ep:EventPublisher):Unit = {
+  private val projection = {
+    val seed = DecisionProjection.of(initialEvent)
+    history.foldLeft(seed)((acc, evt) => acc(evt))
+  }
+
+  def unfollow()(implicit ep: EventPublisher): Unit = {
     ep.publish(UserUnfollowed(projection.subscriptionId))
   }
 
-  def notifyFollower(messageId: MessageId)(implicit ep:EventPublisher):Unit = {
-    ep.publish(FolloweeMessageQuacked(projection.subscriptionId, messageId))
+  def notifyFollower(messageId: MessageId)(implicit ep: EventPublisher): Unit = {
+    if(projection.isFollowing){
+      ep.publish(FolloweeMessageQuacked(projection.subscriptionId, messageId))
+    }
   }
 }
 
 object Subscription {
-  def follow(follower: UserId, followee: UserId)(implicit ep:EventPublisher): Unit = {
+  def follow(follower: UserId, followee: UserId)(implicit ep: EventPublisher): Unit = {
     ep.publish(UserFollowed(SubscriptionId(follower, followee)))
   }
 
-  case class DecisionProjection(subscriptionId: SubscriptionId)
-  object DecisionProjection{
-    def of(userFollowed: UserFollowed)= DecisionProjection(userFollowed.subscriptionId)
+  case class DecisionProjection(subscriptionId: SubscriptionId, isFollowing:Boolean=true){
+    def apply(evt:SubscriptionEvent)=evt match {
+      case UserUnfollowed(_) => copy(isFollowing=false)
+      case _ => this
+    }
   }
-}
 
+  object DecisionProjection {
+    def of(userFollowed: UserFollowed) = DecisionProjection(userFollowed.subscriptionId)
+  }
+
+}
