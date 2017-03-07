b161353cc6be6727519a13c23447d23150eb4980
Jean Helou <jean.helou@gmail.com>
2017-03-07 20:39:20 +0100

    [Core][UpdateFollowers][Test OK] Given an UpdateFollowers handler, when it receives a UserFollowed event, it should add the follower to the followers repository

--- domain/src/main/scala/mixter/domain/subscription/handlers/UpdateFollowers.scala
+++ domain/src/main/scala/mixter/domain/subscription/handlers/UpdateFollowers.scala
@@ -1,5 +1,10 @@
 package mixter.domain.subscription.handlers
 
-class UpdateFollowers {
+import mixter.domain.subscription.FollowerRepository
+import mixter.domain.subscription.event.UserFollowed
 
+class UpdateFollowers(repository: FollowerRepository) {
+  def apply(event:UserFollowed):Unit ={
+    repository.saveFollower(event.subscriptionId.followee, event.subscriptionId.follower)
+  }
 }
