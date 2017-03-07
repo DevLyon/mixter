5187d3cc7ac618b3ea460eaf3033940fdda1e99b
Jean Helou <jean.helou@gmail.com>
2017-03-07 21:45:14 +0100

    [Core][NotifyFollowerOfFolloweeMessage][Test OK] Given a NotifyFollowerOfFolloweeMessage handler, when it receives a MessageQuacked event, it should publish a FolloweeMessageQuacked to each folllower

--- domain/src/main/scala/mixter/domain/subscription/handlers/NotifyFollowerOfFolloweeMessage.scala
+++ domain/src/main/scala/mixter/domain/subscription/handlers/NotifyFollowerOfFolloweeMessage.scala
@@ -1,5 +1,18 @@
 package mixter.domain.subscription.handlers
 
-class NotifyFollowerOfFolloweeMessage {
+import mixter.domain.EventPublisher
+import mixter.domain.message.event.MessageQuacked
+import mixter.domain.subscription.{FollowerRepository, SubscriptionId, SubscriptionRepository}
+
+class NotifyFollowerOfFolloweeMessage(followerRepository: FollowerRepository,
+                                      subscriptionRepository: SubscriptionRepository
+                                     )(implicit val ep: EventPublisher) {
+
+  def apply(messageQuacked: MessageQuacked): Unit = {
+    for {
+      follower <- followerRepository.getFollowers(messageQuacked.author)
+      subscription <- subscriptionRepository.getById(SubscriptionId(follower, messageQuacked.author))
+    } subscription.notifyFollower(messageQuacked.id)
+  }
 
 }
