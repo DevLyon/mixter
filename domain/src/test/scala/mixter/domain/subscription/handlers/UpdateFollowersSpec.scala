66cfcc8c77fa5b43047c6d6ecea5f7b80d32b21d
Jean Helou <jean.helou@gmail.com>
2017-03-07 20:44:12 +0100

    [Core][UpdateFollowers][Test KO] Given an UpdateFollowers handler, when it receives a UserUnfollowed event, it should remove the follower of the followers repository

--- domain/src/test/scala/mixter/domain/subscription/handlers/UpdateFollowersSpec.scala
+++ domain/src/test/scala/mixter/domain/subscription/handlers/UpdateFollowersSpec.scala
@@ -1,8 +1,8 @@
 package mixter.domain.subscription.handlers
 
 import mixter.domain.identity.UserId
-import mixter.domain.subscription.event.UserFollowed
 import mixter.domain.subscription.{FollowerRepositoryFixture, SubscriptionId}
+import mixter.domain.subscription.event.{UserFollowed, UserUnfollowed}
 import org.scalatest.wordspec.AnyWordSpec
 import org.scalatest.matchers.should.Matchers
 
@@ -10,14 +10,29 @@ class UpdateFollowersSpec extends AnyWordSpec with Matchers with FollowerReposit
   "A UpdateFollowers handler" should {
     "add a follower to a user's followers list when receiving a UserFollowed event " in withFollowerRepository { repository =>
       // Given
-      val userFollowed = UserFollowed(SubscriptionId(Follower, Followee))
       val handler = new UpdateFollowers(repository)
       // When
-      handler(userFollowed)
+      handler(UserFollowedEvent)
       // Then
       repository.getFollowers(Followee) should contain only(Follower)
     }
+
+    "remove a follower from a user's followers list when receiving a UserUnfollowed event" in withFollowerRepository { repository =>
+      // Given
+      val userUnfollowed = UserUnfollowed(ASubscriptionId)
+      val handler = new UpdateFollowers(repository)
+      handler(UserFollowedEvent)
+
+      // When
+      handler(userUnfollowed)
+
+      // Then
+      repository.getFollowers(Followee) should not contain Follower
+    }
+
   }
   private val Follower = UserId("follower@example.localhost")
   private val Followee = UserId("followee@example.localhost")
+  private val ASubscriptionId = SubscriptionId(Follower, Followee)
+  private val UserFollowedEvent = UserFollowed(ASubscriptionId)
 }
