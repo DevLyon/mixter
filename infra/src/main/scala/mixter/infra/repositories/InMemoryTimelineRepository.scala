f0834eefe76331d9e2eaf7c78fd724688453813e
Jean Helou <jean.helou@gmail.com>
2017-03-07 22:45:33 +0100

    [Infra][TimelineMessageRepository][Test OK] Given an message saved for several users, when it is deleted, it should be removed for all the users

--- infra/src/main/scala/mixter/infra/repositories/InMemoryTimelineRepository.scala
+++ infra/src/main/scala/mixter/infra/repositories/InMemoryTimelineRepository.scala
@@ -1,9 +1,10 @@
 package mixter.infra.repositories
 
 import mixter.domain.identity.UserId
-import mixter.domain.message.{TimelineMessageProjection, TimelineMessageRepository}
+import mixter.domain.message.{MessageId, TimelineMessageProjection, TimelineMessageRepository}
 
 class InMemoryTimelineRepository extends TimelineMessageRepository {
+
   var messages: Set[TimelineMessageProjection] = Set.empty
 
   override def save(message:TimelineMessageProjection):TimelineMessageProjection = {
@@ -13,5 +14,9 @@ class InMemoryTimelineRepository extends TimelineMessageRepository {
 
   override def getMessageOfUser(ownerId: UserId): Iterable[TimelineMessageProjection] =
     messages.filter(_.ownerId==ownerId)
+
+  def delete(messageId: MessageId):Unit = {
+    messages = messages.filterNot(_.messageId==messageId)
+  }
 }
 
