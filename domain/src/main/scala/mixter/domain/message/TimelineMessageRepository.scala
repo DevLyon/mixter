33695567d940852a65479766d23e95299506f0c4
Jean Helou <jean.helou@gmail.com>
2017-03-07 22:54:10 +0100

    [Core][UpdateTimeline][Test KO] When handle MessageDeleted Then remove this message in TimelineMessageProjections

--- domain/src/main/scala/mixter/domain/message/TimelineMessageRepository.scala
+++ domain/src/main/scala/mixter/domain/message/TimelineMessageRepository.scala
@@ -5,4 +5,5 @@ import mixter.domain.identity.UserId
 trait TimelineMessageRepository {
   def save(message:TimelineMessageProjection):TimelineMessageProjection
   def getMessageOfUser(ownerId:UserId):Iterable[TimelineMessageProjection]
+  def delete(messageId: MessageId):Unit
 }
--- domain/src/test/scala/mixter/domain/message/handlers/TimelineMessageRepositoryFake.scala
+++ domain/src/test/scala/mixter/domain/message/handlers/TimelineMessageRepositoryFake.scala
@@ -1,7 +1,7 @@
 package mixter.domain.message.handlers
 
 import mixter.domain.identity.UserId
-import mixter.domain.message.{TimelineMessageProjection, TimelineMessageRepository}
+import mixter.domain.message.{MessageId, TimelineMessageProjection, TimelineMessageRepository}
 import org.scalatest.{Suite, SuiteMixin}
 
 trait TimelineMessageRepositoryFixture extends SuiteMixin {
@@ -14,13 +14,18 @@ trait TimelineMessageRepositoryFixture extends SuiteMixin {
 }
 
 class TimelineMessageRepositoryFake extends TimelineMessageRepository {
-  var messages: Seq[TimelineMessageProjection] = Seq.empty
+  private[handlers] var messages: Seq[TimelineMessageProjection] = Seq.empty
+  private[handlers] var deletedMessageIds:Set[MessageId] = Set.empty
 
   def getMessages: Seq[TimelineMessageProjection] = messages
 
-  override def save(message:TimelineMessageProjection):TimelineMessageProjection = {
+  override def save(message: TimelineMessageProjection): TimelineMessageProjection = {
     messages = messages :+ message
     message
+  }
+
+  override def delete(messageId: MessageId): Unit = {
+    deletedMessageIds += messageId
   }
 
   override def getMessageOfUser(ownerId: UserId): Iterable[TimelineMessageProjection] = ???
