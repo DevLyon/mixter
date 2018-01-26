b5c17b7192847a9995b85c96025e39da1943486d
Jean Helou <jean.helou@gmail.com>
2018-01-26 14:45:28 +0100

    [Core][Message][Failing] When a message is requacked then it sends a MessageRequackedEvent

--- domain/src/test/scala/mixter/domain/MessageSpec.scala
+++ domain/src/test/scala/mixter/domain/MessageSpec.scala
@@ -3,6 +3,11 @@ package mixter.domain
 import org.scalatest.{Matchers, WordSpec}
 
 class MessageSpec extends WordSpec with Matchers {
+
+  val John = UserId("john@example.com")
+  val Jane = UserId("jane@example.com")
+
+  val MessageContent = "a message"
   "A Message" should {
     "raise MessageQuacked when quacked" in {
       val message = "a message"
@@ -11,6 +16,17 @@ class MessageSpec extends WordSpec with Matchers {
       val quacked = Message.quack(message, author)
 
       quacked should equal(MessageQuacked(message, author))
+    }
+    "raise MessageRequacked when requacked" in {
+      val requacker=UserId("jane@example.com")
+      val message = "a message"
+      val author = UserId("john@example.com")
+      val history =  MessageQuacked(message, author)
+
+      val requacked =  Message.from(history).requack(requacker)
+
+      val expected = Some(MessageRequacked(requacker))
+      requacked should equal(expected)
     }
   }
 }
