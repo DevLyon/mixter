e8f50b88f102fa0915aa738e6b06e596b9a41cc2
fpellet <git@florentpellet.com>
2018-01-31 15:52:13 +0100

    [Identity][UserIdentity] Create Useridentity aggregate

---
+++ Mixter.Tests/Domain.Identity.UserIdentity.fs
@@ -0,0 +1,12 @@
+﻿module Mixter.Tests.Domain.Identity.UserIdentity
+
+open Xunit
+open Swensen.Unquote
+open Mixter.Domain.Identity.UserIdentity;
+
+module ``UserIdentity should`` =
+
+    [<Fact>] 
+    let ``return UserRegistered When register`` () =
+        test <@ register ({ Email = "user@mix-it.fr" }) 
+                    = [ UserRegistered { UserId = { Email = "user@mix-it.fr"} } ] @>
