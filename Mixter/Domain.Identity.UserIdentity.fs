5d77a06a95177990adff007175d8a64bcf6e2773
fpellet <git@florentpellet.com>
2018-01-31 15:52:13 +0100

    [Identity][UserIdentity] Create Useridentity aggregate

---
+++ Mixter/Domain.Identity.UserIdentity.fs
@@ -0,0 +1,15 @@
+﻿module Mixter.Domain.Identity.UserIdentity
+
+open Mixter.Domain.Documentation
+
+[<StructuredFormatDisplay("{Email}")>]
+type UserId = { Email: string }
+
+[<Event>]
+type Event = 
+    | UserRegistered of UserRegistered
+and UserRegistered = { UserId: UserId }
+
+[<Command>]
+let register userId = 
+    [ UserRegistered { UserId = userId } ]
