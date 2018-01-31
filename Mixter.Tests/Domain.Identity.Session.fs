e3edd6ee2bb65b883ed4d53ded3b57fc96f5f24b
fpellet <git@florentpellet.com>
2018-01-31 18:41:23 +0100

    [Identity][Session] Create Session agreggate

---
+++ Mixter.Tests/Domain.Identity.Session.fs
@@ -0,0 +1,39 @@
+﻿module Mixter.Tests.Domain.Identity.Session
+
+open Xunit
+open Swensen.Unquote
+open System
+open Mixter.Domain.Identity.UserIdentity;
+open Mixter.Domain.Identity.Session;
+
+module ``Session should`` =
+
+    [<Fact>]
+    let ``Return UserConnected When logs in`` () =
+        let sessionId = SessionId.Generate()
+        let currentTime = DateTime.Now
+        let user = { Email = "clem@mix-it.fr" }
+
+        test <@ logIn user sessionId currentTime
+                    = [ UserConnected { SessionId = sessionId; UserId = user; ConnectedAt = currentTime } ] @>
+
+    [<Fact>]
+    let ``Return UserDisconnected When disconnect`` () =
+        let sessionId = SessionId.Generate()
+        let userId = { Email = "clem@mix-it.fr" }
+        let history = [ UserConnected { SessionId = sessionId; UserId = userId; ConnectedAt = DateTime.Now }]
+        
+        test <@ history |> logOut
+                    = [UserDisconnected { SessionId = sessionId; UserId = userId } ] @>
+    
+    [<Fact>]
+    let ``Return nothing When disconnect If session have been disconnected`` () =
+        let sessionId = SessionId.Generate()
+        let userId = { Email = "clem@mix-it.fr" }
+        let history = [  
+            UserConnected { SessionId = sessionId; UserId = userId; ConnectedAt = DateTime.Now };
+            UserDisconnected { SessionId = sessionId; UserId = userId } 
+        ]   
+        
+        test <@ history |> logOut |> Seq.isEmpty @>
+        
---
+++ Mixter/Domain.Identity.Session.fs
@@ -0,0 +1,42 @@
+﻿module Mixter.Domain.Identity.Session
+
+open Mixter.Domain.Identity.UserIdentity
+open Mixter.Domain.Documentation
+
+type SessionId = SessionId of string
+    with static member Generate () = SessionId (System.Guid.NewGuid().ToString())
+
+[<Event>]
+type Event = 
+    | UserConnected of UserConnectedEvent
+    | UserDisconnected of UserDisconnectedEvent
+and UserConnectedEvent = { SessionId: SessionId; UserId: UserId; ConnectedAt: System.DateTime}
+and UserDisconnectedEvent = { SessionId: SessionId; UserId: UserId }
+
+[<Projection>]
+type DecisionProjection = 
+    | NotConnectedUser
+    | ConnectedUser of ConnectedUser
+    | DisconnectedUser of DisconnectedUser
+and DisconnectedUser = { UserId: UserId }
+and ConnectedUser = { UserId: UserId; SessionId: SessionId }
+    
+let private applyOne decisionProjection event =
+    match (decisionProjection, event) with
+    | (NotConnectedUser _, UserConnected e) -> ConnectedUser { UserId = e.UserId; SessionId = e.SessionId }
+    | (ConnectedUser _, UserDisconnected e) -> DisconnectedUser { UserId = e.UserId }
+    | _ -> failwith "Invalid transition"
+
+let private apply history =
+    Seq.fold applyOne NotConnectedUser history
+
+[<Command>]
+let logIn userId sessionId currentTime =
+    [ UserConnected { SessionId = sessionId; UserId = userId; ConnectedAt = currentTime } ]
+
+[<Command>]
+let logOut history =
+    match history |> apply with
+    | ConnectedUser p -> 
+        [ UserDisconnected { SessionId = p.SessionId; UserId = p.UserId } ]
+    | _ -> []
