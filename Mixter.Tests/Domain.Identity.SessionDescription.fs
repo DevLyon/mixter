0bf04586d6fcc5ea0a20beffde349eb0041d54ea
fpellet <git@florentpellet.com>
2018-02-01 15:48:35 +0100

    [Identity][SessionProjection] Creation session projection with repository

---
+++ Mixter.Tests/Domain.Identity.SessionDescription.fs
@@ -0,0 +1,40 @@
+﻿module Mixter.Tests.Domain.Identity.SessionDescription
+
+open Xunit
+open Swensen.Unquote
+open System
+open Mixter.Domain.Identity.UserIdentity;
+open Mixter.Domain.Identity.Session;
+open Mixter.Domain.Identity.SessionDescription;
+open Mixter.Infrastructure.Identity;
+
+module ``Session events handler should`` =
+    let sessionId = SessionId.Generate()
+    let userId = { Email = "clem@mix-it.fr"}
+    
+    [<Fact>]
+    let ``Add a SessionDescription When handle UserConnected`` () =
+        let userConnected = UserConnected { 
+            SessionId = sessionId
+            UserId = userId
+            ConnectedAt = DateTime.Now
+        }
+        let sessionsStore = MemorySessionsStore()
+        let handle = handle sessionsStore.Add sessionsStore.Remove
+
+        handle userConnected
+
+        test <@ sessionsStore.GetSession sessionId = Some { UserId = userId } @>
+            
+    [<Fact>]
+    let ``Remove Session When handle user disconnected`` () =
+        let userConnected = UserDisconnected { 
+            SessionId = sessionId
+            UserId = userId 
+        }
+        let sessionsStore = MemorySessionsStore()
+        let handle = handle sessionsStore.Add sessionsStore.Remove
+
+        handle userConnected
+
+        test <@ sessionsStore.GetSession sessionId = None @>
---
+++ Mixter/Infrastructure.Identity.fs
@@ -0,0 +1,30 @@
+﻿module Mixter.Infrastructure.Identity
+
+open Mixter.Domain.Documentation
+open Mixter.Domain.Identity.Session
+open Mixter.Domain.Identity.SessionDescription
+
+open System.Collections.Generic
+
+[<Repository>]
+type MemorySessionsStore() =
+    let store = Dictionary<SessionId, SessionDescription>()
+
+    [<Query>]
+    member _.GetSession sessionId = 
+        if store.ContainsKey(sessionId) 
+        then Some store[sessionId]
+        else option.None
+
+    [<Query>]
+    member _.GetUserSession userId =
+        let keyValue = store |> Seq.tryFind (fun d -> d.Value.UserId = userId)
+        match keyValue with
+        | Some x -> Some x.Key
+        | None -> None
+
+    member _.Add sessionId session = 
+        store.Add (sessionId, session)
+
+    member _.Remove sessionId = 
+        store.Remove sessionId |> ignore
