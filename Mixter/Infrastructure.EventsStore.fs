0bb4e48511d3ae0dd0f2f426f70e49205c3109e5
fpellet <git@florentpellet.com>
2018-02-01 16:00:45 +0100

    Create eventsStore

---
+++ Mixter/Infrastructure.EventsStore.fs
@@ -0,0 +1,21 @@
+﻿module Mixter.Infrastructure.EventsStore
+
+open System
+open System.Collections.Generic
+
+type MemoryEventsStore () = 
+    let eventsByAggregate = new Dictionary<Object, Object list>();
+
+    member __.Store (aggregateId: 'a) (events: 'b list) : unit = 
+        if not (eventsByAggregate.ContainsKey(aggregateId))
+        then eventsByAggregate.Add(aggregateId, [])
+        
+        let currentEvents = eventsByAggregate.[aggregateId]
+        let newEvents = currentEvents @ (events |> List.map (fun c -> c :> Object))
+
+        eventsByAggregate.[aggregateId] <- newEvents
+    
+    member __.Get<'b> aggregateId : 'b list = 
+        match eventsByAggregate.ContainsKey(aggregateId) with
+        | false -> []
+        | true -> eventsByAggregate.[aggregateId] |> List.map (fun c -> c :?> 'b)
