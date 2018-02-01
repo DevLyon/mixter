2b0b138474838d1c1c907c48f8d433817a243d4d
fpellet <git@florentpellet.com>
2018-02-01 15:48:35 +0100

    [Identity][SessionProjection] Creation session projection with repository

---
+++ Mixter/Domain.Identity.SessionDescription.fs
@@ -0,0 +1,16 @@
+﻿module Mixter.Domain.Identity.SessionDescription
+
+open Mixter.Domain.Documentation
+open Mixter.Domain.Identity.UserIdentity
+open Mixter.Domain.Identity.Session
+
+[<Projection>]
+type SessionDescription = { UserId: UserId }
+
+[<Handler>]
+let handle add remove event = 
+    match event with
+    | UserConnected e -> 
+        add e.SessionId { UserId = e.UserId }
+    | UserDisconnected e -> 
+        remove e.SessionId
---
+++ Mixter/Infrastructure.Identity.fs
@@ -0,0 +1,30 @@
+﻿module Mixter.Infrastructure.Identity
+
+open Mixter.Domain.Documentation
+open Mixter.Domain.Identity.Session
+open Mixter.Domain.Identity.SessionDescription
+
+open System.Collections.Generic
+
+[<Repository>]
+type MemorySessionsStore() =
+    let store = new Dictionary<SessionId, SessionDescription>()
+
+    [<Query>]
+    member __.GetSession sessionId = 
+        if store.ContainsKey(sessionId) 
+        then Some store.[sessionId]
+        else option.None
+
+    [<Query>]
+    member __.GetUserSession userId =
+        let keyValue = store |> Seq.tryFind (fun d -> d.Value.UserId = userId)
+        match keyValue with
+        | Some x -> Some x.Key
+        | None -> None
+
+    member __.Add sessionId session = 
+        store.Add (sessionId, session)
+
+    member __.Remove sessionId = 
+        store.Remove sessionId |> ignore
