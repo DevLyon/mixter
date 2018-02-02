c16a616f18bd69900d72125406bd9b8a363df288
fpellet <git@florentpellet.com>
2018-02-02 18:56:32 +0100

    [Core][Subscription][Test OK] When Follow Then UserFollowed is raised

--- Mixter/Domain.Core.Subscription.fs
+++ Mixter/Domain.Core.Subscription.fs
@@ -1,6 +1,16 @@
 ﻿module Mixter.Domain.Core.Subscription
 
+open Mixter.Domain.Documentation
 open Mixter.Domain.Identity.UserIdentity
 open Mixter.Domain.Core.Message
 
 type SubscriptionId = { Follower: UserId; Followee: UserId }
+
+[<Event>]
+type Event = 
+    | UserFollowed of UserFollowed
+and UserFollowed = { SubscriptionId: SubscriptionId }
+
+[<Command>]
+let follow follower followee =
+    [ UserFollowed { SubscriptionId = { Follower = follower; Followee = followee } } ]
