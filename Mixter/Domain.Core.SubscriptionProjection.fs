501f717f74af46df1bdce2cc56ba6eb5f400446b
fpellet <git@florentpellet.com>
2018-02-02 19:25:55 +0100

    [Core][UpdateFollowers][Test OK] When UserFollowed Then save follower

--- Mixter/Domain.Core.SubscriptionProjection.fs
+++ Mixter/Domain.Core.SubscriptionProjection.fs
@@ -6,3 +6,12 @@ open Mixter.Domain.Core.Subscription
 
 [<Projection>]
 type SubscriptionProjection = { Followee: UserId; Follower: UserId }
+
+let private project (subscriptionId: SubscriptionId) =
+    { Followee = subscriptionId.Followee; Follower = subscriptionId.Follower }
+
+[<Handler>]
+let handle save remove evt =
+    match evt with
+    | UserFollowed e -> project e.SubscriptionId |> save
+    | _ -> ()
