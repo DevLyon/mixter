97c43ad0b17ee4bc84ce1fa1175fa12260c544e9
fpellet <git@florentpellet.com>
2018-02-02 19:38:44 +0100

    [Core][NotifyFollowerOfFolloweeMessage][Test KO] Given follower When MessageQuacked by followee Then raise FolloweeMessageQuacked

---
+++ Mixter.Tests/Domain.Core.NotifyFollowerOfFolloweeMessage.fs
@@ -0,0 +1,27 @@
+namespace Mixter.Tests.Domain.Core.NotifyFollowerOfFolloweeMessage
+
+open Xunit
+open Swensen.Unquote
+open Mixter.Infrastructure.EventsStore
+open Mixter.Domain.Core.Message
+
+module ``NotifyFollowerOfFolloweeMessage should`` =
+    open Mixter.Domain.Core.Subscription
+    open Mixter.Domain.Identity.UserIdentity
+    open Mixter.Domain.Core.NotifyFollowerOfFolloweeMessage
+    open Mixter.Infrastructure.Core
+
+    [<Fact>] 
+    let ``Raise FolloweeMessageQuacked When MessageQuacked by followee`` () =
+        let followerRepository = MemoryFollowersRepository()
+        let eventsStore = MemoryEventsStore()
+        let follower = { Email = "follower@mix-it.fr" } 
+        let followee = { Email = "followee@mix-it.fr" }
+        followerRepository.Save { Follower = follower; Followee = followee }
+        let subscriptionId = {Follower = follower; Followee = followee}
+        eventsStore.Store subscriptionId [UserFollowed { SubscriptionId = subscriptionId}]
+        let handle = handle (eventsStore.Get) (followerRepository.GetFollowers)
+        let messageId = MessageId.Generate()
+
+        test <@ MessageQuacked { MessageId = messageId; AuthorId = followee; Content = "hello" }
+                |> handle |> Seq.toList = [ FolloweeMessageQuacked { SubscriptionId = subscriptionId; Message = messageId } ] @>
---
+++ Mixter/Domain.Core.NotifyFollowerOfFolloweeMessage.fs
@@ -0,0 +1,17 @@
+﻿module Mixter.Domain.Core.NotifyFollowerOfFolloweeMessage
+
+open Mixter.Domain.Documentation
+open Mixter.Domain.Identity.UserIdentity
+open Mixter.Domain.Core.Subscription
+open Mixter.Domain.Core.Message
+
+type GetSubscriptionHistory = SubscriptionId -> Subscription.Event list
+type GetFollowers = UserId -> UserId seq
+
+[<Handler>]
+let handle 
+        (getSubscriptionHistory: GetSubscriptionHistory) 
+        (getFollowers: GetFollowers) 
+        (evt: Message.Event) 
+        : Subscription.Event seq =
+    Seq.empty
