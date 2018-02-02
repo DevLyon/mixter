5fb44baefa80128252f4407aa048b96fa6b3f71b
fpellet <git@florentpellet.com>
2018-02-02 21:35:01 +0100

    [Core][Timeline] Create repository

---
+++ Mixter.Tests/Infrastructure.Core.fs
@@ -0,0 +1,32 @@
+﻿namespace Mixter.Tests.Infrastructure.Core
+
+open Swensen.Unquote
+open Xunit
+open Mixter.Infrastructure.Core
+
+module ``MemoryTimelineMessageStore should`` =
+    open Mixter.Domain.Identity.UserIdentity
+    open Mixter.Domain.Core.Message
+    open Mixter.Domain.Core.Timeline
+
+    [<Fact>]
+    let ``return messages of user when GetMessagesOfUser`` () =
+        let repository = MemoryTimelineMessageStore()
+        let timelineMessage = { Owner = { Email = "A" }; Author = { Email = "A" }; Content = "Hello"; MessageId = MessageId.Generate() }
+
+        repository.Save timelineMessage
+
+        test <@ repository.GetMessagesOfUser timelineMessage.Owner |> Seq.toList
+                    = [timelineMessage] @>
+
+    [<Fact>]
+    let ``save only one message when save two same message`` () =
+        let repository = MemoryTimelineMessageStore()
+        let timelineMessage = { Owner = { Email = "A" }; Author = { Email = "A" }; Content = "Hello"; MessageId = MessageId.Generate() }
+
+        repository.Save timelineMessage
+        repository.Save timelineMessage
+
+        test <@ repository.GetMessagesOfUser timelineMessage.Owner |> Seq.toList
+                  = [timelineMessage] @>
+            
---
+++ Mixter/Infrastructure.Core.fs
@@ -0,0 +1,18 @@
+﻿module Mixter.Infrastructure.Core
+
+open Mixter.Domain.Documentation
+open Mixter.Domain.Core
+open Mixter.Domain.Core.Timeline
+
+open System.Collections.Generic
+
+[<Repository>]
+type MemoryTimelineMessageStore() =
+    let store = new HashSet<TimelineMessage>()
+
+    member __.Save timelineMessage =
+        store.Add timelineMessage |> ignore
+
+    [<Query>]
+    member __.GetMessagesOfUser userId =
+        store |> Seq.filter (fun p -> p.Owner = userId)
