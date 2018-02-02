18692f9e12c41f68ff2c909d7c22bcc7111a3f20
Hauke Stange <hauke.stange@jimdo.com>
2018-02-02 12:41:12 +0100

    Adds follow another user

--- app/Domain/Subscriptions/Subscription.php
+++ app/Domain/Subscriptions/Subscription.php
@@ -7,7 +7,12 @@ use App\Domain\IEventPublisher;
 
 class Subscription
 {
-    public static function followUser(IEventPublisher $eventPublisher, UserId $followerId, UserId $followeeId)
+    public static function followUser(IEventPublisher $eventPublisher, UserId $followerId, UserId $followeeId): void
     {
+        $eventPublisher->publish(
+            new UserFollowed(
+                new SubscriptionId($followerId,$followeeId)
+            )
+        );
     }
 }
