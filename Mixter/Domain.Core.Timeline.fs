9eaff332a45541f17bffe626404b73af90f00db5
fpellet <git@florentpellet.com>
2018-02-02 21:33:24 +0100

    [Core][Timeline] Create projection

---
+++ Mixter/Domain.Core.Timeline.fs
@@ -0,0 +1,8 @@
+﻿module Mixter.Domain.Core.Timeline
+
+open Mixter.Domain.Documentation
+open Mixter.Domain.Identity.UserIdentity
+open Mixter.Domain.Core.Message
+
+[<Projection>]
+type TimelineMessage = { Owner: UserId; Author: UserId; Content: string; MessageId: MessageId }
