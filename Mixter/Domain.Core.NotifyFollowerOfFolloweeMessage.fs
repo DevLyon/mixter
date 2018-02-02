5c2c21d905f3f5511083686337b15be0847cfe41
fpellet <git@florentpellet.com>
2018-02-02 19:39:02 +0100

    [Core][NotifyFollowerOfFolloweeMessage][Test OK] Given follower When MessageQuacked by followee Then raise FolloweeMessageQuacked

--- Mixter/Domain.Core.NotifyFollowerOfFolloweeMessage.fs
+++ Mixter/Domain.Core.NotifyFollowerOfFolloweeMessage.fs
@@ -8,10 +8,22 @@ open Mixter.Domain.Core.Message
 type GetSubscriptionHistory = SubscriptionId -> Subscription.Event list
 type GetFollowers = UserId -> UserId seq
 
+let notifyAllFollowers (getSubscriptionHistory: GetSubscriptionHistory) (getFollowers: GetFollowers) (followee: UserId) messageId =
+    getFollowers followee 
+    |> Seq.collect (fun follower -> 
+        { Follower = follower; Followee = followee }
+        |> getSubscriptionHistory 
+        |> Subscription.notifyFollower messageId)
+
 [<Handler>]
 let handle 
         (getSubscriptionHistory: GetSubscriptionHistory) 
         (getFollowers: GetFollowers) 
         (evt: Message.Event) 
         : Subscription.Event seq =
-    Seq.empty
+    let notifyAllFollowers = notifyAllFollowers getSubscriptionHistory getFollowers
+
+    match evt with
+    | MessageQuacked e -> notifyAllFollowers e.AuthorId e.MessageId
+    | _ -> Seq.empty
+
