550b3c01564653d7ec421c9098ae309dbd1e0e38
fpellet <git@florentpellet.com>
2018-02-02 15:22:33 +0100

    [Core][Subscription][Test KO] When Follow Then UserFollowed is raised

---
+++ Mixter.Tests/Domain.Core.Subscription.fs
@@ -0,0 +1,16 @@
+﻿namespace Mixter.Tests.Domain.Core.Subscription
+
+open Xunit
+open Swensen.Unquote
+open Mixter.Domain.Identity.UserIdentity
+open Mixter.Domain.Core.Message
+open Mixter.Domain.Core.Subscription
+
+module ``Subscription should`` =
+    [<Fact>] 
+    let ``When follow Then UserFollowed is returned`` () =
+        let follower = { Email = "follower@mix-it.fr" } 
+        let followee = { Email = "followee@mix-it.fr" }
+
+        test <@ follow follower followee
+                    = [UserFollowed { SubscriptionId = { Follower = follower; Followee = followee } }] @>
